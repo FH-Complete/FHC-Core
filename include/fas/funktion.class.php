6af0939000200f9cefb2c73575f107edf7c1cdb3
Rudolf Hangl <ruhan@technikum-wien.at>
2006-11-29 13:42:12 +0000

    Funktion sync

---
+++ include/fas/funktion.class.php
@@ -0,0 +1,239 @@
+<?php
+/* Copyright (C) 2006 Technikum-Wien
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Authors: Christian Paminger <christian.paminger@technikum-wien.at>, 
+ *          Andreas Oesterreicher <andreas.oesterreicher@technikum-wien.at> and
+ *          Rudolf Hangl <rudolf.hangl@technikum-wien.at>.
+ */
+/** 
+ * Klasse funktion (FAS-Online)
+ * @create 14-03-2006
+ */
+
+class funktion
+{
+	var $conn;   			// @var resource DB-Handle
+	var $new;     			// @var boolean
+	var $errormsg; 		// @var string
+	var $result = array(); 	// @var fachbereich Objekt 
+	
+	//Tabellenspalten
+	var $funktion_kurzbz;	// @var integer
+	var $bezeichnung;		// @var string
+	var $aktiv;			// @var boolean
+	var $updateamum;		// @var timestamp
+	var $updatevon=0;		// @var string
+	var $insertamum;		// @var timestamp
+	var $insertvon=0;		// @var string
+	var $ext_id;			// @var bigint
+	
+	
+	/**
+	 * Konstruktor
+	 * @param $conn Connection zur DB
+	 *        $funktion_kurzbz ID der zu ladenden Funktion
+	 */
+	function funktion($conn, $funktion_kurzbz=null)
+	{
+		$this->conn = $conn;
+		if($funktion_kurzbz != null)
+			$this->load($funktion_kurzbz);
+	}
+	
+	/**
+	 * Laedt alle verfuegbaren Funktionen
+	 * @return true wenn ok, false im Fehlerfall
+	 */
+	function getAll()
+	{
+		$qry = 'SELECT * FROM tbl_funktion order by funktion_kurzbz;';
+		
+		if(!$res = pg_query($this->conn, $qry))
+		{
+			$this->errormsg = 'Fehler beim laden der Datensaetze';
+			return false;
+		}
+		
+		while($row = pg_fetch_object($res))
+		{
+			$funktion_obj = new funktion($this->conn);
+			
+			$funktion_obj->funktion_kurzbz = $row->funktion_kurzbz;
+			$funktion_obj->bezeichnung    = $row->bezeichnung;
+			$funktion_obj->aktiv           = $row->aktiv;
+			$funktion_obj->insertamum=$row->insertamum;
+			$funktion_obj->insertvon=$$row->insertvon;
+			$funktion_obj->updateamum=$row->updateamum;
+			$funktion_obj->updatevon=$row->updatevon;
+			
+			$this->result[] = $funktion_obj;
+		}
+		return true;
+	}
+	
+	/**
+	 * Laedt eine Funktion
+	 * @param $funktion_kurzbz ID der zu ladenden Funktion
+	 * @return true wenn ok, false im Fehlerfall
+	 */
+	function load($funktion_kurzbz)
+	{
+		if($funktion_kurzbz == '')
+		{
+			$this->errormsg = 'funktion_bz darf nicht leer sein sein';
+			return false;
+		}
+		
+		$qry = "SELECT * FROM tbl_funktion WHERE funktion_kurzbz = '$funktion_kurzbz';";
+		
+		if(!$res = pg_query($this->conn, $qry))
+		{
+			$this->errormsg = 'Fehler beim laden des Datensatzes';
+			return false;
+		}
+		
+		if($row=pg_fetch_object($res))
+		{
+			$this->funktion_kurzbz	= $row->funktion_kurzbz;
+			$this->bezeichnung		= $row->bezeichnung;
+			$this->aktiv			= $row->aktiv;
+			$this->insertamum		=$row->insertamum;
+			$this->insertvon		=$row->insertvon;
+			$this->updateamum		= $row->updateamum;
+			$this->updatevon		= $row->updatevon;
+		}
+		else 
+		{
+			$this->errormsg = 'Es ist kein Datensatz mit dieser ID vorhanden';
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Loescht einen Datensatz
+	 * @param $fachb_id id des Datensatzes der geloescht werden soll
+	 * @return true wenn ok, false im Fehlerfall
+	 */
+	function delete($funktion_kurzbz)
+	{
+		$this->errormsg = 'Noch nicht implementiert';
+		return false;
+	}
+	function addslashes($var)
+	{
+		return ($var!=''?"'".addslashes($var)."'":'null');
+	}
+	/**
+	 * Speichert den aktuellen Datensatz
+	 * @return true wenn ok, false im Fehlerfall
+	 */
+	/**
+	 * Prueft die Gueltigkeit der Variablen
+	 * @return true wenn ok, false im Fehlerfall
+	 */
+	function checkvars()
+	{	
+		$this->bezeichnung = str_replace("'",'´',$this->bezeichnung);
+		
+		//Laenge Pruefen
+		if(strlen($this->bezeichnung)>64)           
+		{
+			$this->errormsg = "Bezeichnung darf nicht laenger als 128 Zeichen sein bei <b>$this->funktion_kurzbz</b> - $this->bezeichnung";
+			return false;
+		}
+				
+		$this->errormsg = '';
+		return true;		
+	}
+	function save()
+	{
+		//Gueltigkeit der Variablen pruefen
+		if(!$this->checkvars())
+			return false;
+			
+		if($this->new)
+		{
+			//Pruefen ob funktion_kurzbz befüllt ist
+			if($this->funktion_kurzbz == '')
+			{
+				$this->errormsg = 'funktion_kurzbz darf nicht leer sein';
+				return false;
+			}
+			//Neuen Datensatz anlegen		
+			$qry = 'INSERT INTO tbl_funktion (funktion_kurzbz, bezeichnung, aktiv, insertamum, insertvon, 
+				updateamum, updatevon) VALUES ('.
+				$this->addslashes($this->funktion_kurzbz).', '.
+				$this->addslashes($this->bezeichnung).', '.
+				($this->aktiv?'true':'false').', '. 
+				$this->addslashes($this->insertamum).', '.
+				$this->addslashes($this->insertvon).', '.
+				$this->addslashes($this->updateamum).', '.
+				$this->addslashes($this->updatevon).'); ';
+		}
+		else 
+		{
+			//bestehenden Datensatz akualisieren
+			
+			//Pruefen ob fachbereich_id eine gueltige Zahl ist
+			if(!is_numeric($this->fachbereich_id) || $this->fachbereich_id == '')
+			{
+				$this->errormsg = 'fachbereich_id muss eine gueltige Zahl sein';
+				return false;
+			}
+			
+			$qry = 'UPDATE tbl_funktion SET '. 
+				'funktion_kurzbz='.$this->addslashes($this->funktion_kurzbz).', '.
+				'bezeichnung='.$this->addslashes($this->bezeichnung).', '.
+				'aktiv='.($this->aktiv?'true':'false') .', '.
+				'insertamum='.$this->addslashes($this->insertamum).', '.
+				'insertvon='.$this->addslashes($this->insertvon).', '.
+				'updateamum='.$this->addslashes($this->updateamum).', '.
+				'updatevon='.$this->addslashes($this->updatevon).', '.
+				'WHERE funktion_kurzbz = '.$this->addslashes($this->funktion_kurzbz).';';
+		}
+		
+		if(pg_query($this->conn, $qry))
+		{
+			/*//Log schreiben
+			$sql = $qry;
+			$qry = "SELECT nextval('log_seq') as id;";
+			if(!$row = pg_fetch_object(pg_query($this->conn, $qry)))
+			{
+				$this->errormsg = 'Fehler beim Auslesen der Log-Sequence';
+				return false;
+			}
+						
+			$qry = "INSERT INTO log(log_pk, creationdate, creationuser, sql) VALUES('$row->id', now(), '$this->updatevon', '".addslashes($sql)."')";
+			if(pg_query($this->conn, $qry))
+				return true;
+			else 
+			{
+				$this->errormsg = 'Fehler beim Speichern des Log-Eintrages';
+				return false;
+			}*/
+			return true;
+		}
+		else
+		{
+			$this->errormsg = 'Fehler beim Speichern des Datensatzes';
+			return false;
+		}		
+	}
+}
+?>
