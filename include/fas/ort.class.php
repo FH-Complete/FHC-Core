fa37009c05cf87858d91fd7ab470e1b66dcdf4c4
Rudolf Hangl <ruhan@technikum-wien.at>
2006-11-30 16:40:26 +0000

    SYNC Ort

--- include/fas/ort.class.php
+++ include/fas/ort.class.php
@@ -20,11 +20,11 @@
  *          Rudolf Hangl <rudolf.hangl@technikum-wien.at>.
  */
 /** 
- * Klasse fachbereich (FAS-Online)
+ * Klasse ort (FAS-Online)
  * @create 14-03-2006
  */
 
-class fachbereich
+class ort
 {
 	var $conn;   			// @var resource DB-Handle
 	var $new;     			// @var boolean
@@ -32,37 +32,40 @@ class fachbereich
 	var $result = array(); 	// @var fachbereich Objekt 
 	
 	//Tabellenspalten
-	var $fachbereich_id;		// @var integer
+	var $ort_kurzbz;		// @var string
 	var $bezeichnung;		// @var string
-	var $kurzbz;			// @var string
-	var $farbe;			// @var string
-	var $studiengang_kz;	// @var integer
+	var $planbezeichnung;	// @var string
+	var $max_person;		// @var integer
+	var $aktiv;			// @var boolean
+	var $lageplan;		// @var oid
+	var $dislozierung;		// @var smallint
+	var $kosten;			// @var numeric(8,2)
+	var $lehre;			// @var boolean
 	var $updateamum;		// @var timestamp
 	var $updatevon=0;		// @var string
 	var $insertamum;		// @var timestamp
 	var $insertvon=0;		// @var string
-	var $ext_id;			// @var bigint
 	
 	
 	/**
 	 * Konstruktor
 	 * @param $conn Connection zur DB
-	 *        $fachb_id ID des zu ladenden Fachbereiches
+	 *        $fachb_id ID des zu ladenden Ortes
 	 */
-	function fachbereich($conn, $fachb_id=null)
+	function ort($conn, $ort_kurzbz=null)
 	{
 		$this->conn = $conn;
-		if($fachb_id != null)
-			$this->load($fachb_id);
+		if($ort_kurzbz != null)
+			$this->load($ort_kurzbz);
 	}
 	
 	/**
-	 * Laedt alle verfuegbaren Fachbereiche
+	 * Laedt alle verfuegbaren Orte
 	 * @return true wenn ok, false im Fehlerfall
 	 */
 	function getAll()
 	{
-		$qry = 'SELECT * FROM tbl_fachbereich order by name;';
+		$qry = 'SELECT * FROM tbl_ort order by ort_kurzbz;';
 		
 		if(!$res = pg_query($this->conn, $qry))
 		{
@@ -72,33 +75,41 @@ class fachbereich
 		
 		while($row = pg_fetch_object($res))
 		{
-			$fachb_obj = new fachbereich($this->conn);
+			$ort_obj = new ort($this->conn);
 			
-			$fachb_obj->fachbereich_id = $row->fachbereich_pk;
-			$fachb_obj->erhalter_id    = $row->erhalter_fk;
-			$fachb_obj->name           = $row->name;
-			$fachb_obj->updateamum     = $row->creationdate;
-			$fachb_obj->updatevon     = $row->creationuser;
+			$ort_obj->ort_kurzbz = $row->ort_kurzbz;
+			$ort_obj->bezeichnung = $row->bezeichnung;
+			$ort_obj->planbezeichnung = $row->planbezeichnung;
+			$ort_obj->max_person = $row->max_person;
+			$ort_obj->aktiv = $row->aktiv;
+			$ort_obj->lageplan = $row->lageplan;
+			$ort_obj->dislozierung = $row->dislozierung;
+			$ort_obj->kosten = $row->kosten;
+			$ort_obj->lehre = $row->lehre;
+			$ort_obj->insertamum = $row->insertamum;
+			$ort_obj->insertvon = $row->insertvon;
+			$ort_obj->updateamum = $row->updateamum;
+			$ort_obj->updatevon     = $row->updatevon;
 			
-			$this->result[] = $fachb_obj;
+			$this->result[] = $ort_obj;
 		}
 		return true;
 	}
 	
 	/**
-	 * Laedt einen Fachbereich
-	 * @param $fachb_id ID des zu ladenden Fachbereiches
+	 * Laedt einen Ort
+	 * @param $fachb_id ID des zu ladenden Ortes
 	 * @return true wenn ok, false im Fehlerfall
 	 */
-	function load($fachb_id)
+	function load($ort_kurzbz)
 	{
-		if(!is_numeric($fachb_id) || $fachb_id == '')
+		if($ort_kurzbz == '')
 		{
-			$this->errormsg = 'fachb_id muss eine gueltige Zahl sein';
+			$this->errormsg = 'kurzbz darf nicht leer sein';
 			return false;
 		}
 		
-		$qry = "SELECT * FROM tbl_fachbereich WHERE fachbereich_pk = '$fachb_id';";
+		$qry = "SELECT * FROM tbl_ort WHERE ort_kurzbz = '$ort_kurzbz';";
 		
 		if(!$res = pg_query($this->conn, $qry))
 		{
@@ -108,11 +119,19 @@ class fachbereich
 		
 		if($row=pg_fetch_object($res))
 		{
-			$this->fachbereich_id = $row->fachbereich_pk;
-			$this->erhalter_id    = $row->erhalter_fk;
-			$this->name           = $row->name;
-			$this->updateamum     = $row->creationdate;
-			$this->updatevon      = $row->creationuser;
+			$this->ort_kurzbz = $row->ort_kurzbz;
+			$this->bezeichnung = $row->bezeichnung;
+			$this->planbezeichnung = $row->planbezeichnung;
+			$this->max_person = $row->max_person;
+			$this->aktiv = $row->aktiv;
+			$this->lageplan = $row->lageplan;
+			$this->dislozierung = $row->dislozierung;
+			$this->kosten = $row->kosten;
+			$this->lehre = $row->lehre;
+			$this->insertamum = $row->insertamum;
+			$this->insertvon = $row->insertvon;
+			$this->updateamum = $row->updateamum;
+			$this->updatevon     = $row->updatevon;
 		}
 		else 
 		{
@@ -128,7 +147,7 @@ class fachbereich
 	 * @param $fachb_id id des Datensatzes der geloescht werden soll
 	 * @return true wenn ok, false im Fehlerfall
 	 */
-	function delete($fachb_id)
+	function delete($ort_kurzbz)
 	{
 		$this->errormsg = 'Noch nicht implementiert';
 		return false;
@@ -144,20 +163,20 @@ class fachbereich
 	function checkvars()
 	{	
 		$this->bezeichnung = str_replace("'",'´',$this->bezeichnung);
-		$this->kurzbz = str_replace("'",'´',$this->kurzbz);
+		$this->planbezeichnung = str_replace("'",'´',$this->planbezeichnung);
 
 		
 		//Laenge Pruefen
-		if(strlen($this->bezeichnung)>128)           
+		if(strlen($this->bezeichnung)>30)           
 		{
-			$this->errormsg = "Bezeichnung darf nicht laenger als 128 Zeichen sein bei <b>$this->ext_id</b> - $this->bezeichnung";
-			return false;
-		}
-		if(strlen($this->kurzbz)>16)
-		{
-			$this->errormsg = "Kurzbez darf nicht laenger als 16 Zeichen sein bei <b>$this->ext_id</b> - $this->kurzbz";
+			$this->errormsg = "Bezeichnung darf nicht laenger als 30 Zeichen sein bei <b>$this->ext_id</b> - $this->bezeichnung";
 			return false;
 		}		
+		if(strlen($this->planbezeichnung)>5)
+		{
+			$this->errormsg = "Planbezeichnung darf nicht laenger als 5 Zeichen sein bei <b>$this->ext_id</b> - $this->kurzbz";
+			return false;
+		}	
 		$this->errormsg = '';
 		return true;		
 	}
@@ -173,49 +192,56 @@ class fachbereich
 			
 		if($this->new)
 		{
-			//Pruefen ob fachbereich_id eine gueltige Zahl ist
-			if(!is_numeric($this->fachbereich_id) || $this->fachbereich_id == '')
+			//Pruefen ob ort_kurzbz eine gueltige Bezeichnung ist
+			if($this->ort_kurzbz == '')
 			{
-				$this->errormsg = 'fachbereich_id muss eine gueltige Zahl sein';
+				$this->errormsg = 'ort_kurzbz darf nicht leer sein';
 				return false;
 			}
 			//Neuen Datensatz anlegen		
-			$qry = 'INSERT INTO tbl_fachbereich (fachbereich_id, bezeichnung, kurzbz, farbe, ext_id, insertamum, insertvon, 
-				updateamum, updatevon, studiengang_kz) VALUES ('.
-				$this->addslashes($this->fachbereich_id).', '.
+			$qry = 'INSERT INTO tbl_ort (ort_kurzbz, bezeichnung, planbezeichnung, max_person, aktiv, lageplan, 
+				dislozierung, kosten, lehre, insertamum, insertvon, updateamum, updatevon) VALUES ('.
+				$this->addslashes($this->ort_kurzbz).', '.
 				$this->addslashes($this->bezeichnung).', '.
-				$this->addslashes($this->kurzbz).', '.
-				$this->addslashes($this->farbe).', '.
-				$this->addslashes($this->ext_id).', '.
+				$this->addslashes($this->planbezeichnung).', '.
+				$this->addslashes($this->max_person).', '.
+				($this->aktiv?'true':'false').', '. 
+				$this->addslashes($this->lageplan).', '.
+				$this->addslashes($this->dislozierung).', '.
+				$this->addslashes($this->kosten).', '.
+				($this->lehre?'true':'false').', '. 
 				$this->addslashes($this->insertamum).', '.
 				$this->addslashes($this->insertvon).', '.
 				$this->addslashes($this->updateamum).', '.
-				$this->addslashes($this->updatevon).', '.
-				$this->addslashes($this->studiengang_kz).');';
+				$this->addslashes($this->updatevon).');';
+
 		}
 		else 
 		{
 			//bestehenden Datensatz akualisieren
 			
-			//Pruefen ob fachbereich_id eine gueltige Zahl ist
-			if(!is_numeric($this->fachbereich_id) || $this->fachbereich_id == '')
+			//Pruefen ob ort_kurzbz gueltig ist
+			if($this->ort_kurzbz == '')
 			{
-				$this->errormsg = 'fachbereich_id muss eine gueltige Zahl sein';
+				$this->errormsg = 'ort_kurzbz darf nicht leer sein';
 				return false;
 			}
 			
-			$qry = 'UPDATE tbl_fachbereich SET '. 
-				'fachbereich_id='.$this->addslashes($this->fachbereich_id).', '.
+			$qry = 'UPDATE tbl_ort SET '. 
+				'ort_kurzbz='.$this->addslashes($this->ort_kurzbz).', '.
 				'bezeichnung='.$this->addslashes($this->bezeichnung).', '.
-				'kurzbz='.$this->addslashes($this->kurzbz).', '.
-				'farbe='.$this->addslashes($this->farbe).', '.
-				'ext_id='.$this->addslashes($this->ext_id).', '.
+				'planbezeichnung='.$this->addslashes($this->planbezeichnung).', '.
+				'max_person='.$this->addslashes($this->max_person).', '.
+				'aktiv='.($this->aktiv?'true':'false') .', '.
+				'lageplan='.$this->addslashes($this->lageplan).', '.
+				'dislozierung='.$this->addslashes($this->dislozierung).', '.
+				'kosten='.$this->addslashes($this->kosten).', '.
+				'lehre='.($this->lehre?'true':'false') .', '.
 				'insertamum='.$this->addslashes($this->insertamum).', '.
 				'insertvon='.$this->addslashes($this->insertvon).', '.
 				'updateamum='.$this->addslashes($this->updateamum).', '.
-				'updatevon='.$this->addslashes($this->updatevon).', '.
-				'studiengang_kz='.$this->addslashes($this->studiengang_kz).' '.
-				'WHERE fachbereich_id = '.$this->addslashes($this->fachbereich_id).';';
+				'updatevon='.$this->addslashes($this->updatevon).' '.
+				'WHERE ort_kurzbz = '.$this->addslashes($this->ort_kurzbz).';';
 		}
 		
 		if(pg_query($this->conn, $qry))
