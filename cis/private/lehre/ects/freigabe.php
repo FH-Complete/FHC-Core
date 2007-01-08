87fe8cb18bd0e2fd0a624330716fa04f2f181bba
Andreas Österreicher <oesi@technikum-wien.at>
2007-01-08 16:32:10 +0000

    Anpassung an LOG_PATH

--- cis/private/lehre/ects/freigabe.php
+++ cis/private/lehre/ects/freigabe.php
@@ -46,7 +46,7 @@
 	function WriteLog($qry,$uid)
 	{
 
-		if($fp=fopen(LVINFO_LOG_PATH,"a"))
+		if($fp=fopen(LOG_PATH.'lvinfo.log',"a"))
 		{
 			fwrite($fp,"\n");
 			fwrite($fp,$uid." ". date("d.m.Y - H:i:s") . " ". $qry);
