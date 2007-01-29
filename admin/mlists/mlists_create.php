3dda69c8af5e27efd345bae646f4677c0d4f3f90
Andreas Österreicher <oesi@technikum-wien.at>
2007-01-29 16:13:19 +0000

    - Anpassung mlists - Jahresplan aktualisiert - Kollegiumskandidaturen hinzugefügt

--- admin/mlists/mlists_create.php
+++ admin/mlists/mlists_create.php
@@ -2,21 +2,22 @@
 /**
  * Changes:	23.10.2004: Anpassung an neues DB-Schema (WM)
  */
-	include('../../config.inc.php');
-	include('../../../include/functions.inc.php');
-	include('../../../include/globals.inc.php');
+	require_once('../../vilesci/config.inc.php');
+	require_once('../../include/functions.inc.php');
+	require_once('../../include/globals.inc.php');
 	
 	if (!$conn = @pg_pconnect(CONN_STRING))
-	   	die("Es konnte keine Verbindung zum Server aufgebaut werden.");
-	if(!($result_stg=pg_exec($conn, "SELECT studiengang_kz, bezeichnung, kurzbz FROM tbl_studiengang ORDER BY kurzbz ASC")))
+	   	die('Es konnte keine Verbindung zum Server aufgebaut werden.');
+	if(!($result_stg=pg_query($conn, "SELECT studiengang_kz, bezeichnung, lower(typ::varchar(1) || kurzbz) as kurzbz FROM public.tbl_studiengang ORDER BY kurzbz ASC")))
 		die(pg_errormessage($conn));
-	$num_rows=pg_numrows($result_stg);
+	$num_rows=pg_num_rows($result_stg);
+	
 ?>
 <HTML>
 <HEAD>
 <TITLE>Mailinglisten</TITLE>
 <META http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<LINK rel="stylesheet" href="../../../skin/vilesci.css" type="text/css">
+<LINK rel="stylesheet" href="../../skin/vilesci.css" type="text/css">
 </HEAD>
 
 <BODY class="background_main">
@@ -27,39 +28,40 @@
 	$crlf="\n";
 	for ($i=0; $i<$num_rows; $i++)
 	{
-		$row=pg_fetch_object($result_stg, $i);
+		$row=pg_fetch_object($result_stg);
      	$stg_id=$row->studiengang_kz;
 		$stg_kzbz=$row->kurzbz;
-		$sql_query="SELECT * FROM tbl_mailgrp WHERE studiengang_kz=$stg_id ORDER BY mailgrp_kurzbz";
+		$sql_query="SELECT * FROM public.tbl_gruppe WHERE studiengang_kz=$stg_id AND mailgrp=true ORDER BY gruppe_kurzbz";
 		//echo $sql_query;
-		if(!($result_mg=pg_exec($conn, $sql_query)))
+		if(!($result_mg=pg_query($conn, $sql_query)))
 			die(pg_errormessage($conn));
-		$nr_mg=pg_numrows($result_mg);
+		$nr_mg=pg_num_rows($result_mg);
 
 		// Mailgroups
 		for  ($j=0; $j<$nr_mg; $j++)
 		{
 			$row_mg=pg_fetch_object($result_mg, $j);
-			$mg_kurzbz=$row_mg->mailgrp_kurzbz;
-			$sql_query='SELECT uid, nachname, vornamen '.
-				       'FROM tbl_person as p join tbl_personmailgrp using(uid) '.
-				       'WHERE tbl_personmailgrp.mailgrp_kurzbz=\''.$mg_kurzbz.'\' '."AND p.uid NOT LIKE '\\\\_%'".
+			$mg_kurzbz=$row_mg->gruppe_kurzbz;
+			$sql_query='SELECT tbl_benutzergruppe.uid, nachname, vorname '.
+				       'FROM campus.vw_benutzer, public.tbl_benutzergruppe '.
+				       'WHERE vw_benutzer.uid=tbl_benutzergruppe.uid AND '.
+				       "UPPER(gruppe_kurzbz)=UPPER('$mg_kurzbz') AND tbl_benutzergruppe.uid NOT LIKE '\\\\_%' ".
 					   'ORDER BY nachname';
 			//echo $sql_query;
-			if(!($result_person=pg_exec($conn, $sql_query)))
+			if(!($result_person=pg_query($conn, $sql_query)))
 				die(pg_errormessage($conn));
 
 			// File Operations
 			$name=$mg_kurzbz.'.txt';
 			$name=strtolower($name);
-			$fp=fopen('../../../../mlists/'.$name,"w");
+			$fp=fopen('../../../mlists/'.$name,"w");
 			//$fp=fopen('../../../../mlists/'.$name,"w");
 			
-			$nr_person=pg_numrows($result_person);
+			$nr_person=pg_num_rows($result_person);
 			for  ($p=0; $p<$nr_person; $p++)
 			{
 				$row=pg_fetch_object($result_person, $p);
-				fwrite($fp, '#'.$row->nachname.' '.$row->vornamen.$crlf.$row->uid.$crlf);
+				fwrite($fp, '#'.$row->nachname.' '.$row->vorname.$crlf.$row->uid.$crlf);
 			}
 			fclose($fp);
 			echo $name.' created<br>';
@@ -77,16 +79,16 @@
 	}
 	
 	//$qry = "SELECT vornamen, nachname, uid, alias FROM tbl_person where alias<>'' ORDER BY nachname, vornamen";
-	$qry = "SELECT vornamen, nachname, uid, alias FROM tbl_person LEFT JOIN tbl_student using(uid) 
+	$qry = "SELECT vorname, nachname, uid, alias FROM (public.tbl_person JOIN public.tbl_benutzer USING(person_id)) LEFT JOIN tbl_student on(uid=student_uid) 
 	        WHERE alias<>'' AND (studiengang_kz NOT IN($noalias_kz) OR studiengang_kz is null) 
-	        ORDER BY nachname, vornamen";
+	        ORDER BY nachname, vorname";
 	
 	if($result = pg_query($conn, $qry))
 	{
 		$fp=fopen('../../../../mlists/tw_alias.txt',"w");
 		while($row=pg_fetch_object($result))
 		{
-			fwrite($fp,"# ".$row->nachname." ".$row->vornamen.$crlf);
+			fwrite($fp,"# ".$row->nachname." ".$row->vorname.$crlf);
 			fwrite($fp,$row->alias.": ".$row->uid.$crlf);			
 		}
 		fclose($fp);
