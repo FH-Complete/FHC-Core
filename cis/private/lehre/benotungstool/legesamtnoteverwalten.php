6efcbf36f641edaec288e6a4bfea439d50f620d0
Gerald Raab <raab@technikum-wien.at>
2007-09-04 14:47:40 +0000

    ie-cache-bug

--- cis/private/lehre/benotungstool/legesamtnoteverwalten.php
+++ cis/private/lehre/benotungstool/legesamtnoteverwalten.php
@@ -32,6 +32,7 @@ require_once('../../../../include/beispiel.class.php');
 require_once('../../../../include/studentnote.class.php');
 require_once('../../../../include/datum.class.php');
 require_once('../../../../include/legesamtnote.class.php');
+$ts = time();
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -89,7 +90,7 @@ require_once('../../../../include/legesamtnote.class.php');
 		erzeugeAnfrage(); 
 	    //note = document.getElementById(uid).note.value;
 	    stud_uid = uid;
-	    var url= '<?php echo "legesamtnoteeintragen.php?lvid=$lvid&lehreinheit_id=$lehreinheit_id&stsem=$stsem"; ?>';
+	    var url= '<?php echo "legesamtnoteeintragen.php?lvid=$lvid&lehreinheit_id=$lehreinheit_id&stsem=$stsem&$ts"; ?>';
 	    url += '&submit=1&student_uid='+uid+"&note="+note;
 	    anfrage.open("GET", url, true);
 	    anfrage.onreadystatechange = updateSeite;
