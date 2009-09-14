c34147f93aa39f828c7e3bf5edde85c0f97996c3
Andreas Österreicher <oesi@technikum-wien.at>
2009-09-14 09:42:25 +0000

    - Bugfix beim CIS-Bildupload - Encoding Fix bei LV-Plan Hilfe

--- cis/private/bildupload.php
+++ cis/private/bildupload.php
@@ -31,7 +31,12 @@ require_once('../../include/benutzer.class.php');
 require_once('../../include/akte.class.php');
 
 $PHP_SELF = $_SERVER['PHP_SELF'];
-echo "<html><body>";
+echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>';
 
 function resize($filename, $width, $height)
 {
