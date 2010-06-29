3b2ee7794437c402634e46664dbfd91fcc133aa3
Andreas Österreicher <oesi@technikum-wien.at>
2010-06-29 15:29:47 +0000

    Vilesci Frames Resizeable

--- vilesci/index.php
+++ vilesci/index.php
@@ -1,15 +1,15 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/frameset.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN" "http://www.w3.org/TR/html4/frameset.dtd">
 <html>
 <head>
 	<title>VileSci</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" href="../skin/vilesci.css" type="text/css" />
 </head>
-<frameset rows="50px,*" framespacing="1" border="0"/>
+<frameset rows="49px,*" framespacing="1" border="1">
   	<frame src="top.php" id="top" name="top" frameborder="0" scrolling="No"/>
-  	<frameset cols="18%,*" frameborder="0" />
-  		<frame src="left.php" id="nav" name="nav" frameborder="0" />
-  		<frame src="main.php" id="main" name="main" frameborder="0" />
+  	<frameset cols="200px,*" >
+  		<frame src="left.php" id="nav" name="nav" />
+  		<frame src="main.php" id="main" name="main" />
   	</frameset>
 	<noframes>
 		<body bgcolor="#FFFFFF">
