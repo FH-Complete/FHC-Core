fc26ed03285e8863227db5e4d44bedf943efe38b
Karl Burkhart <burkhart@technikum-wien.at>
2011-06-27 14:38:44 +0000

    mit phrasenmodul übersetzt

--- cis/private/lvplan/raumsuche.php
+++ cis/private/lvplan/raumsuche.php
@@ -24,6 +24,7 @@ require_once('../../../config/cis.config.inc.php');
 require_once('../../../include/ort.class.php');
 require_once('../../../include/raumtyp.class.php');
 require_once('../../../include/datum.class.php');
+require_once('../../../include/phrasen.class.php'); 
 
 $datum = (isset($_POST['datum'])?$_POST['datum']:date('d.m.Y'));
 $vonzeit = (isset($_POST['vonzeit'])?$_POST['vonzeit']:date('H:i'));
@@ -33,36 +34,42 @@ $anzahlpersonen = (isset($_POST['anzahlpersonen'])?$_POST['anzahlpersonen']:'0')
 $sent = true; //isset($_POST['sent']);
 $datum_obj = new datum();
 
+if(isset($_GET['lang']))
+	setSprache($_GET['lang']);
+	
+$sprache = getSprache(); 	
+$p = new phrasen($sprache);
+
 echo '
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Reservierungsliste</title>
+	<title>'.$p->t('global/reservierungsliste').'</title>
 	<link rel="stylesheet" href="../../../skin/style.css.php" type="text/css">
 	<script language="Javascript">
 	function checkdata()
 	{
 		if(document.getElementById("datum").value=="")
 		{
-			alert("Es muss ein Datum eingegeben werden");
+			alert("'.$p->t('global/datumAngeben').'");
 			return false;
 		}
 		datum = document.getElementById("datum").value
 		if(datum.length!=10)
 		{
-			alert("Das angegebene Datum ist ungueltig! Bitte geben Sie das Datum im Format dd.mm.YYYY (31.12.2008) ein");
+			alert("'.$p->t('global/datumUngueltig').'");
 			return false;
 		}
 		
 		if(document.getElementById("vonzeit").value=="")
 		{
-			alert("VON-Zeit muss eingegeben werden");
+			alert("'.$p->t('global/vonZeitEingeben').'");
 			return false;
 		}
 				
 		if(document.getElementById("biszeit").value=="")
 		{
-			alert("BIS-Zeit muss eingegeben werden");
+			alert("'.$p->t('global/bisZeitEingeben').'");
 			return false;
 		}
 		return true;
@@ -72,7 +79,7 @@ echo '
 <body id="inhalt">
 	<H2><table class="tabcontent">
 		<tr>
-		<td>&nbsp;<a class="Item" href="index.php">Lehrveranstaltungsplan</a> &gt;&gt; Raumsuche</td>
+		<td>&nbsp;<a class="Item" href="index.php">'.$p->t('global/lehrveranstaltungsplan').'</a> &gt;&gt; '.$p->t('global/raumsuche').'</td>
 		<td align="right"></td>
 		</tr>
 		</table>
@@ -80,11 +87,11 @@ echo '
 ';
 
 echo '<form action="'.$_SERVER['PHP_SELF'].'" method="POST" onsubmit="return checkdata()">
-		Datum* <input type="text" name="datum" id="datum" size="10" value="'.$datum.'">
-		Von* <input type="text" name="vonzeit" id="vonzeit" size="5" value="'.$vonzeit.'">
-		Bis* <input type="text" name="biszeit" id="biszeit" size="5" value="'.$biszeit.'">
-		Raumtyp: <SELECT name="raumtyp">
-		<OPTION value="">Alle</OPTION>';
+		'.$p->t('global/datum').'* <input type="text" name="datum" id="datum" size="10" value="'.$datum.'">
+		'.$p->t('global/von').'* <input type="text" name="vonzeit" id="vonzeit" size="5" value="'.$vonzeit.'">
+		'.$p->t('global/bis').'* <input type="text" name="biszeit" id="biszeit" size="5" value="'.$biszeit.'">
+		'.$p->t('global/raumtyp').': <SELECT name="raumtyp">
+		<OPTION value="">'.$p->t('global/alle').'</OPTION>';
 $raumtyp_obj = new raumtyp();
 $raumtyp_obj->getAll();
 
@@ -98,25 +105,25 @@ foreach ($raumtyp_obj->result as $row)
 	echo '<OPTION value="'.$row->raumtyp_kurzbz.'" '.$selected.'>'.$row->raumtyp_kurzbz.'</OPTION>';
 }
 echo '	</SELECT>
-		Anzahl Personen <input type="text" size="3" name="anzahlpersonen" value="'.$anzahlpersonen.'">
-		<input type="submit" name="sent" value="Suchen" />
+		'.$p->t('global/anzahlPersonen').' <input type="text" size="3" name="anzahlpersonen" value="'.$anzahlpersonen.'">
+		<input type="submit" name="sent" value="'.$p->t('global/suchen').'" />
 	  </form>';
 if($sent)
 {
 	$error=false;
 	if($datum=='')
 	{
-		echo "<br>Es muss ein Datum angegeben werden";
+		echo "<br>".$p->t('global/datumAngeben');
 		$error = true;
 	}
 	if($vonzeit=='')
 	{
-		echo "<br>VON-Zeit muss angegeben werden";
+		echo "<br>".$p->t('global/vonZeitEingeben');
 		$error = true;
 	}
 	if($biszeit=='')
 	{
-		echo "<br>BIS-Zeit muss angegeben werden";
+		echo "<br>".$p->t('global/bisZeitEingeben');
 		$error = true;
 	}
 	
@@ -125,20 +132,20 @@ if($sent)
 		//Von Zeit pruefen
 		if(!preg_match('/^[0-9]{2}:[0-9]{2}$/', $vonzeit))
 		{
-			echo "<br>VON-Zeit muss im Format hh:mm (12:30) angegeben werden";
+			echo "<br>".$p->t('global/toZeitFormat');
 			$error = true;
 		}
 		//Bis Zeit pruefen
 		if(!preg_match('/^[0-9]{2}:[0-9]{2}$/', $biszeit))
 		{
-			echo "<br>BIS-Zeit muss im Format hh:mm (12:30) angegeben werden";
+			echo "<br>".$p->t('global/bisZeitFormat');
 			$error = true;
 		}
 		
 		//Datum pruefen
 		if(!$datum_obj->checkDatum($datum))
 		{
-			echo "<br>Das angegebene Datum ist ungueltig! Bitte geben Sie das Datum im Format dd.mm.YYYY (31.12.2008) ein";
+			echo "<br>".$p->t('global/datumUngueltig');
 			$error = true;
 		}
 	}
@@ -152,18 +159,18 @@ if($sent)
 		else 
 		{
 			echo '<br><table>';
-			echo '<tr class="liste"><th>Raum</th><th>Bezeichnung</th><th>Nummer</th><th>Personen</th><th>Aktion</th></tr>';
+			echo '<tr class="liste"><th>'.$p->t('global/raum').'</th><th>'.$p->t('global/bezeichnung').'</th><th>'.$p->t('global/nummer').'</th><th>'.$p->t('global/personen').'</th><th>'.$p->t('global/aktion').'</th></tr>';
 			$i=0;
 			$datum_sec = $datum_obj->mktime_datum($datum)-1;
 			foreach ($ort->result as $row)
 			{
 				$i++;
 				echo '<tr class="liste'.($i%2).'">';
-				echo '<td><a href="'.RAUMINFO_PATH.$row->ort_kurzbz.'.html" class="Item" title="Rauminfo anzeigen">'.$row->ort_kurzbz.'</td>';
+				echo '<td><a href="'.RAUMINFO_PATH.$row->ort_kurzbz.'.html" class="Item" title="'.$p->t('global/rauminfoAnzeigen').'">'.$row->ort_kurzbz.'</td>';
 				echo "<td>$row->bezeichnung</td>";
 				echo "<td>$row->planbezeichnung</td>";
 				echo "<td>$row->max_person</td>";
-				echo "<td><a href='stpl_week.php?type=ort&ort_kurzbz=$row->ort_kurzbz&datum=".$datum_sec."' class='Item'>zur Reservierung</a></td>";
+				echo "<td><a href='stpl_week.php?type=ort&ort_kurzbz=$row->ort_kurzbz&datum=".$datum_sec."' class='Item'>".$p->t('global/zurReservierung')."</a></td>";
 				echo '</tr>';
 				flush();
 			}
