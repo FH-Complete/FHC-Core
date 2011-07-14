29a4817537318980d4654f25f6e5c0140b84aa73
Manfred Kindl <kindlm@technikum-wien.at>
2011-07-14 09:07:09 +0000

    Datei "Kommunikation" in "Personensuche" umbenannt

---
+++ locale/en-US/personensuche.php
@@ -0,0 +1,8 @@
+<?php
+$this->phrasen['kommunikation/sucheNach']='Search for';
+$this->phrasen['kommunikation/allePersonen']='All personen';
+$this->phrasen['kommunikation/mitarbeiterIn']='Employee';
+$this->phrasen['kommunikation/mitarbeiterInFix']='Fix employee';
+$this->phrasen['kommunikation/mitarbeiterInExtern']='External Employee';
+$this->phrasen['kommunikation/studentIn']='Student';
+?>
