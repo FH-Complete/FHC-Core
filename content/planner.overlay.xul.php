5bab4c7ae52924c6c0cf1acb8f6e05c33b5333c6
Karl Burkhart <burkhart@technikum-wien.at>
2011-09-07 15:00:49 +0000

    im ressourcemenü Spalte Typ hinzugefügt

--- content/planner.overlay.xul.php
+++ content/planner.overlay.xul.php
@@ -147,6 +147,8 @@ echo '<?xul-overlay href="'.APP_ROOT.'content/projekt/gantt.overlay.xul.php"?>';
 	<treecols>
 	    <treecol id="treecol-ressourcemenue-description" label="Anzeige" flex="2"  primary="true"/>
 	    <splitter class="tree-splitter"/>
+	   	<treecol id="treecol-ressourcemenue-typ" label="typ" flex="5" hidden ="true" />
+	    <splitter class="tree-splitter"/>
 	    <treecol id="treecol-ressourcemenue-ressource_id" label="ID" flex="5" hidden ="true" />
 	    <splitter class="tree-splitter"/>
 	    <treecol id="treecol-ressourcemenue-bezeichnung" label="Bezeichnung" flex="2" hidden="true" />
@@ -168,6 +170,7 @@ echo '<?xul-overlay href="'.APP_ROOT.'content/projekt/gantt.overlay.xul.php"?>';
 	       <treeitem uri="rdf:*">
 	         <treerow>
 	           <treecell label="rdf:http://www.technikum-wien.at/ressource/rdf#rdf_description"/>
+	           <treecell label="rdf:http://www.technikum-wien.at/ressource/rdf#typ"/>
 	           <treecell label="rdf:http://www.technikum-wien.at/ressource/rdf#ressource_id"/>
 	           <treecell label="rdf:http://www.technikum-wien.at/ressource/rdf#bezeichnung"/>
 	           <treecell label="rdf:http://www.technikum-wien.at/ressource/rdf#beschreibung"/>
