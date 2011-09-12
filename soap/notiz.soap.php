7151fd85c0dae0d858c4b5caf629b7c1c068acbd
Andreas Österreicher <oesi@technikum-wien.at>
2011-09-12 15:11:27 +0000

    Notizen editieren und neu Anlegen

---
+++ soap/notiz.soap.php
@@ -0,0 +1,137 @@
+<?php
+/* Copyright (C) 2010 Technikum-Wien
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Authors: Christian Paminger <christian.paminger@technikum-wien.at>,
+ *          Andreas Oesterreicher <andreas.oesterreicher@technikum-wien.at> and
+ *          Karl Burkhart <burkhart@technikum-wien.at>.
+ */
+header("Cache-Control: no-cache");
+header("Cache-Control: post-check=0, pre-check=0",false);
+header("Expires Mon, 26 Jul 1997 05:00:00 GMT");
+header("Pragma: no-cache");
+
+require_once('../config/vilesci.config.inc.php'); 
+require_once('../include/notiz.class.php');
+require_once('../include/datum.class.php');
+require_once('../include/functions.inc.php');
+require_once('../include/benutzerberechtigung.class.php');
+
+$SOAPServer = new SoapServer(APP_ROOT."/soap/notiz.wsdl.php?".microtime());
+$SOAPServer->addFunction("saveNotiz");
+$SOAPServer->addFunction("deleteNotiz");
+$SOAPServer->handle();
+
+// WSDL Chache auf aus
+ini_set("soap.wsdl_cache_enabled", "0");
+
+/**
+ * 
+ * Speichert die vom Webservice übergebenen Parameter in die DB
+ * @param string $notiz_id
+ * @param string $titel
+ * @param string $text
+ * @param string $verfasser_uid
+ * @param string $bearbeiter_uid
+ * @param string $start
+ * @param string $ende
+ * @param string $projekt_kurzbz
+ * @param string $projektphase_id
+ * @param string $projekttask_id
+ * @param string $uid
+ * @param string $person_id
+ * @param string $prestudent_id
+ * @param string $bestellung_id
+ */
+function saveNotiz($notiz_id, $titel, $text, $verfasser_uid, $bearbeiter_uid, $start, $ende, $projekt_kurzbz, $projektphase_id, $projekttask_id, $uid, $person_id, $prestudent_id, $bestellung_id)
+{ 	
+	$user = get_uid();
+		
+	$rechte = new benutzerberechtigung();
+	$rechte->getBerechtigungen($user);
+		
+	if(!$rechte->isBerechtigt('basis/notiz', null, 'sui'))
+		return new SoapFault("Server", "Sie haben keine Berechtigung zum Speichern von Notizen");
+	
+	$notiz = new notiz();
+	if($notiz_id != '')
+	{
+		if($notiz->load($notiz_id))
+		{
+			$notiz->new = false;
+		}
+		else
+			return new SoapFault("Server", "Fehler beim Laden"); 
+	}
+	else
+	{
+		$notiz->new=true;
+		$notiz->insertvon = $user;
+		$notiz->insertamum = date('Y-m-d H:i:s');
+	}
+
+	$notiz->titel=$titel;
+	$notiz->text=$text;
+	$notiz->verfasser_uid = $verfasser_uid;
+	$notiz->bearbeiter_uid = $bearbeiter_uid;
+	$notiz->start = $start;
+	$notiz->ende = $ende;
+	
+	if($notiz->save())
+	{
+		if($notiz->new)
+		{
+			$notiz->projekt_kurzbz = $projekt_kurzbz;
+			$notiz->projektphase_id = $projektphase_id;
+			$notiz->projekttask_id = $projekttask_id;
+			$notiz->uid = $uid;
+			$notiz->person_id = $person_id;
+			$notiz->prestudent_id = $prestudent_id;
+			$notiz->bestellung_id = $bestellung_id;
+			
+			if(!$notiz->saveZuordnung())
+				return new SoapFault("Server", $notiz->errormsg);
+		}		
+		return $notiz->notiz_id;
+	} 
+	else
+		return new SoapFault("Server", $notiz->errormsg);
+}
+
+/**
+ * 
+ * Löscht die Notiz mit der vom Webservice übergebenen ID 
+ * @param $notiz_id
+ */
+function deleteNotiz($notiz_id)
+{
+	$user = get_uid();
+		
+	$rechte = new benutzerberechtigung();
+	$rechte->getBerechtigungen($user);
+		
+	if(!$rechte->isBerechtigt('basis/notiz', null, 'suid'))
+		return new SoapFault("Server", "Sie haben keine Berechtigung zum Speichern von Notizen");
+	
+	$notiz = new notiz();
+	if($notiz->delete($notiz_id))
+		return "OK";
+	else
+		return new SoapFault("Server", $projekttask->errormsg);
+}
+?>
+
+
---
+++ soap/notiz.wsdl.php
@@ -0,0 +1,81 @@
+<?php 
+require_once('../config/vilesci.config.inc.php');
+header("Content-type: text/plain");
+echo "<?xml version='1.0' encoding='utf-8' ?>";
+?>
+
+<wsdl:definitions name="Notiz" 
+targetNamespace="http://localhost/soap/" 
+xmlns:wsdl="http://schemas.xmlsoap.org/wsdl/" 
+xmlns:tns="http://localhost/soap/" 
+xmlns:xsd="http://www.w3.org/2001/XMLSchema"    
+xmlns:xsd1="http://localhost/soap/notiz.xsd"    
+xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/">
+
+	<wsdl:message name="saveNotizRequest">
+		<wsdl:part name="notiz_id" type="xsd:string"></wsdl:part>
+		<wsdl:part name="titel" type="xsd:string"></wsdl:part>
+		<wsdl:part name="text" type="xsd:string"></wsdl:part>
+		<wsdl:part name="verfasser_uid" type="xsd:string"></wsdl:part>
+		<wsdl:part name="bearbeiter_uid" type="xsd:string"></wsdl:part>
+		<wsdl:part name="start" type="xsd:string"></wsdl:part>
+		<wsdl:part name="ende" type="xsd:string"></wsdl:part>
+		<wsdl:part name="projekt_kurzbz" type="xsd:string"></wsdl:part>
+		<wsdl:part name="projektphase_id" type="xsd:string"></wsdl:part>
+		<wsdl:part name="projekttask_id" type="xsd:string"></wsdl:part>
+		<wsdl:part name="uid" type="xsd:string"></wsdl:part>
+		<wsdl:part name="person_id" type="xsd:string"></wsdl:part>
+		<wsdl:part name="prestudent_id" type="xsd:string"></wsdl:part>
+		<wsdl:part name="bestellung_id" type="xsd:string"></wsdl:part>
+    </wsdl:message>
+
+ 	<wsdl:message name="saveNotizResponse">
+       <wsdl:part name="message" type="xsd:string"></wsdl:part>
+  	</wsdl:message>
+  	
+  	<wsdl:message name="deleteNotizRequest">
+  		<wsdl:part name="notiz_id" type="xsd:string"></wsdl:part>
+  	</wsdl:message>
+    <wsdl:message name="deleteNotizResponse">
+  		<wsdl:part name="message" type="xsd:string"></wsdl:part>
+  	</wsdl:message>
+ 
+ <wsdl:portType name="ConfigPortType" >
+       <wsdl:operation name="saveNotiz">
+           <wsdl:input message="tns:saveNotizRequest"></wsdl:input>
+           <wsdl:output message="tns:saveNotizResponse"></wsdl:output>        
+       </wsdl:operation>
+       <wsdl:operation name="deleteNotiz">
+           <wsdl:input message="tns:deleteNotizRequest"></wsdl:input>
+           <wsdl:output message="tns:deleteNotizResponse"></wsdl:output>        
+       </wsdl:operation>
+   </wsdl:portType>
+
+   <wsdl:binding name="ConfigBinding" type="tns:ConfigPortType">
+       <soap:binding style="rpc" transport="http://schemas.xmlsoap.org/soap/http" />
+       <wsdl:operation name="saveNotiz">
+           <soap:operation soapAction="<?php echo APP_ROOT."soap/saveNotiz";?>" />
+           <wsdl:input> 
+               <soap:body use="encoded" namespace="http://localhost/soap/" encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" />
+           </wsdl:input>
+           <wsdl:output>
+               <soap:body use="encoded" namespace="http://localhost/soap/" encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"/>
+           </wsdl:output>
+       </wsdl:operation> 
+       <wsdl:operation name="deleteNotiz">
+           <soap:operation soapAction="<?php echo APP_ROOT."soap/deleteNotiz";?>" />
+           <wsdl:input> 
+               <soap:body use="encoded" namespace="http://localhost/soap/" encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" />
+           </wsdl:input>
+           <wsdl:output>
+               <soap:body use="encoded" namespace="http://localhost/soap/" encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"/>
+           </wsdl:output>
+       </wsdl:operation> 
+   </wsdl:binding>  
+ 
+   <wsdl:service name="Notiz">
+       <wsdl:port name="ConfigWebservicePort" binding="tns:ConfigBinding">
+           <soap:address location="<?php echo APP_ROOT."soap/notiz.soap.php";?>"/>
+       </wsdl:port>
+   </wsdl:service>
+</wsdl:definitions>
