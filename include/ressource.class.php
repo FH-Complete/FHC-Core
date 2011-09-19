9d3421dff49f54a16e0807ffcdd57651cc24b388
Karl Burkhart <burkhart@technikum-wien.at>
2011-09-19 14:50:59 +0000

    kleinen Fehler bei Abfrage ausgebessert

--- include/ressource.class.php
+++ include/ressource.class.php
@@ -145,7 +145,7 @@ class ressource extends basis_db
 	public function getProjectRessourcen($project_kurzbz)
 	{
 		$qry = "SELECT ressource.* FROM fue.tbl_ressource as ressource
-		JOIN fue.tbl_projekt_ressource project ON(project.projekt_ressource_id = ressource.ressource_id) 
+		JOIN fue.tbl_projekt_ressource project ON(project.ressource_id = ressource.ressource_id) 
 		WHERE project.projekt_kurzbz ='".addslashes($project_kurzbz)."';";
 		
 		$this->result=array();
@@ -188,7 +188,7 @@ class ressource extends basis_db
 	public function getPhaseRessourcen($projektphase_id)
 	{
 		$qry = "SELECT ressource.* FROM fue.tbl_ressource as ressource
-		JOIN fue.tbl_projekt_ressource project ON(project.projekt_ressource_id = ressource.ressource_id) 
+		JOIN fue.tbl_projekt_ressource project ON(projectressource_id = ressource.ressource_id) 
 		WHERE project.projektphase_id ='".addslashes($projektphase_id)."';";
 		
 		$this->result=array();
