6b99764af5385b77cba473290653360ea1121029
Karl Burkhart <burkhart@technikum-wien.at>
2011-11-07 14:47:40 +0000

    Webservice zum Onlinekauf des Semestertickets

---
+++ soap/semesterticket_client.php
@@ -0,0 +1,82 @@
+<?php 
+require_once('../config/vilesci.config.inc.php');
+require_once('stip.class.php'); 
+?>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<script type="text/javascript" src="../include/js/jqSOAPClient.js"></script> 
+		<script type="text/javascript" src="../include/js/jquery.js"></script> 
+		<title>STIP-Client</title>
+	</head>
+	<body>
+
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+		  <table border="0" cellpadding="5" cellspacing="0" bgcolor="#E0E0E0">
+		    <tr>
+		      <td align="right">token:</td>
+		      <td><input name="token" type="text" size="30" maxlength="15" value="<?php echo (isset($_REQUEST['token']) ? $_REQUEST['token'] : "");?>"></td>
+		    </tr>
+		    <tr>
+		      <td align="right">Matrikelnummer:</td>
+		      <td><input name="matrikelnummer" type="text" size="30" maxlength="15" value="<?php echo (isset($_REQUEST['matrikelnummer']) ? $_REQUEST['matrikelnummer'] : "");?>"></td>
+		    </tr>
+		    <tr>
+		      <td align="right">Familienname:</td>
+		      <td><input name="name" type="text" size="30" maxlength="255" value="<?php echo (isset($_REQUEST['familienname']) ? $_REQUEST['familienname'] : "");?>"></td>
+		    </tr>
+		        <tr>
+		      <td align="right">Vorname:</td>
+		      <td><input name="vorname" type="text" size="30" maxlength="255" value="<?php echo (isset($_REQUEST['vorname']) ? $_REQUEST['vorname'] : "");?>"></td>
+		    </tr>
+		  	<tr>
+		      <td align="right">Geburtsdatum:</td>
+		      <td><input name="geburtsdatum" type="text" size="30" maxlength="10" value="<?php echo (isset($_REQUEST['geburtsdatum']) ? $_REQUEST['geburtsdatum'] : "");?>"></td>
+		    </tr>
+		   <tr>
+		      <td align="right">Postleitzahl:</td>
+		      <td><input name="postleitzahl" type="text" size="30" maxlength="10" value="<?php echo (isset($_REQUEST['plz']) ? $_REQUEST['plz'] : "");?>"></td>
+		    </tr>
+		        <tr>
+		      <td align="right">Semesterkuerzel:</td>
+		      <td><input name="semesterkuerzel" type="text" size="30" maxlength="3" value="<?php echo (isset($_REQUEST['semester']) ? $_REQUEST['semester'] : "11W");?>"></td>
+		    </tr>
+		    <tr>
+		      <td align="right"></td>
+		      <td>
+		        <input type="submit" value=" Absenden " name="submit">
+		        <input type="button" onclick="sendSoap();" value="send Soap">
+		      </td>
+		    </tr>
+		</table>
+		</form>
+
+
+<?php 
+
+if(isset($_REQUEST['submit']))
+{
+	$client = new SoapClient("http://calva.technikum-wien.at/burkhart/fhcomplete/trunk/soap/semesterticket.wsdl.php?".microtime()); 
+	
+	try
+	{
+		class foo {}; 
+		$obj = new foo(); 
+		$obj->token = $_REQUEST['token'];
+		$obj->matrikelnummer = $_REQUEST['matrikelnummer'];
+		$obj->name = $_REQUEST['name'];
+		$obj->vorname = $_REQUEST['vorname'];
+		$obj->geburtsdatum = $_REQUEST['geburtsdatum'];
+		$obj->postleitzahl = $_REQUEST['postleitzahl'];
+		$obj->semesterkuerzel = $_REQUEST['semesterkuerzel'];
+		$response = $client->verifyData($obj);
+		var_dump($response);
+	}
+	catch(SoapFault $fault) 
+	{
+    	echo "SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR;
+	}
+	 
+}
+
+?>
