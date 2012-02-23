831ba4a490409d0741e4921a1af2c48699dd4858
Karl Burkhart <burkhart@technikum-wien.at>
2012-02-23 09:46:04 +0000

    Authentifizierung ist bei Aufruf der Seite erforderlich

--- soap/stip_client.php
+++ soap/stip_client.php
@@ -1,6 +1,10 @@
 <?php 
 require_once('../config/vilesci.config.inc.php');
+require_once('../include/functions.inc.php');
 require_once('stip.class.php'); 
+$getuid=get_uid();
+if(!check_lektor($getuid))
+	die('Sie haben keine Berechtigung für diese Seite.');
 ?>
 <html>
 	<head>
