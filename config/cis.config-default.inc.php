a12e36246eb89f53354726c2a16777d04ee87616
Andreas Österreicher <oesi@technikum-wien.at>
2012-05-24 08:40:46 +0000

    Cleanup CIS Default Config

--- config/cis.config-default.inc.php
+++ config/cis.config-default.inc.php
@@ -1,59 +1,65 @@
 <?php
+/**
+ * Vorlage fuer CIS Konfigurationsdatei
+ * Diese Datei muss auf cis.config.inc.php kopiert werden
+ */
+// Error reporting
 ini_set('display_errors','1');
 error_reporting(E_ALL);
 
+// Encoding
 mb_internal_encoding("UTF-8");
 mb_regex_encoding("UTF-8");
 setlocale (LC_ALL, 'de_DE.UTF8','de_DE@euro', 'de_DE', 'de','DE', 'ge','German');
+
+// Zeitzone
 date_default_timezone_set('Europe/Vienna');
 
 // Connection Strings zur Datenbank
 define("DB_SYSTEM","pgsql");
 define("DB_HOST","localhost");
-define("DB_PORT","5433");
+define("DB_PORT","5432");
 define("DB_NAME","fhcomplete");
 define("DB_USER","bla");
 define("DB_PASSWORD","bla");
 define("DB_CONNECT_PERSISTENT",TRUE);
 define('CONN_CLIENT_ENCODING','UTF-8' );
 
-define("CONN_STRING_MOODLE","host=localhost dbname=bla user=bla password=bla");
+// Pfad fuer Logfiles
+define('LOG_PATH','/var/fhcomplete/log/');
 
-define('TABLE_ID','_id');
-define('TABLE_BEGIN','tbl_');
-define('VIEW_BEGIN','vw_');
+// Name des Servers (benoetigt fuer Cronjobs)
+define('SERVER_NAME','www.technikum-wien.at');
 
-//Pfad zum Logfile
-define('LOG_PATH','/var/www/system/');
+// Ablagepfad der hochgeladenen Benotungstool Dokumente
+define('BENOTUNGSTOOL_PATH','/var/fhcomplete/documents/benotungstool/');
+// Ablagepfad der Dokumente des DMS (sollte ausserhalb von htdocs liegen)
+define('DMS_PATH','/var/fhcomplete/documents/dms/');
+// Import Verzeichnis fuer DMS
+define('IMPORT_PATH','/var/fhcomplete/documents/import/');
+// Ablagepfad der hochgeladenen Projektarbeiten (sollte ausserhalb von htdocs liegen)
+define('PAABGABE_PATH','/var/fhcomplete/documents/paabgabe/');
 
-//Pfad zum Moodle
-define('MOODLE_PATH','http://dav.technikum-wien.at/oesi/portal/trunk/moodle/');
-//Moodle verwenden Ja/Nein
-define('MOODLE',true);
+// Pfad zu den Rauminfos
+define('RAUMINFO_PATH','/var/www/rauminfos/');
 
-//Name des Servers (benoetigt fuer Cronjobs
-define('SERVER_NAME','cis.technikum-wien.at');
-	
-//Pfad zum Upload-Ordner
-define('BENOTUNGSTOOL_PATH','/websites/portal/trunk/documents/benotungstool/');
-//Pfad zu Verzeichnis fuer Dokumentenmanagementsystem (sollte ausserhalb von htdocs liegen)
-define('DMS_PATH','/var/documents/');
-
-// XML fuer XSL-Vorlagen
-define('XML_ROOT','http://dav.technikum-wien.at/oesi/portal/trunk/rdf/');
-define('APP_ROOT','http://dav.technikum-wien.at/oesi/portal/trunk/');
-define('DOC_ROOT','/var/www/cis/htdocs');
+// URL zu RDF Verzeichnis
+define('XML_ROOT','http://www.technikum-wien.at/rdf/');
+// URL zu Application Root
+define('APP_ROOT','http://www.technikum-wien.at/');
+// Pfad zu Document Root
+define('DOC_ROOT','/var/www/');
 
 // Externe Funktionen - Unterordner im Include-Verzeichnis
 define('EXT_FKT_PATH','tw');
 
-//ID des CMS-Contents bei dem das CIS Menue beginnt 
+// ID des CMS-Contents bei dem das CIS Menue beginnt 
 define('CIS_MENU_ENTRY_CONTENT',1);
 
-//Zusaetzliches Menue bei Lehrveranstaltungen anzeigen (Lektorenbereich, Info u. Kommunikation, etc)
+// Zusaetzliche Links bei News anzeigen (Lehrziele, Allg. Download, Newsgroups)
 define('CIS_EXT_MENU',true);
 
-//Legt fest ob bei den Zeitsperren der Bereich fuer die Resturlaubstage angezeigt wird
+// Legt fest ob bei den Zeitsperren der Bereich fuer die Resturlaubstage angezeigt wird
 define('URLAUB_TOOLS',true);
 
 // LDAP_SERVER: Adresse des LDAP Servers
@@ -69,65 +75,71 @@ define('MAILVERTEILER_SPERRE', true);
 // Bezeichnung des Campus
 define('CAMPUS_NAME','FH Technikum Wien');
 
-// MAXNEWSALTER: beinhaltet die Anzahl der Tag die eine Nachricht am Pinboard angezeigt wird.
+// Anzahl der Tag die eine Nachricht am Pinboard angezeigt wird.
 define("MAXNEWSALTER",60);
-// MAXNEWS: Anzahl der Newseintraege die maximal angezeigt werden
+// Anzahl der Newseintraege die maximal angezeigt werden
 define('MAXNEWS', 99);
 
-//legt fest wie die Attribute in der Tabelle Sprache heissen (benoetigt fuer LVinfo)
+// legt fest wie die Attribute in der Tabelle Sprache heissen (benoetigt fuer LVinfo)
 define("ATTR_SPRACHE_DE","German");
 define("ATTR_SPRACHE_EN","English");
 
-// Version des aktuellen Stundenplans
-define('VERSION','7.1 vom 9.9.2006');
-
-//Konstanten fuer die Reservierung
-define('RES_TAGE_STUDENT','1');
+// Konstanten fuer die Reservierung
 // Tage ab wann ein Mitarbeiter reservieren kann.
 define('RES_TAGE_LEKTOR_MIN','5'); 
 // Datum bis wann im voraus ein Mitarbeiter reservieren kann.
-define('RES_TAGE_LEKTOR_BIS','2008-08-01'); 
+define('RES_TAGE_LEKTOR_BIS','2020-01-01'); 
 
-// Stundenplan
+// Wie viele Tage pro Woche werden im LVPlan angezeigt
 define('TAGE_PRO_WOCHE','7');
 
+// Kalenderkategorie beim Export des LVPlans
 define('LVPLAN_KATEGORIE', 'StundenplanTW');
 
-//Default Stylesheet
+// Default Stylesheet
 define('DEFAULT_STYLE','tw');
-//Layout Wechsel im CIS moeglich?
+// Layout Wechsel im CIS moeglich?
 define('CHOOSE_LAYOUT',false);
 
-//Default Sprache
+// Default Sprache
 define('DEFAULT_LANGUAGE','German');
 
-// MAIL Adressen
-define('MAIL_DEBUG','oesi@technikum-wien.at');
-define('MAIL_GST','pam@technikum-wien.at,oesi@technikum-wien.at');
-define('MAIL_ADMIN','vilesci@technikum-wien.at');
-define('MAIL_LVPLAN','pam@technikum-wien.at,lvplan@technikum-wien.at');
-define('MAIL_CIS','cis@technikum-wien.at');
-define('MAIL_INTERNATIONAL','erika.groh@technikum-wien.at');
-define('MAIL_INTERNATIONAL_OUTGOING', 'paivi.kumpukari-stascha@technikum-wien.at');
+// E-Mail Einstellungen
+// Wenn MAIL_DEBUG gesetzt ist, werden alle Mails an diese Adresse gesendet
+define('MAIL_DEBUG','invalid@technikum-wien.at');
+// Geschaeftsstelle / Personalabteilung
+define('MAIL_GST','invalid@technikum-wien.at');
+//Administrator
+define('MAIL_ADMIN','invalid@technikum-wien.at');
+//LVPlan-Stelle
+define('MAIL_LVPLAN','invalid@technikum-wien.at');
+//CIS Admin
+define('MAIL_CIS','invalid@technikum-wien.at');
+//Ansprechpartner fuer Incoming
+define('MAIL_INTERNATIONAL','invalid@technikum-wien.at');
+//Ansprechpartner fuer Outgoing
+define('MAIL_INTERNATIONAL_OUTGOING', 'invalid@technikum-wien.at');
 
-//OPUS
+// ***** MOODLE *****
+// Moodle verwenden Ja/Nein
+define('MOODLE',true);
+// Pfad zum Moodle
+define('MOODLE_PATH','http://www.technikum-wien.at/moodle/');
+// Connection String fuer Moodle Datenbank (nur Posgresql)
+define("CONN_STRING_MOODLE","host=localhost dbname=bla user=bla password=bla");
+
+// ***** OPUS *****
+// Angaben fuer OPUS Schnittstelle
 define('OPUS_SERVER','www.technikum-wien.at');
 define('OPUS_USER','bla');
 define('OPUS_PASSWD','bla');
 define('OPUS_DB','bla');
-// Projektabgabepfad
-define('PAABGABE_PATH','/var/www/htdocs/PaUpload/');
-//Pfad von PAAbgabe zum OPUS
-define('OPUS_PATH_PAA','/var/www/htdocs/opus/htdocs/volltexte/');
+// Pfad zu den Opus Volltexten
+define('OPUS_PATH_PAA','/var/www/opus/volltexte/');
 
-//Pfad zu den Rauminfos
-define('RAUMINFO_PATH','/documents/rauminfo/');
-
-//Default Sprache
-define('DEFAULT_LANGUAGE','German');
-
-//Dokumentemanagement-System
-define('DMS_PATH','/var/www/oesi/documents/');
-define('IMPORT_PATH','/var/www/import/'); 
+// ***** Nicht aendern *****
+define('TABLE_ID','_id');
+define('TABLE_BEGIN','tbl_');
+define('VIEW_BEGIN','vw_');
 
 ?>
