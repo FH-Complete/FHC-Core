0c20d2713fd0e43e3dc9f06786eb3f9ffe00417c
Andreas Österreicher <oesi@technikum-wien.at>
2012-08-07 13:38:28 +0000

    Horde Syncronisation nach LVPlan Update aktiviert

--- system/sync/sync_stpldev_stpl.php
+++ system/sync/sync_stpldev_stpl.php
@@ -662,7 +662,7 @@ if(count($users)>0)
 		fclose($fp);
 
 		//Horde Syncro starten
-		//exec('php5 '.DOC_ROOT.'../system/hordelvplansync/synchordelvplan.php lvplanupdate.txt >>/var/log/sync/synchordelvplan.log 2>&1');
+		exec('php5 '.DOC_ROOT.'../system/hordelvplansync/synchordelvplan.php lvplanupdate.txt >>/var/log/sync/synchordelvplan.log 2>&1');
 	}
 }
 // Mail an Admin
