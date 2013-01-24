154d0dbd8e4a481f25baaa01a0517b9b8a2b75db
Andreas Österreicher <oesi@technikum-wien.at>
2013-01-24 10:26:46 +0000

    Faxnummer aktualisiert

--- locale/de-AT/telefonverzeichnis.php
+++ locale/de-AT/telefonverzeichnis.php
@@ -3,4 +3,5 @@ $this->phrasen['telefonverzeichnis/titelTelefonverzeichnis']='Telefonverzeichnis
 $this->phrasen['telefonverzeichnis/hauptnummer']='Hauptnummer';
 $this->phrasen['telefonverzeichnis/durchwahl']='DW';
 $this->phrasen['telefonverzeichnis/faxnummern']='Faxnummern';
+$this->phrasen['telefonverzeichnis/faxnummernFuerMitarbeiter']='Faxnummern für Mitarbeiter: +43 1 333 40 77 - 99 &lt;Telefon DW&gt;';
 ?>
--- locale/en-US/telefonverzeichnis.php
+++ locale/en-US/telefonverzeichnis.php
@@ -3,4 +3,5 @@ $this->phrasen['telefonverzeichnis/titelTelefonverzeichnis']='Telephone list';
 $this->phrasen['telefonverzeichnis/hauptnummer']='Main number';
 $this->phrasen['telefonverzeichnis/durchwahl']='Extn.';
 $this->phrasen['telefonverzeichnis/faxnummern']='Fax numbers';
+$this->phrasen['telefonverzeichnis/faxnummernFuerMitarbeiter']='Fax numbers for employees: +43 1 333 40 77 - 99 &lt;Telephone Extn.&gt;';
 ?>
