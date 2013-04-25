e38b0b80998236bb8b7da92eab7a00901dd177df
Karl Burkhart <burkhart@technikum-wien.at>
2013-04-25 12:27:05 +0000

    xml daten für die erstellung des Ausbildungsvertrages

--- rdf/ausbildungsvertrag.xml.php
+++ rdf/ausbildungsvertrag.xml.php
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
  * Authors: Andreas Oesterreicher <andreas.oesterreicher@technikum-wien.at>
+ *          Karl Burkhart <burkhart@technikum-wien.at>
  */
 header("Content-type: application/xhtml+xml");
 require_once('../config/vilesci.config.inc.php');
@@ -25,15 +26,42 @@ require_once('../include/student.class.php');
 require_once('../include/prestudent.class.php');
 require_once('../include/adresse.class.php');
 
-$db = new basis_db(); 
-
-
 $uid_arr = (isset($_REQUEST['uid'])?$_REQUEST['uid']:null);
 
 $uid_arr = explode(";",$uid_arr);
 
 echo "<?xml version='1.0' encoding='UTF-8' standalone='yes'?>\n"; 
 echo "<ausbildungsvertraege>\n";
+
+$uid = isset($uid_arr[1])?$uid_arr[1]:$uid_arr[0];
+
+$student_help = new student(); 
+// an 2ter stelle da im Aufruf vom FAS ;<uid>; der erste immer '' ist
+if($student_help->load($uid))
+{
+    $studiengang = new studiengang();
+    $studiengang->load($student_help->studiengang_kz);
+    switch($studiengang->typ)
+    {
+        case 'b':
+            $studTyp = 'Bachelor'; 
+            $titel_kurzbz = 'BSc'; 
+            break; 
+        case 'm': 
+            $studTyp = 'Master'; 
+            $titel_kurzbz ='MSc'; 
+            break; 
+        case 'd':
+            $studTyp = 'Diplom'; 
+            break; 
+        default: 
+            $studTyp =''; 
+            $titel_kurzbz = ''; 
+    }
+    echo "\t<studiengang_typ>".$studTyp."</studiengang_typ>\n";
+    echo "\t<studiengang>".$studiengang->bezeichnung."</studiengang>\n";
+}
+
 foreach($uid_arr as $uid)
 {
 	if($uid=='')
@@ -50,9 +78,9 @@ foreach($uid_arr as $uid)
 			$studiengang->load($student->studiengang_kz);
 			
 			
-			
+			echo "\t\t<quote>1</quote>\n"; 
 			echo "\t\t<anrede>".$student->anrede."</anrede>\n";
-			echo "\t\t<vorname>".$student->vorname."</vorname>\n";
+			echo "\t\t<vorname>".$student->vorname." ".$student->vornamen."</vorname>\n";
 			echo "\t\t<vornamen>".$student->vornamen."</vornamen>\n";
 			echo "\t\t<nachname>".$student->nachname."</nachname>\n";
 			echo "\t\t<titelpre>".$student->titelpre."</titelpre>\n";
@@ -63,7 +91,33 @@ foreach($uid_arr as $uid)
 			echo "\t\t<studiengang>".$studiengang->bezeichnung."</studiengang>\n";
 			echo "\t\t<studiengang_englisch>".$studiengang->english."</studiengang_englisch>\n";
 			echo "\t\t<studiengang_kz>".sprintf('%04s', $studiengang->studiengang_kz)."</studiengang_kz>\n";
-			echo "\t\t<studiengang_typ>".$studiengang->typ."</studiengang_typ>\n";
+            echo "\t\t<studiengangSprache>".$studiengang->sprache."</studiengangSprache>"; 
+            
+            // check ob Quereinsteiger
+            $prestudent = new prestudent(); 
+            $ausbildungssemester = ($prestudent->getFirstStatus($student->prestudent_id, 'Student'))?$prestudent->ausbildungssemester:'';           
+            echo "\t\t<semesterStudent>".$ausbildungssemester."</semesterStudent>";
+            
+            switch($studiengang->typ)
+            {
+                case 'b':
+                    $studTyp = 'Bachelor'; 
+                    $titel_kurzbz = 'BSc'; 
+                    break; 
+                case 'm': 
+                    $studTyp = 'Master'; 
+                    $titel_kurzbz ='MSc'; 
+                    break; 
+                case 'd':
+                    $studTyp = 'Diplom'; 
+                    break; 
+                default: 
+                    $studTyp =''; 
+                    $titel_kurzbz = ''; 
+            }
+            
+            echo "\t\t<titel_kurzbz>".$titel_kurzbz."</titel_kurzbz>\n"; 
+			echo "\t\t<studiengang_typ>".$studTyp."</studiengang_typ>\n";
 			echo "\t\t<studiengang_sprache>".$studiengang->sprache."</studiengang_sprache>\n";
 			echo "\t\t<studiengang_maxsemester>".$studiengang->max_semester."</studiengang_maxsemester>\n";
 			echo "\t\t<datum_aktuell>".$datum_aktuell."</datum_aktuell>\n";
@@ -76,8 +130,7 @@ foreach($uid_arr as $uid)
 				if($row_adresse->zustelladresse)
 				{
 					echo "\t\t<strasse>".$row_adresse->strasse."</strasse>\n";
-					echo "\t\t<plz>".$row_adresse->plz."</plz>\n";
-					echo "\t\t<ort>".$row_adresse->ort."</ort>\n";
+					echo "\t\t<plz>".$row_adresse->plz." ".$row_adresse->ort."</plz>\n";
 					break;
 				}
 			}
