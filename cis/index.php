a71b0d301d65e0dc71243a81bb9b21c308e06e50
Manfred Kindl <kindlm@technikum-wien.at>
2013-07-19 15:32:08 +0000

    Anpassungen an CIS-Redesign ampel.php: Neues Erscheinungsbild index.html: Wird nun als Loginsiete der CIS vorgeschaltet menu.php: Neue Slide-Funktionen, neues Untermenü-Konzept

--- cis/index.php
+++ cis/index.php
@@ -26,6 +26,7 @@ require_once('../include/functions.inc.php');
 require_once('../include/sprache.class.php');
 require_once('../include/phrasen.class.php');
 require_once('../include/mail.class.php');
+require_once('../include/student.class.php');
 
 /**
  * Prueft die URL damit keine boesen URLS uebergeben werden koennen
@@ -73,7 +74,7 @@ if(isset($_GET['content_id']))
 }
 else
 	$id = '';
-
+	
 if(isset($_GET['menu']))
 {
 	$menu = $_GET['menu'];
@@ -82,20 +83,41 @@ if(isset($_GET['menu']))
 else
 	$menu = 'menu.php?content_id='.$id;
 	
+$user = get_uid();
+$student = new student();
+if($student->load($user))
+{
+	$studiengang_kz=$student->studiengang_kz;
+	$semester=$student->semester;
+	$verband=$student->verband;
+}
+else
+{
+	$studiengang_kz='';
+	$semester='';
+	$verband='';
+}
+
 if(isset($_GET['content']))
 {
 	$content = $_GET['content'];
 	validURLCheck($content);
 }
 else
-	$content = '../cms/news.php';
+{
+	if($studiengang_kz=='' && $semester=='' && $verband=='' )
+		$content = '../cms/news.php';
+	else
+		if ($semester=='0' && $verband=='I')
+			$content = '../cms/news.php?studiengang_kz=10006&semester=0';
+		else
+			$content = '../cms/news.php?studiengang_kz='.$studiengang_kz.'&semester='.$semester.'';
+}
 	
 $sprache = getSprache();
 $p = new phrasen($sprache);
 $db = new basis_db();
-?>
-
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 	<title>CIS - <?php echo CAMPUS_NAME; ?></title>
@@ -131,57 +153,101 @@ function loadampel()
 	$('#ampel').load('ampel.php?'+gettimestamp());
 }
 </script>
-<body style="margin-top:0; padding-top:0" onload="loadampel()">
-<table class="tabcontent">
-	 <tr>
-	 	<td></td>
-	 	<td width="100%" ></td>
-	 	<td width="100%" ></td>
-	 	<td></td>
-	 </tr>
-	<tr>
-	    <td width="170" class="tdwrap" onclick="self.location.href='index.php'">
-			<div class="home_logo">&nbsp;</div>
-	    </td>
-        <td id="header" colspan="2" valign="top">
-    	    <div class="header_line" ></div>	
-        </td>
-        <td nowrap>
-        	<div style="font-size: 10px; text-align: right">
-        		<i>Powered by <a href="http://fhcomplete.technikum-wien.at/" target="blank">FH Complete 2.0</a></i>        		
-        	</div>
-        </td>
-        
-	</tr>
-	<tr>
-		<td></td>
-   	 	<td align="right" nowrap  colspan="2">
-   	 		<span id="ampel"></span>
-   	 		<?php require_once('../include/'.EXT_FKT_PATH.'/cis_menu_global.inc.php'); 	?>
-   	 		    <?php
-  
-				$sprache = new sprache();
-				$sprache->getAll(true);
-				foreach($sprache->result as $row)
-				{
-					echo ' <a href="#'.$row->sprache.'" title="'.$row->sprache.'" onclick="changeSprache(\''.$row->sprache.'\'); return false;"><img src="../cms/image.php?src=flag&sprache='.$row->sprache.'" alt="'.$row->sprache.'"></a>';
-				}
-			?>
-   	 	</td>
-   	    <td align="right" nowrap>
-			<form name="searchform" action="private/tools/suche.php" method="GET" target="content" style="display:inline">
-        	<input type="search" size="10" name="search" placeholder="<?php echo $p->t('tools/suchbegriff');?> ..."/>
-        	<img src="../skin/images/search.png" height="14px" onclick="document.searchform.submit()" class="suchicon"/>
-        </form>
-   	    </td>
-	</tr>
+<body class="main" onload="loadampel()">
+<table width="100%" height="100%" cellspacing="0" cellpadding="0" border="0">
+<tr>
+<td class="rand"></td>
+<td class="boxshadow" height="100%">
+	<table cellspacing="0" cellpadding="0" class="header">
+		<tr class="header">
+		<td valign="top" align="left">
+		<a href="index.php"><img class="header_logo" src="../skin/images/fhtw_logo.png" alt="fhtw_logo"></a>
+		<img class="header_left" src="../skin/images/header_left.png" alt="header_links">
+		<img class="header_right" src="../skin/images/header_right.png" alt="header_rechts">	   	 	
+	   	 	<table width="100%" height="100%" style="position:relative; top:10px; z-index:4" cellpadding="0">
+	   	 	<!--<tr valign="top" height="33%">
+		   	 	<td colspan="2" align="right" style="padding-right: 10px;">
+		   	 		<?php	  
+						$sprache = new sprache();
+						$sprache->getAll(true);
+						foreach($sprache->result as $row)
+						{
+							echo ' &nbsp;&nbsp;<a href="#'.$row->sprache.'" title="'.$row->sprache.'" onclick="changeSprache(\''.$row->sprache.'\'); return false;">'.$row->sprache.'</a>';
+					}?>
+				</td>
+		   	 		<!--<?php //require_once('../include/'.EXT_FKT_PATH.'/cis_menu_global.inc.php'); ?>
+		   	  </tr>-->
+		   	  <tr height="50%">
+		   	    <td width="20%" align="center">&nbsp;
+		        </td>
+		         <td align="center">
+					<form name="searchform" action="private/tools/suche.php" method="GET" target="content" style="display:inline">				
+		        	<input class="search" type="search" size="45" name="search" placeholder=" <?php echo $p->t('menu/suchePersonOrtDokumentInhalt');?> ..."/>
+		        	<img src="../skin/images/search.png" height="14px" onclick="document.searchform.submit()" class="suchicon"/>
+		        	</form>
+		        </td>
+		         <td width="20%" align="right" style="padding-right: 10px;">
+					<?php	  
+						$sprache = new sprache();
+						$sprache->getAll(true);
+						foreach($sprache->result as $row)
+						{
+							echo ' &nbsp;&nbsp;<a href="#'.$row->sprache.'" title="'.$row->sprache.'" onclick="changeSprache(\''.$row->sprache.'\'); return false;">'.$row->sprache.'</a>';
+					}?>
+		        </td>
+		   	  </tr>
+		   	  <tr height="50%" valign="top">
+		   	    <td colspan="3" width="100%" align="center" id="ampel"></td>
+	   	      </tr>
+	   	    </table>
+	   	</td>
+	   	</tr>
+	   	<tr>
+			<td valign="top" align="left">
+			<iframe id="menue" src="<?php echo $db->convert_html_chars($menu); ?>" name="menu" frameborder="0">
+				No iFrames
+			</iframe>
+			<iframe id="content" src="<?php echo $db->convert_html_chars($content); ?>" name="content" frameborder="0";>
+				No iFrames
+			</iframe>
+			</td>
+		</tr>
+	</table>
+</td>
+<td class="rand">
+	<!--  Menubox-Effekt am Seitenrand. Nettes Feature aber dzt. nicht sinnvoll einsetzbar
+	<div class="hoverbox">
+		<div class="preview">
+			<img src="../skin/images/aufklappen.png" />
+			<div class="hoverbox_inhalt">
+				<table class="hoverbox">
+					<tr>
+					<td><table style="text-align:right;" width="150px">
+						<tr>
+						<td><a href="http://www.hofer.at" target="blank">Impressum</a></td>
+						</tr>
+						<tr>
+						<td><a href="mailto:kindlm@technikum-wien.at">Kontakt</a></td>
+						</tr>
+						<tr>
+						<td>&nbsp;</td>
+						</tr>
+						<tr>
+						<td>Powered by <a href="http://fhcomplete.technikum-wien.at" target="blank">FH Complete 2.0</i></a></td>
+						</tr>
+						</table>
+					</td>
+					<td><img src="../skin/images/aufklappen.png" /></td>
+					</td>
+					</tr>
+				
+				</table>				
+			</div>
+		</div>
+	</div>-->
+</td>
+</tr>
 </table>
-<iframe id="menue" src="<?php echo $db->convert_html_chars($menu); ?>" name="menu" frameborder="0">
-	No iFrames
-</iframe>
-<iframe id="content" src="<?php echo $db->convert_html_chars($content); ?>" name="content" frameborder="0">
-	No iFrames
-</iframe>
 </body>
 </html>
 <?php
