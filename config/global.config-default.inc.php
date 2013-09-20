5eef86fd59c8237a0004aab13da2273890c245dc
Martin Tatzber <tatzberm@techikum-wien.at>
2013-09-20 11:48:37 +0000

    Gruppieren von zeitgleichen Lehreinheiten als config-Parameter

--- config/global.config-default.inc.php
+++ config/global.config-default.inc.php
@@ -10,6 +10,9 @@ define('DEFAULT_LEHREINHEIT_LEHRFORM','UE');
 define('CIS_LEHRVERANSTALTUNG_NEWSGROUPS_ANZEIGEN',true);
 define('CIS_LEHRVERANSTALTUNG_FEEDBACK_ANZEIGEN',true);
 
+//Anmerkung bei Unterrichtseinheiten im LV-Plan anzeigen
 define('LVPLAN_ANMERKUNG_ANZEIGEN',true);
+//Gruppieren zeitgleicher Lehreinheiten im LV-Plan
+define('LVPLAN_LEHREINHEITEN_GRUPPIEREN',true);
 
 ?>
