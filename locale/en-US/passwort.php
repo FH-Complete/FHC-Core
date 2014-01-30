abee559a73228798a9385703c447e2e41c460eb8
Andreas Österreicher <oesi@technikum-wien.at>
2014-01-30 15:11:25 +0000

    Seite zur Passwortaenderung

---
+++ locale/en-US/passwort.php
@@ -0,0 +1,26 @@
+<?php
+$this->phrasen['passwort/Title']='Password Change';
+$this->phrasen['passwort/PasswortAenderFuer']='Password Change for User %s %s ( %s )';
+$this->phrasen['passwort/InfotextPolicy']='Das Passwort muss zumindest 8 Zeichen enthalten, davon mindestens 1 Großbuchstabe,
+1 Kleinbuchstabe und eine Ziffer!<br><br>
+Das Passwort darf keine Leerzeichen und Umlaute enthalten!<br>
+Erlaubte Sonderzeichen sind: -$#[]{}!().,*:;_';
+
+$this->phrasen['passwort/AltesPasswort']='old Password';
+$this->phrasen['passwort/NeuesPasswort']='new Password';
+$this->phrasen['passwort/PasswortWiederholung']='new Password';
+$this->phrasen['passwort/PasswortAendern']='change Password';
+$this->phrasen['passwort/MinLaenge']='Das neue Passwort muss mindestens 8 Zeichen lang sein.';
+$this->phrasen['passwort/Grossbuchstabe']='Das neue Passwort muss mindestens einen Grossbuchstaben enthalten.';
+$this->phrasen['passwort/Kleinbuchstabe']='Das neue Passwort muss mindestens einen Kleinbuchstaben enthalten.';
+$this->phrasen['passwort/Ziffer']='Es muss mindestens eine Ziffer vorhanden sein.';
+$this->phrasen['passwort/Leerzeichen']='Es darf kein Leerzeichen im Passwort vorkommen.';
+$this->phrasen['passwort/Umlaute']='Es dürfen keine Umlaute verwendet werden.';
+$this->phrasen['passwort/Sonderzeichen']='Bitte verwenden Sie nur erlaubte Sonderzeichen';
+$this->phrasen['passwort/AenderungOK']='Das Passwort wurde erfolgreich geaendert';
+$this->phrasen['passwort/AenderungFehler']='Passwort Änderung fehlgeschlagen:%s';
+$this->phrasen['passwort/NichtUebereinstimmend']='Passwörter stimmen nicht überein';
+$this->phrasen['passwort/NoHttps']='ACHTUNG - Sie verwenden eine unverschlüsselte Verbindung. Sie sollten Ihr Passwort nur über eine verschlüsselte Verbindung ändern.';
+$this->phrasen['passwort/ZuHttpsWechseln']='Jetzt zu verschlüsselter Verbindung wechseln';
+
+?>
