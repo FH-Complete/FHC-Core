3dcc53c42dc62a906bcab28af534bfcc2a128fba
Andreas Österreicher <oesi@technikum-wien.at>
2014-03-07 08:03:01 +0000

    Notenübernahme aus Moodle optimiert

--- system/moodlePlugin/fhcompletews/version.php
+++ system/moodlePlugin/fhcompletews/version.php
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version  = 2013031302;   // The (date) version of this module + 2 extra digital for daily versions
+$plugin->version  = 2014030605;   // The (date) version of this module + 2 extra digital for daily versions
                                   // This version number is displayed into /admin/forms.php
                                   // TODO: if ever this plugin get branched, the old branch number
                                   // will not be updated to the current date but just incremented. We will
@@ -20,5 +20,5 @@ $plugin->version  = 2013031302;   // The (date) version of this module + 2 extra
                                   // display this version number with userdate (dev friendly)
 $plugin->requires = 2010112400;  // Requires this Moodle version - at least 2.0
 $plugin->cron     = 0;
-$plugin->release = '1.0 (Build: 2013031302)';
+$plugin->release = '1.0 (Build: 2014030605)';
 $plugin->maturity = MATURITY_STABLE;
