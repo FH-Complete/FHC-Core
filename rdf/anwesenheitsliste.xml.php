6075cd5f5978e7455c72a6f5e893b0590af3c063
Nikolaus Krondraf <krondraf@technikum-wien.at>
2014-10-07 11:51:28 +0000

    Script erweitert

--- rdf/anwesenheitsliste.xml.php
+++ rdf/anwesenheitsliste.xml.php
@@ -43,15 +43,11 @@ if(isset($_GET['typ']) && $_GET['typ'] == 'lehreinheit')
 	$db = new basis_db();
 	
 	// Daten der Lehreinheit ermitteln
-	$qry = "SELECT le.lehrveranstaltung_id, bezeichnung, stundenblockung, sp.ort_kurzbz, datum, beginn, ende, vorname, nachname "
+	$qry = "SELECT le.lehrveranstaltung_id, lv.lvnr, bezeichnung, stundenblockung, sp.ort_kurzbz, datum, beginn, ende "
 		. "FROM lehre.tbl_lehreinheit le "
 		. "JOIN lehre.tbl_lehrveranstaltung lv ON lv.lehrveranstaltung_id = le.lehrveranstaltung_id "
 		. "JOIN lehre.tbl_stundenplan sp ON sp.unr = le.unr "
 		. "JOIN lehre.tbl_stunde stu ON stu.stunde = sp.stunde "
-		. "JOIN lehre.tbl_lehreinheitmitarbeiter lema ON lema.lehreinheit_id = le.lehreinheit_id "
-		. "JOIN public.tbl_mitarbeiter ma ON ma.mitarbeiter_uid = lema.mitarbeiter_uid "
-		. "JOIN public.tbl_benutzer be ON be.uid = ma.mitarbeiter_uid "
-		. "JOIN public.tbl_person per ON per.person_id = be.person_id "
 		. "WHERE le.lehreinheit_id = " . $db->db_add_param($lehreinheit_id) . " "
 		. "AND sp.datum = " . $db->db_add_param($datum);
 
@@ -65,24 +61,44 @@ if(isset($_GET['typ']) && $_GET['typ'] == 'lehreinheit')
 			// Ausgabe der Lehrveranstaltung
 			echo "\n		<lehreinheit>";
 			echo "\n			<bezeichnung><![CDATA[".$row->bezeichnung."]]></bezeichnung>";
+			echo "\n			<kuerzel><![CDATA[".$row->lvnr."]]></kuerzel>";
 			echo "\n			<stundenblockung><![CDATA[".$row->stundenblockung."]]></stundenblockung>";
 			echo "\n			<ort><![CDATA[".$row->ort_kurzbz."]]></ort>";
 			echo "\n			<datum><![CDATA[".date('d.m.Y', strtotime($row->datum))."]]></datum>";
 			echo "\n			<beginn><![CDATA[".mb_substr($row->beginn, 0, 5)."]]></beginn>";
 			echo "\n			<ende><![CDATA[".mb_substr($row->ende, 0, 5)."]]></ende>";
-			echo "\n			<nachname><![CDATA[".$row->nachname."]]></nachname>";
-			echo "\n			<vorname><![CDATA[".$row->vorname."]]></vorname>";
 			echo "\n		</lehreinheit>";
 		}
 	}
 	
+	// Daten der Vortragenden ermitteln
+	$qry = "SELECT vorname, nachname, titelpre, titelpost "
+		. "FROM lehre.tbl_lehreinheitmitarbeiter lema "
+		. "JOIN public.tbl_benutzer be ON be.uid = lema.mitarbeiter_uid "
+		. "JOIN public.tbl_person pe ON pe.person_id = be.person_id "
+		. "WHERE lehreinheit_id = " . $db->db_add_param($lehreinheit_id);
+	
+	if($db->db_query($qry))
+	{
+		while($row = $db->db_fetch_object())
+		{
+			// Ausgabe der Vortragenden
+			echo "\n		<vortragender>";
+			echo "\n			<vorname><![CDATA[".$row->vorname."]]></vorname>";
+			echo "\n			<nachname><![CDATA[".$row->nachname."]]></nachname>";
+			echo "\n			<titelpre><![CDATA[".$row->titelpre."]]></titelpre>";
+			echo "\n			<titelpost><![CDATA[".$row->titelpost."]]></titelpost>";
+			echo "\n		</vortragender>";
+		}
+	}
+		
+
 	// Daten der Teilnehmer ermitteln
-	$qry = "SELECT vorname, nachname "
+	$qry = "SELECT vorname, nachname, titelpre, titelpost "
 		. "FROM campus.vw_student_lehrveranstaltung stlv "
 		. "JOIN public.tbl_benutzer be ON be.uid = stlv.uid "
 		. "JOIN public.tbl_person pe ON pe.person_id = be.person_id "
-		. "WHERE stlv.lehrveranstaltung_id = " . $db->db_add_param($lv) . " "
-		. "AND studiensemester_kurzbz = 'WS2013'";
+		. "WHERE stlv.lehreinheit_id = " . $db->db_add_param($lehreinheit_id);
 	
 	if($db->db_query($qry))
 	{
@@ -92,6 +108,8 @@ if(isset($_GET['typ']) && $_GET['typ'] == 'lehreinheit')
 			echo "\n		<teilnehmer>";
 			echo "\n			<vorname><![CDATA[".$row->vorname."]]></vorname>";
 			echo "\n			<nachname><![CDATA[".$row->nachname."]]></nachname>";
+			echo "\n			<titelpre><![CDATA[".$row->titelpre."]]></titelpre>";
+			echo "\n			<titelpost><![CDATA[".$row->titelpost."]]></titelpost>";
 			echo "\n		</teilnehmer>";
 		}
 	}
