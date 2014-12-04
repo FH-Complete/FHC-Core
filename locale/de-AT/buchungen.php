80aaa0d30793cbcac1c330ba9da798abdc8bb5d8
Nikolaus Krondraf <krondraf@technikum-wien.at>
2014-12-04 12:54:19 +0000

    CIS-Seite "Buchungen" hinzugefügt

---
+++ locale/de-AT/buchungen.php
@@ -0,0 +1,7 @@
+<?php
+$this->phrasen['buchungen/titel']='Buchungen';
+$this->phrasen['buchungen/buchungsdatum']='Buchungsdatum';
+$this->phrasen['buchungen/buchungstext']='Buchungstext';
+$this->phrasen['buchungen/betrag']='Betrag';
+$this->phrasen['buchungen/buchgstyp']='Buchungstyp';
+?>
