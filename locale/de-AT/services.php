8a449b10b9a409d65e8d1e014538df42d2ff4c2f
Manfred Kindl <kindlm@technikum-wien.at>
2014-12-15 07:09:32 +0000

    service.class.php: Content_id in allen Klassen hinzugefügt service_uebersicht.php: Servicekatalog mit Übersicht aller Services

---
+++ locale/de-AT/services.php
@@ -0,0 +1,6 @@
+﻿<?php
+$this->phrasen['services/service']='Service';
+$this->phrasen['services/uebersichtUeberServicesOrganisationseinheiten']='Übersicht über die Services der Organisationseinheiten';
+$this->phrasen['services/details']='Details';
+$this->phrasen['services/filtern']='Filtern';
+?>
