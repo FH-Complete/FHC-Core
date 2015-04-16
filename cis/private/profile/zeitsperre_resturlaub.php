a753ec4d776d895e6547a2c9796cb965e96ed0eb
Gerald Raab <raab@technikum-wien.at>
2015-04-16 11:21:09 +0200

    ZA anzeigen und syncen

--- cis/private/profile/zeitsperre_resturlaub.php
+++ cis/private/profile/zeitsperre_resturlaub.php
@@ -86,7 +86,7 @@ $addon_obj = new addon();
 $addon_obj->loadAddons();
 foreach($addon_obj->result as $addon)
 {
-	if(file_exists('./../../addons/'.$addon->kurzbz.'/cis/init.js.php'))
+	if(file_exists('../../../addons/'.$addon->kurzbz.'/cis/init.js.php'))
 		echo '<script type="application/x-javascript" src="../../../addons/'.$addon->kurzbz.'/cis/init.js.php" ></script>';
 }
 
