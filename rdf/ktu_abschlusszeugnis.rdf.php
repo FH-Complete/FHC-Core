5903f68ef6d1f02697ad8f4a0f340dacef52b39b
Andreas Moik <moik@technikum-wien.at>
2016-03-10 17:44:26 +0100

    cleanup

--- rdf/ktu_abschlusszeugnis.rdf.php
+++ rdf/ktu_abschlusszeugnis.rdf.php
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
  * Authors: Stefan Puraner <stefan.puraner@technikum-wien.at>,
- Andreas Moik <moik@technikum-wien.at>
+ *          Andreas Moik <moik@technikum-wien.at>
 
  */
 header("Content-type: application/xhtml+xml");
