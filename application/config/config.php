932d48d95b96c2cc472fec38f83f9f420034b967
paolo <bison.paolo@gmail.com>
2016-04-26 14:04:10 +0200

    FIXed Autoload function using spl_autoload_register

--- application/config/config.php
+++ application/config/config.php
@@ -508,9 +508,12 @@ $config['proxy_ips'] = '';
 | Autoload Custom Controllers
 |--------------------------------------------------------------------------
 |
-Don't work so sometime delete this*/
-function __autoload($class) 
-{
+| It's working, so don't delete this :D
+ */
+spl_autoload_register(function ($class) {
+	
+	error_log("__autoload");
+	
     if (substr($class,0,3) !== 'CI_' && substr($class,0,4) !== 'FHC_') 
 	{
         if (file_exists($file = APPPATH . 'core/' . $class . '.php')) 
@@ -518,4 +521,4 @@ function __autoload($class)
             require_once $file;
         }
     }
-}
+});
