39f65875c6eed253e7d1c133fab493f6ca1d5fb9
Paminger <christian@paminger.at>
2016-04-26 15:23:21 +0200

    PersonCept

--- tests/codeception/api/v1/PersonCept.php
+++ tests/codeception/api/v1/PersonCept.php
@@ -1,17 +1,27 @@
 <?php
 
 $I = new ApiTester($scenario);
-$I->wantTo('test the Person code');
+$I->wantTo('test the Person id');
 $I->amHttpAuthenticated("admin", "1q2w3");
 $I->haveHttpHeader('FHC-API-KEY', 'testapikey@fhcomplete.org');
-$I->sendGET('v1/person/Person/person?code=4ffd30cc58718');
+$I->sendGET('v1/person/Person/person?person_id=3');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
     'person_id' => '3',
     'nachname' => 'McKenzie']);
 
-$I->sendGET('v1/person/person/Person', array('code' => 'bd94ef5d5a'));
+$I->wantTo('test the Person code');
+$I->haveHttpHeader('FHC-API-KEY', 'testapikey@fhcomplete.org');
+$I->sendGET('v1/person/Person/person?code=bd94ef5d5a');
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+    'person_id' => '4',
+    'nachname' => 'Wilderman']);
+
+$I->wantTo('test the Person not found');
+$I->sendGET('v1/person/person/Person', array('code' => '12345'));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
