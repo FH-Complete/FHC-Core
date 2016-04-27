549b0133c2937ded1135c1aa5c8458367f823488
paolo <bison.paolo@gmail.com>
2016-04-27 11:54:20 +0200

    Merge origin/ci into ci
    Conflicts:
    	application/config/config.php
    	tests/codeception/_data/dump.sql
    	tests/codeception/api/v1/PersonCept.php

--- application/config/config.php
+++ application/config/config.php
@@ -509,13 +509,9 @@ $config['proxy_ips'] = '';
 |--------------------------------------------------------------------------
 |
 | It's working, so don't delete this :D
- */
+*/
 spl_autoload_register(function ($class) {
     if (substr($class,0,3) !== 'CI_' && substr($class,0,4) !== 'FHC_') 
-	{
-        if (file_exists($file = APPPATH . 'core/' . $class . '.php')) 
-		{
-            require_once $file;
-        }
-    }
+	    if (file_exists($file = APPPATH . 'core/' . $class . '.php')) 
+	        require_once $file;
 });
--- tests/codeception/api/v1/PersonCept.php
+++ tests/codeception/api/v1/PersonCept.php
@@ -1,26 +1,33 @@
 <?php
 
 $I = new ApiTester($scenario);
-$I->wantTo('Test API call v1/person/person/Person');
+$I->wantTo('test the Person id');
 $I->amHttpAuthenticated("admin", "1q2w3");
 $I->haveHttpHeader('FHC-API-KEY', 'testapikey@fhcomplete.org');
 $I->sendGET('v1/person/person/Person', array('person_id' => 3));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
-	'success' => TRUE,
-	'message' => 'Person found']);
+    'person_id' => '3',
+    'nachname' => 'McKenzie']);
 
 $I->sendGET('v1/person/person/Person', array('code' => '01234567A'));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
-	'success' => TRUE,
-	'message' => 'Person found']);
+    'person_id' => '4',
+    'nachname' => 'Wilderman']);
 
-$I->sendGET('v1/person/person/Person', array('code' => '01234567A', 'email' => 'mckenzie.vicenta@calva.dev'));
+$I->wantTo('test the Person not found');
+$I->sendGET('v1/person/person/Person', array('code' => '12345'));
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseContainsJson([
+	'success' => false,
+	'message' => 'Person not found']);
+
+/*$I->sendGET('v1/person/person/Person', array('code' => 'bd94ef5d5a', 'email' => '12351235'));
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseContainsJson([
 	'success' => TRUE,
-	'message' => 'Person found']);
