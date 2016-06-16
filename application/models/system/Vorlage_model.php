9ed42794a7fffdd311d2cb5bb1e2b83f28e1e2c9
Paminger <christian@paminger.at>
2016-06-16 11:40:49 +0200

    Recover fhcdb_helper

--- application/models/system/Vorlage_model.php
+++ application/models/system/Vorlage_model.php
@@ -11,4 +11,15 @@ class Vorlage_model extends DB_Model
 		$this->dbTable = 'public.tbl_vorlage';
 		$this->pk = 'vorlage_kurzbz';
 	}
+
+	public function getMimeTypes()
+	{
+		$qry = 'SELECT DISTINCT mimetype FROM public.tbl_vorlage ORDER BY mimetype;';
+
+		
+		if ($res = $this->db->query($qry))
+			return $this->_success($res);
+		else
+			return $this->_error($this->db->error());
+	}
 }
