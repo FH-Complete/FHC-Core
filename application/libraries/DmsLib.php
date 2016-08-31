578742c5c1f915ca85553c342c6e08c570c8b918
oesi <oesi@technikum-wien.at>
2016-08-31 12:54:51 +0200

    Fixed File Upload

--- application/libraries/DmsLib.php
+++ application/libraries/DmsLib.php
@@ -3,35 +3,35 @@
 if (! defined("BASEPATH")) exit("No direct script access allowed");
 
 /**
- * 
+ *
  */
 class DmsLib
 {
-	// 
+	//
 	const FILE_CONTENT_PROPERTY = "file_content";
-	
+
 	/**
 	 * Object initialization
 	 */
 	public function __construct()
 	{
 		$this->ci =& get_instance();
-		
+
 		$this->ci->load->model("content/Dms_model", "DmsModel");
 		$this->ci->load->model("content/DmsVersion_model", "DmsVersionModel");
 		$this->ci->load->model("content/DmsFS_model", "DmsFSModel");
 	}
-	
+
 	public function read($dms_id, $version = null)
 	{
 		$result = null;
-		
+
 		if (isset($dms_id))
 		{
 			$this->ci->DmsModel->addJoin("campus.tbl_dms_version", "dms_id");
 			$this->ci->DmsModel->addOrder("version", "DESC");
 			$this->ci->DmsModel->addLimit(1);
-			
+
 			if (!isset($version))
 			{
 				$result = $this->ci->DmsModel->load($dms_id);
@@ -41,7 +41,7 @@ class DmsLib
 				$result = $this->ci->DmsModel->loadWhere(array("dms_id" => $dms_id, "version" => $version));
 			}
 		}
-		
+
 		if (is_object($result) && $result->error == EXIT_SUCCESS && is_array($result->retval) && count($result->retval) > 0)
 		{
 			$resultFS = $this->ci->DmsFSModel->read($result->retval[0]->filename);
@@ -53,21 +53,21 @@ class DmsLib
 			{
 				$result = $resultFS;
 			}
-			
+
 		}
-		
+
 		return $result;
 	}
-	
+
 	public function save($dms)
 	{
 		$result = null;
-		
+
 		if(isset($dms["new"]) && $dms["new"] == true)
 		{
 			// Remove new parameter to avoid DB insert errors
 			unset($dms["new"]);
-			
+
 			$result = $this->_saveFileOnInsert($dms);
 			if (is_object($result) && $result->error == EXIT_SUCCESS)
 			{
@@ -108,43 +108,43 @@ class DmsLib
 				}
 			}
 		}
-		
+
 		return $result;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private function _saveFileOnInsert($dms)
 	{
 		$filename = uniqid() . "." . pathinfo($dms["name"], PATHINFO_EXTENSION);
-		
-		return $this->ci->DmsFSModel->write($filename, $dms["file_content"]);
+
+		$result = $this->ci->DmsFSModel->write($filename, $dms["file_content"]);
 		if (is_object($result) && $result->error == EXIT_SUCCESS)
 		{
 			$result->retval = $filename;
 		}
-		
+
 		return $result;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private function _saveFileOnUpdate($dms)
 	{
 		$result = null;
-		
+
 		if(isset($dms["version"]))
 		{
 			$result = $this->read($dms["dms_id"], $dms["version"]);
-			
+
 			if (is_object($result) && $result->error == EXIT_SUCCESS && is_array($result->retval) && count($result->retval) > 0)
 			{
 				$result = $this->ci->DmsFSModel->write($result->retval[0]->filename, $dms["file_content"]);
 			}
 		}
-		
+
 		return $result;
 	}
 }
