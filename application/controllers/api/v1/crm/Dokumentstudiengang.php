7b7039d9bc45c5b2f13fcf1e59b18c49d9e013f6
Stefan Puraner <puraner@technikum-wien.at>
2016-10-11 12:16:08 +0200

    added workaround to convert pgsql arrays to php arrays to another method

--- application/controllers/api/v1/crm/Dokumentstudiengang.php
+++ application/controllers/api/v1/crm/Dokumentstudiengang.php
@@ -62,6 +62,9 @@ class Dokumentstudiengang extends APIv1_Controller
 		{
 			$result = $this->DokumentstudiengangModel->getDokumentstudiengangByStudiengang_kz($studiengang_kz, $onlinebewerbung, $pflicht);
 			
+			// Workaround
+			$result = $this->escapeArrays($result, array('bezeichnung_mehrsprachig', 'dokumentbeschreibung_mehrsprachig'));
+			
 			$this->response($result, REST_Controller::HTTP_OK);
 		}
 		else
