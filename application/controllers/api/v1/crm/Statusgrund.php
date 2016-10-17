9e7f9a7fe54205bd8ca69d73e2534c6dbb78939e
bison-paolo <bison.paolo@gmail.com>
2016-10-17 17:11:31 +0200

    Added new controller Statusgrund

---
+++ application/controllers/api/v1/crm/Statusgrund.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * FH-Complete
+ *
+ * @package		FHC-API
+ * @author		FHC-Team
+ * @copyright	Copyright (c) 2016, fhcomplete.org
+ * @license		GPLv3
+ * @link		http://fhcomplete.org
+ * @since		Version 1.0
+ * @filesource
+ */
+// ------------------------------------------------------------------------
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Statusgrund extends APIv1_Controller
+{
+	/**
+	 * Status API constructor.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		// Load model StatusModel
+		$this->load->model('crm/Statusgrund_model', 'StatusgrundModel');
+	}
+
+	/**
+	 * @return void
+	 */
+	public function getStatusgrund()
+	{
+		$statusgrund_kurzbz = $this->get('statusgrund_kurzbz');
+		
+		if (isset($statusgrund_kurzbz))
+		{
+			$result = $this->StatusgrundModel->load($statusgrund_kurzbz);
+			
+			$this->response($result, REST_Controller::HTTP_OK);
+		}
+		else
+		{
+			$this->response();
+		}
+	}
+
+	/**
+	 * @return void
+	 */
+	public function postStatusgrund()
+	{
+		if ($this->_validate($this->post()))
+		{
+			if (isset($this->post()['statusgrund_kurzbz']))
+			{
+				$result = $this->StatusgrundModel->update($this->post()['statusgrund_kurzbz'], $this->post());
+			}
+			else
+			{
+				$result = $this->StatusgrundModel->insert($this->post());
+			}
+			
+			$this->response($result, REST_Controller::HTTP_OK);
+		}
+		else
+		{
+			$this->response();
+		}
+	}
+	
+	private function _validate($statusgrund = NULL)
+	{
+		return true;
+	}
+}
