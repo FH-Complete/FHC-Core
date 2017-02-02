bf5120a2a9265f7c602d636cbd16f265d4cc56f8
oesi <oesi@technikum-wien.at>
2017-02-02 13:58:13 +0100

    Datenbank Index für tbl_prestudent und prestudentstatus hinzugefügt

--- system/dbupdate_3.2.php
+++ system/dbupdate_3.2.php
@@ -1483,6 +1483,25 @@ if($result = $db->db_query("SELECT data_type FROM information_schema.columns WHE
 	}
 }
 
+// Index fuer prestudent und prestudentstatus
+if($result = $db->db_query("SELECT * FROM pg_class WHERE relname='idx_prestudent_person'"))
+{
+	if($db->db_num_rows($result)==0)
+	{
+
+		$qry = "
+		CREATE INDEX idx_prestudent_person ON public.tbl_prestudent USING btree (person_id);
+		CREATE INDEX idx_prestudentstatus_prestudent ON public.tbl_prestudentstatus USING btree (prestudent_id);
+		";
+
+		if(!$db->db_query($qry))
+			echo '<strong>Indizes: '.$db->db_last_error().'</strong><br>';
+		else
+			echo 'Diverse Indizes fuer Prestudent und Prestudentstatus hinzugefuegt';
+	}
+}
+
+
 // *** Pruefung und hinzufuegen der neuen Attribute und Tabellen
 echo '<H2>Pruefe Tabellen und Attribute!</H2>';
 
