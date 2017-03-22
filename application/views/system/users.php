70ba8cabed83634661581e5506eeb215f89cbc67
Paolo <bison@technikum-wien.at>
2017-03-22 14:16:08 +0100

    - system/Users: better GUI - added new css and js to templates/header

--- application/views/system/users.php
+++ application/views/system/users.php
@@ -1,4 +1,4 @@
-<?php $this->load->view("templates/header", array("title" => "MessageReply", "jquery" => true, "tinymce" => true)); ?>
+<?php $this->load->view("templates/header", array("title" => "Users manager", "jquery" => true, "tablesort" => true, "jquery_checkboxes" => true, "jquery_custom" => true)); ?>
 
 	<body>
 		<div>
@@ -17,56 +17,67 @@
 				?>
 			</form>
 		</div>
-
+		
+		<br>
+		
 		<form id="linkUsersForm" action="" method="post">
-			
-			<br>
-			<br>
-			<br>
-			
 			<?php
 				if ($users != null)
 				{
 			?>
-				<div>
-					Assign to:
-				</div>
-				<div>
-					<select name="aufnahmegruppe">
-						<?php foreach($gruppen as $v): ?>
-							<?php
-								$selected = '';
-								if ($v->gruppe_kurzbz == $selectedGruppe)
-								{
-									$selected = 'selected';
-								}
-							?>
-							<option value="<?php echo $v->gruppe_kurzbz; ?>" <?php echo $selected; ?>>
-								<?php echo $v->beschreibung; ?>
-							</option>
-						<?php endforeach; ?>
-					</select>
-					&nbsp;&nbsp;&nbsp;
-					<input type="button" id="linkToGruppe" value="Assign to this group">
-				</div>
-				<div>
-					<select name="stufe">
-						<?php foreach($stufen as $v): ?>
-							<?php
-								$selected = '';
-								if ($v->stufe == $selectedStufe)
-								{
-									$selected = 'selected';
-								}
-							?>
-							<option value="<?php echo $v->stufe; ?>" <?php echo $selected; ?>>
-								<?php echo $v->beschreibung; ?>
-							</option>
-						<?php endforeach; ?>
-					</select>
-					&nbsp;&nbsp;&nbsp;
-					<input type="button" id="linkToStufe" value="Assign this stufe">
-				</div>
+				<table>
+					<tr>
+						<td colspan="2">
+							<strong>Assign to:</strong>
+						</td>
+					</tr>
+					<tr>
+						<td height="3px" colspan="2"></td>
+					</tr>
+					<tr>
+						<td>
+							<select name="stufe">
+								<?php foreach($stufen as $v): ?>
+									<?php
+										$selected = '';
+										if ($v->stufe == $selectedStufe)
+										{
+											$selected = 'selected';
+										}
+									?>
+									<option value="<?php echo $v->stufe; ?>" <?php echo $selected; ?>>
+										<?php echo $v->beschreibung; ?>
+									</option>
+								<?php endforeach; ?>
+							</select>
+						</td>
+						<td>&nbsp;</td>
+						<td>
+							<input type="button" id="linkToStufe" value="Assign this stufe">
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<select name="aufnahmegruppe">
+								<?php foreach($gruppen as $v): ?>
+									<?php
+										$selected = '';
+										if ($v->gruppe_kurzbz == $selectedGruppe)
+										{
+											$selected = 'selected';
+										}
+									?>
+									<option value="<?php echo $v->gruppe_kurzbz; ?>" <?php echo $selected; ?>>
+										<?php echo $v->beschreibung; ?>
+									</option>
+								<?php endforeach; ?>
+							</select>
+						<td>&nbsp;</td>
+						<td>
+							<input type="button" id="linkToGruppe" value="Assign to this group">
+						</td>
+					</tr>
+				</table>
 			<?php
 				}
 			?>
@@ -78,13 +89,13 @@
 					if ($users != null)
 					{
 				?>
-						<table>
+						<table id="t0" class="tablesorter">
 							<thead>
 								<tr>
 									<th>&nbsp;</th>
-									<th>Prestudent ID</th>
-									<th>Person ID</th>
-									<th>Vorname</th>
+									<th class="clm_prestudent_id header">Prestudent ID</th>
+									<th class="clm_person_id header">Person ID</th>
+									<th class="header headerSortDown">Vorname</th>
 									<th>Nachname</th>
 									<th>Geschlecht</th>
 									<th>Studiengang</th>
@@ -205,6 +216,43 @@
 					}
 				});
 			}
+			
+			$(".tablesorter").each(function(i, v) {
+				$("#"+v.id).tablesorter(
+				{
+					widgets: ["zebra"],
+					sortList: [[3,0],[4,0]],
+					headers: {0: { sorter: false}}
+				});
+
+				$("#toggle_"+v.id).on('click', function(e) {
+					$("#"+v.id).checkboxes('toggle');
+					e.preventDefault();
+					if ($("input.chkbox:checked").size() > 0)
+						$("#mailSendButton").html('Mail an markierte Personen senden');
+					else
+						$("#mailSendButton").html('Mail an alle senden');
+				});
+
+				$("#uncheck_"+v.id).on('click', function(e) {
+					$("#"+v.id).checkboxes('uncheck');
+					e.preventDefault();
+					if ($("input.chkbox:checked").size() > 0)
+						$("#mailSendButton").html('Mail an markierte Personen senden');
+					else
+						$("#mailSendButton").html('Mail an alle senden');
+				});
+
+				$("#"+v.id).checkboxes('range', true);
+			});
+
+			$('.chkbox').change(function()
+			{
+				if ($("input.chkbox:checked").size() > 0)
+					$("#mailSendButton").html('Mail an markierte Personen senden');
+				else
+					$("#mailSendButton").html('Mail an alle senden');
+			});
 		});
 
 		function studiengangSelected(value)
--- application/views/widgets/usersfilters.php
+++ application/views/widgets/usersfilters.php
@@ -10,88 +10,98 @@
 <?php
 	}
 ?>
-<div>
-	<div>
-		<select id="studiengang" name="studiengang">
-			<?php foreach($studiengaenge as $v): ?>
-				<?php
-					$selected = '';
-					if ($v->studiengang_kz == $selectedStudiengang)
-					{
-						$selected = 'selected';
-					}
-				?>
-				<option value="<?php echo $v->studiengang_kz; ?>" <?php echo $selected; ?> onClick="studiengangSelected(this.value)">
+<table>
+	<tr>
+		<td>
+			<select id="studiengang" name="studiengang">
+				<?php foreach($studiengaenge as $v): ?>
 					<?php
-						if ($v->studiengang_kz == '-1')
-						{	
-							echo $v->kurzbzlang;
-						}
-						else
+						$selected = '';
+						if ($v->studiengang_kz == $selectedStudiengang)
 						{
-							echo $v->kurzbzlang . ' (' . $v->bezeichnung . ')';
+							$selected = 'selected';
 						}
 					?>
-				</option>
-			<?php endforeach; ?>
-		</select>
-		<select id="studiensemester" name="studiensemester">
-			<?php foreach($studiensemester as $v): ?>
-				<?php
-					$selected = '';
-					if ($v->studiensemester_kurzbz == $selectedStudiensemester)
-					{
-						$selected = 'selected';
-					}
-				?>
-				<option value="<?php echo $v->studiensemester_kurzbz; ?>" <?php echo $selected; ?> onClick="studiensemesterSelected(this.value)">
-					<?php echo $v->beschreibung; ?>
-				</option>
-			<?php endforeach; ?>
-		</select>
-		<select id="reihungstest" name="reihungstest">
-			<?php foreach($reihungstest as $v): ?>
-				<?php
-					$selected = '';
-					if ($v->reihungstest_id == $selectedReihungstest)
-					{
-						$selected = 'selected';
-					}
-				?>
-				<option value="<?php echo $v->reihungstest_id; ?>" <?php echo $selected; ?> onClick="reihungstestSelected(this.value)">
-					<?php echo $v->beschreibung; ?>
-				</option>
-			<?php endforeach; ?>
-		</select>
-	</div>
-	<div>
-		<select id="gruppe" name="gruppe">
-			<?php foreach($gruppen as $v): ?>
-				<?php
-					$selected = '';
-					if ($v->gruppe_kurzbz == $selectedGruppe)
-					{
-						$selected = 'selected';
-					}
-				?>
-				<option value="<?php echo $v->gruppe_kurzbz; ?>" <?php echo $selected; ?> onClick="gruppeSelected(this.value)">
-					<?php echo $v->beschreibung; ?>
-				</option>
-			<?php endforeach; ?>
-		</select>
-		<select id="stufe" name="stufe">
-			<?php foreach($stufen as $v): ?>
-				<?php
-					$selected = '';
-					if ($v->stufe == $selectedStufe)
-					{
-						$selected = 'selected';
-					}
-				?>
-				<option value="<?php echo $v->stufe; ?>" <?php echo $selected; ?> onClick="stufeSelected(this.value)">
-					<?php echo $v->beschreibung; ?>
-				</option>
-			<?php endforeach; ?>
-		</select>
-	</div>
-</div>
+					<option value="<?php echo $v->studiengang_kz; ?>" <?php echo $selected; ?> onClick="studiengangSelected(this.value)">
+						<?php
+							if ($v->studiengang_kz == '-1')
+							{	
+								echo $v->kurzbzlang;
+							}
+							else
+							{
+								echo $v->kurzbzlang . ' (' . $v->bezeichnung . ')';
+							}
+						?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+		</td>
+		<td>
+			<select id="studiensemester" name="studiensemester">
+				<?php foreach($studiensemester as $v): ?>
+					<?php
+						$selected = '';
+						if ($v->studiensemester_kurzbz == $selectedStudiensemester)
+						{
+							$selected = 'selected';
+						}
+					?>
+					<option value="<?php echo $v->studiensemester_kurzbz; ?>" <?php echo $selected; ?> onClick="studiensemesterSelected(this.value)">
+						<?php echo $v->beschreibung; ?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+		</td>
+		<td>
+			<select id="reihungstest" name="reihungstest">
+				<?php foreach($reihungstest as $v): ?>
+					<?php
+						$selected = '';
+						if ($v->reihungstest_id == $selectedReihungstest)
+						{
+							$selected = 'selected';
+						}
+					?>
+					<option value="<?php echo $v->reihungstest_id; ?>" <?php echo $selected; ?> onClick="reihungstestSelected(this.value)">
+						<?php echo $v->beschreibung; ?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<select id="gruppe" name="gruppe">
+				<?php foreach($gruppen as $v): ?>
+					<?php
+						$selected = '';
+						if ($v->gruppe_kurzbz == $selectedGruppe)
+						{
+							$selected = 'selected';
+						}
+					?>
+					<option value="<?php echo $v->gruppe_kurzbz; ?>" <?php echo $selected; ?> onClick="gruppeSelected(this.value)">
+						<?php echo $v->beschreibung; ?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+		</td>
+		<td colspan="2">
+			<select id="stufe" name="stufe">
+				<?php foreach($stufen as $v): ?>
+					<?php
+						$selected = '';
+						if ($v->stufe == $selectedStufe)
+						{
+							$selected = 'selected';
+						}
+					?>
+					<option value="<?php echo $v->stufe; ?>" <?php echo $selected; ?> onClick="stufeSelected(this.value)">
+						<?php echo $v->beschreibung; ?>
+					</option>
+				<?php endforeach; ?>
+			</select>
+		</td>
+	</tr>
+</table>
