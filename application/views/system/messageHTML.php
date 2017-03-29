f25cfc3fb11292c816a07ce778ef7c3867129f21
Paolo <bison@technikum-wien.at>
2017-03-29 18:45:32 +0200

    Message viewer by token updated with standard CSS

--- application/views/system/messageHTML.php
+++ application/views/system/messageHTML.php
@@ -1,9 +1,38 @@
-<div>
-	S: <?php echo $message->subject; ?>
-</div>
-<div>
-	B: <?php echo $message->body; ?>
-</div>
-<div>
-	<a href="<?php echo $href; ?>">Reply</a>
-</div>
+<?php $this->load->view("templates/header", array("title" => "Message viewer")); ?>
+
+	<body>
+		
+		<table widht="70%">
+			<tr>
+				<td>
+					Subject:
+				</td>
+				<td>
+					&nbsp;
+				</td>
+				<td>
+					<?php echo $message->subject; ?>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Message: 
+				</td>
+				<td>
+					&nbsp;
+				</td>
+				<td>
+					<?php echo $message->body; ?>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td colspan="2" align="center">
+					<a href="<?php echo $href; ?>">Reply</a>
+				</td>
+			</tr>
+		</table>
+	
+	</body>
+
+<?php $this->load->view("templates/footer"); ?>
