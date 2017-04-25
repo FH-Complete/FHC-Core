e936c7655698c6b2eae205ef768496e24ff12868
oesi <oesi@technikum-wien.at>
2017-04-25 15:56:20 +0200

    - Wenn mehrere Moodle Kurse zu einer LV vorhanden sind, werden die Kurse zu denen der eingeloggte User zugeteilt ist getrennt dargestellt um leichter erkennen zu können welche die eigenen Kurse sind - Wenn die Moodle Verbindung nicht klappt wird nun stattdessen die Kurs ID als Linkname angezeigt da sonst der Link nicht sichtbar ist

--- cis/private/lehre/moodle_choice.php
+++ cis/private/lehre/moodle_choice.php
@@ -27,6 +27,7 @@ require_once('../../../include/moodle.class.php');
 require_once('../../../include/moodle19_course.class.php');
 require_once('../../../include/moodle24_course.class.php');
 require_once('../../../include/phrasen.class.php');
+require_once('../../../include/lehreinheit.class.php');
 
 if (!$db = new basis_db())
 	die('Fehler beim Herstellen der Datenbankverbindung');
@@ -37,15 +38,15 @@ $p = new phrasen(getSprache());
 
 if(isset($_GET['lvid']))
 	$lvid=$_GET['lvid'];
-else 
+else
 	die('lvid muss uebergeben werden');
-	
+
 if(isset($_GET['stsem']))
 	$stsem = $_GET['stsem'];
-else 
+else
 	die('Es wurde kein Studiensemester uebergeben');
-	
-echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+echo '<!DOCTYPE HTML>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -53,39 +54,74 @@ echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www
 </head>
 <body>
 <h1>'.$p->t('moodle/kursUebersicht').'</h1>
-
-<table width="100%">
-	<tr>
-		<td>';
+';
 
 $moodle = new moodle();
 $moodle->getAll($lvid, $stsem);
 
+$meinekurse = '';
+$allgemeinekurse = '';
+
 foreach ($moodle->result as $row)
 {
+	$kurs = '';
 	switch($row->moodle_version)
 	{
 		case '1.9':
 			$mdlcourse19=new moodle19_course();
 			$mdlcourse19->load($row->mdl_course_id);
-			echo "<a href='".$moodle->getPfad($row->moodle_version)."course/view.php?id=".$row->mdl_course_id."' class='Item'>$mdlcourse19->mdl_fullname</a><br>";
+			$bezeichnung = $mdlcourse19->mdl_fullname;
+			if($bezeichnung=='')
+				$bezeichnung = 'Course '.$row->mdl_course_id;
+			$kurs = "<a href='".$moodle->getPfad($row->moodle_version)."course/view.php?id=".$row->mdl_course_id."' class='Item'>$bezeichnung</a><br>";
 			break;
 
 		case '2.4':
 			$mdlcourse24=new moodle24_course();
 			$mdlcourse24->load($row->mdl_course_id);
-			echo "<a href='".$moodle->getPfad($row->moodle_version)."course/view.php?id=".$row->mdl_course_id."' class='Item'>$mdlcourse24->mdl_fullname</a><br>";
+			$bezeichnung = $mdlcourse24->mdl_fullname;
+			if($bezeichnung=='')
+				$bezeichnung = 'Course '.$row->mdl_course_id;
+			$kurs = "<a href='".$moodle->getPfad($row->moodle_version)."course/view.php?id=".$row->mdl_course_id."' class='Item'>$bezeichnung</a><br>";
 			break;
 
 		default:
-			echo $p->t('moodle/ungueltigeVersion',array($row->moodle_version)).'<br>';
+			$kurs = $p->t('moodle/ungueltigeVersion',array($row->moodle_version)).'<br>';
 			break;
 	}
+	if($row->lehreinheit_id!='')
+	{
+		$le = new lehreinheit();
+		$stud = $le->getStudenten($row->lehreinheit_id);
+		$zugeordnet = false;
+
+		foreach($stud as $row_stud)
+		{
+			if($row_stud->uid == $user)
+			{
+				$zugeordnet = true;
+				break;
+			}
+		}
+
+		if($zugeordnet)
+		{
+			$meinekurse .= $kurs;
+		}
+	}
+	$allgemeinekurse .= $kurs;
 }
 
-echo '	</td>
-	</tr>
-</table>
-</body>
+if($meinekurse!='')
+{
+	echo '<h2>'.$p->t('moodle/meineKurse').'</h2>';
+	echo $meinekurse;
+}
+
+echo '<br><br><h2>'.$p->t('moodle/vorhandeneKurse').'</h2>';
+
+echo $allgemeinekurse;
+
+echo '</body>
 </html>';
 ?>
