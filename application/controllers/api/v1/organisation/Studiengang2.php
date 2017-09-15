5ff875917cc38158423cfb9ec4d40f6aba706a89
Paolo <bison@technikum-wien.at>
2017-09-15 14:15:04 +0200

    Added method getAppliedStudiengangFromNowOE to get all the applied study courses for one person - Starting from the current study semester - For an organisation unit present in the organisation units tree that has as root the given parameter

--- application/controllers/api/v1/organisation/Studiengang2.php
+++ application/controllers/api/v1/organisation/Studiengang2.php
@@ -24,15 +24,15 @@ class Studiengang2 extends APIv1_Controller
 		// Load model PersonModel
 		$this->load->model("organisation/studiengang_model", "StudiengangModel");
 	}
-	
+
 	public function getStudiengang()
 	{
 		$studiengang_kz = $this->get("studiengang_kz");
-		
+
 		if (isset($studiengang_kz))
 		{
 			$result = $this->StudiengangModel->load($studiengang_kz);
-			
+
 			$this->response($result, REST_Controller::HTTP_OK);
 		}
 		else
@@ -40,12 +40,12 @@ class Studiengang2 extends APIv1_Controller
 			$this->response();
 		}
 	}
-	
+
 	public function getAllForBewerbung()
 	{
 		$this->response($this->StudiengangModel->getAllForBewerbung(), REST_Controller::HTTP_OK);
 	}
-	
+
 	/**
 	 * Method getStudiengangStudienplan
 	 */
@@ -56,16 +56,16 @@ class Studiengang2 extends APIv1_Controller
 		$ausbildungssemester = $this->get("ausbildungssemester");
 		$aktiv = $this->get("aktiv");
 		$onlinebewerbung = $this->get("onlinebewerbung");
-		
+
 		// If $studiensemester_kurzbz and $ausbildungssemester are present
 		if (isset($studiensemester_kurzbz) && isset($ausbildungssemester))
 		{
 			// Check & set
 			if (!isset($aktiv)) $aktiv = "TRUE";
 			if (!isset($onlinebewerbung)) $onlinebewerbung = "TRUE";
-			
+
 			$result = $this->StudiengangModel->getStudienplan($studiensemester_kurzbz, $ausbildungssemester, $aktiv, $onlinebewerbung);
-			
+
 			$this->response($result, REST_Controller::HTTP_OK);
 		}
 		else
@@ -73,14 +73,14 @@ class Studiengang2 extends APIv1_Controller
 			$this->response();
 		}
 	}
-	
+
 	public function getStudiengangBewerbung()
 	{
 		$result = $this->StudiengangModel->getStudiengangBewerbung();
-			
+
 		$this->response($result, REST_Controller::HTTP_OK);
 	}
-	
+
 	/**
 	 * @return void
 	 */
@@ -89,17 +89,17 @@ class Studiengang2 extends APIv1_Controller
 		$person_id = $this->get('person_id');
 		$studiensemester_kurzbz = $this->get('studiensemester_kurzbz');
 		$titel = $this->get('titel');
-		
+
 		if (isset($person_id) && isset($studiensemester_kurzbz) && isset($titel))
 		{
 			$this->load->model('organisation/Studiengang_model', 'StudiengangModel');
-			
+
 			$result = $this->StudiengangModel->getAppliedStudiengang(
 				$person_id,
 				$studiensemester_kurzbz,
 				$titel
 			);
-			
+
 			$this->response($result, REST_Controller::HTTP_OK);
 		}
 		else
@@ -107,7 +107,7 @@ class Studiengang2 extends APIv1_Controller
 			$this->response();
 		}
 	}
-	
+
 	/**
 	 * @return void
 	 */
@@ -115,16 +115,43 @@ class Studiengang2 extends APIv1_Controller
 	{
 		$person_id = $this->get('person_id');
 		$titel = $this->get('titel');
-		
+
 		if (isset($person_id) && isset($titel))
 		{
 			$this->load->model('organisation/Studiengang_model', 'StudiengangModel');
-			
+
 			$result = $this->StudiengangModel->getAppliedStudiengangFromNow(
 				$person_id,
 				$titel
 			);
-			
+
+			$this->response($result, REST_Controller::HTTP_OK);
+		}
+		else
+		{
+			$this->response();
+		}
+	}
+
+	/**
+	 * @return void
+	 */
+	public function getAppliedStudiengangFromNowOE()
+	{
+		$person_id = $this->get('person_id');
+		$titel = $this->get('titel');
+		$oe_kurzbz = $this->get('oe_kurzbz');
+
+		if (isset($person_id) && isset($titel) && isset($oe_kurzbz))
+		{
+			$this->load->model('organisation/Studiengang_model', 'StudiengangModel');
+
+			$result = $this->StudiengangModel->getAppliedStudiengangFromNowOE(
+				$person_id,
+				$titel,
+				$oe_kurzbz
+			);
+
 			$this->response($result, REST_Controller::HTTP_OK);
 		}
 		else
