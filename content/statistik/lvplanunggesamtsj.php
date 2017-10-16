71c03dc945c1819a4d54f0142758b514b895e8cc
Andreas Oesterreicher <oesi@technikum-wien.at>
2017-10-16 08:19:44 +0200

    tbl_lehrfach aus Statistik entfernt

--- content/statistik/lvplanunggesamtsj.php
+++ content/statistik/lvplanunggesamtsj.php
@@ -45,34 +45,37 @@ function drawBetreuungen()
 	global $gesamtkosten_lva, $zeile, $spalte, $stsem1, $stsem2, $last_fb, $worksheet;
 	global $format_bold, $format_colored, $gesamtkosten_betreuung;
 	global $gesamtkosten_fb, $format_number, $format_number1;
-	
+
+	$db = new basis_db();
 	$qry_fb = "SELECT
 				*
 			FROM
-				lehre.tbl_projektarbeit, lehre.tbl_lehreinheit, lehre.tbl_lehrveranstaltung, lehre.tbl_projektbetreuer, public.tbl_person, lehre.tbl_lehrfach
+				lehre.tbl_projektarbeit, lehre.tbl_lehreinheit, lehre.tbl_lehrveranstaltung, lehre.tbl_projektbetreuer,
+				public.tbl_person, lehre.tbl_lehrveranstaltung as lehrfach, public.tbl_fachbereich
 			WHERE
 				tbl_projektarbeit.lehreinheit_id=tbl_lehreinheit.lehreinheit_id AND
 				tbl_lehreinheit.lehrveranstaltung_id=tbl_lehrveranstaltung.lehrveranstaltung_id AND
 				tbl_projektarbeit.projektarbeit_id=tbl_projektbetreuer.projektarbeit_id AND
-				tbl_lehreinheit.lehrfach_id=tbl_lehrfach.lehrfach_id AND
+				tbl_lehreinheit.lehrfach_id=lehrfach.lehrveranstaltung_id AND
 				tbl_person.person_id=tbl_projektbetreuer.person_id AND
-				(tbl_lehreinheit.studiensemester_kurzbz='".addslashes($stsem1)."' OR
-				 tbl_lehreinheit.studiensemester_kurzbz='".addslashes($stsem2)."') AND
+				(tbl_lehreinheit.studiensemester_kurzbz=".$db->db_add_param($stsem1)." OR
+				 tbl_lehreinheit.studiensemester_kurzbz=".$db->db_add_param($stsem2).") AND
 				(tbl_projektbetreuer.faktor*tbl_projektbetreuer.stundensatz*tbl_projektbetreuer.stunden)>0 AND
-				tbl_lehrfach.fachbereich_kurzbz='".addslashes($last_fb)."'
+				lehrfach.oe_kurzbz = tbl_fachbereich.oe_kurzbz AND
+				tbl_fachbereich.fachbereich_kurzbz=".$db->db_add_param($last_fb)."
 			";
-	$db = new basis_db();
+
 	$gesamtkosten_betreuung=0;
 	if($result_fb = $db->db_query($qry_fb))
 	{
 		$spalte=11;
 		$worksheet->writeNumber($zeile, ++$spalte, $gesamtkosten_lva, $format_number);
-				
+
 		if($db->db_num_rows($result_fb)>0)
 		{
-					
+
 			$zeile++;
-						
+
 			$zeile++;
 			$worksheet->write($zeile, 1, "Betreuungen", $format_bold);
 			$spalte=2;
@@ -85,8 +88,8 @@ function drawBetreuungen()
 			$worksheet->write($zeile, ++$spalte, "Student", $format_colored);
 			$worksheet->write($zeile, ++$spalte, "Lektor", $format_colored);
 			$worksheet->write($zeile, ++$spalte, "Kosten", $format_colored);
-					
-			
+
+
 			$stunden_betreuung=0;
 			while($row_fb = $db->db_fetch_object($result_fb))
 			{
@@ -97,31 +100,31 @@ function drawBetreuungen()
 				$worksheet->write($zeile, ++$spalte, '');
 				$worksheet->write($zeile, ++$spalte, number_format($row_fb->stunden,2));
 				$worksheet->write($zeile, ++$spalte, '');
-				
+
 				$benutzer = new benutzer();
 				$benutzer->load($row_fb->student_uid);
 				$worksheet->write($zeile, ++$spalte, "$benutzer->nachname $benutzer->vorname");
 				$worksheet->write($zeile, ++$spalte, "$row_fb->nachname $row_fb->vorname");
 				$worksheet->writeNumber($zeile, ++$spalte, ($row_fb->stundensatz*$row_fb->faktor*$row_fb->stunden), $format_number1);
-				
+
 				$gesamtkosten_betreuung +=($row_fb->stundensatz*$row_fb->faktor*$row_fb->stunden);
 				$stunden_betreuung+=$row_fb->stunden;
 			}
-			
+
 			$zeile++;
 			$spalte=7;
 			$worksheet->writeNumber($zeile, $spalte, $stunden_betreuung, $format_number);
 			$spalte=11;
 			$worksheet->writeNumber($zeile, $spalte, $gesamtkosten_betreuung, $format_number);
-					
+
 			$spalte=12;
 			$worksheet->writeNumber($zeile, $spalte, $gesamtkosten_betreuung, $format_number);
-									
+
 		}
 		$gesamtkosten_fb += ($gesamtkosten_betreuung+$gesamtkosten_lva);
 		$gesamtkosten_lva=0;
 	}
-	else 
+	else
 		echo 'Error';
 }
 // ****** END FUNKTIONEN ******* //
@@ -130,34 +133,39 @@ $stsem_obj = new studiensemester();
 
 if(substr($stsem1,0,1)=='S') //Eigentlich gehoert =='W', nur kurzfristige aenderung
 	$stsem2 = $stsem_obj->getNextFrom($stsem1);
-else 
+else
 	$stsem2 = $stsem_obj->getPreviousFrom($stsem1);
-	
+
 $qry = "SELECT
-			tbl_lehrveranstaltung.kurzbz as kurzbz, tbl_lehrveranstaltung.bezeichnung as bezeichnung, tbl_lehrveranstaltung.lehrveranstaltung_id,
-			tbl_lehrveranstaltung.ects as ects, tbl_lehrveranstaltung.semesterstunden as semesterstunden,
-			tbl_lehrfach.kurzbz as lf_kurzbz, tbl_lehrfach.bezeichnung as lf_bezeichnung, tbl_lehreinheit.lehreinheit_id as lehreinheit_id,
-			tbl_lehreinheit.lehrform_kurzbz as lehrform_kurzbz, tbl_lehreinheitmitarbeiter.semesterstunden as lektor_semesterstunden,
-			tbl_lehreinheitmitarbeiter.stundensatz as lektor_stundensatz, tbl_lehreinheitmitarbeiter.faktor as lektor_faktor,
-			tbl_person.vorname, tbl_person.nachname, tbl_lehrveranstaltung.studiengang_kz, tbl_lehrveranstaltung.semester,
-			tbl_lehrfach.fachbereich_kurzbz
+			tbl_lehrveranstaltung.kurzbz as kurzbz, tbl_lehrveranstaltung.bezeichnung as bezeichnung,
+			tbl_lehrveranstaltung.lehrveranstaltung_id, tbl_lehrveranstaltung.ects as ects,
+			tbl_lehrveranstaltung.semesterstunden as semesterstunden, lehrfach.kurzbz as lf_kurzbz,
+			lehrfach.bezeichnung as lf_bezeichnung, tbl_lehreinheit.lehreinheit_id as lehreinheit_id,
+			tbl_lehreinheit.lehrform_kurzbz as lehrform_kurzbz,
+			tbl_lehreinheitmitarbeiter.semesterstunden as lektor_semesterstunden,
+			tbl_lehreinheitmitarbeiter.stundensatz as lektor_stundensatz,
+			tbl_lehreinheitmitarbeiter.faktor as lektor_faktor,
+			tbl_person.vorname, tbl_person.nachname, tbl_lehrveranstaltung.studiengang_kz,
+			tbl_lehrveranstaltung.semester,
+			tbl_fachbereich.fachbereich_kurzbz
 		FROM
 			lehre.tbl_lehrveranstaltung, lehre.tbl_lehreinheit, lehre.tbl_lehreinheitmitarbeiter,
-			lehre.tbl_lehrfach, public.tbl_benutzer, public.tbl_person
+			lehre.tbl_lehrveranstaltung as lehrfach, public.tbl_benutzer, public.tbl_person, public.tbl_fachbereich
 		WHERE
 			tbl_lehrveranstaltung.lehrveranstaltung_id=tbl_lehreinheit.lehrveranstaltung_id AND
 			tbl_lehreinheit.lehreinheit_id=tbl_lehreinheitmitarbeiter.lehreinheit_id AND
-			tbl_lehrfach.lehrfach_id=tbl_lehreinheit.lehrfach_id AND
+			lehrfach.lehrveranstaltung_id=tbl_lehreinheit.lehrfach_id AND
 			tbl_benutzer.uid=tbl_lehreinheitmitarbeiter.mitarbeiter_uid AND
 			tbl_person.person_id=tbl_benutzer.person_id AND
-			(tbl_lehreinheit.studiensemester_kurzbz='".addslashes($stsem1)."' OR
-			 tbl_lehreinheit.studiensemester_kurzbz='".addslashes($stsem2)."')
-		ORDER BY 
-			tbl_lehrfach.fachbereich_kurzbz,
-			tbl_lehrveranstaltung.studiengang_kz, 
-			tbl_lehrveranstaltung.semester, 
-			tbl_lehrveranstaltung.bezeichnung, 
-			tbl_lehrveranstaltung.lehrveranstaltung_id, 
+			(tbl_lehreinheit.studiensemester_kurzbz=".$db->db_add_param($stsem1)." OR
+			 tbl_lehreinheit.studiensemester_kurzbz=".$db->db_add_param($stsem2).") AND
+			 tbl_fachbereich.oe_kurzbz=lehrfach.oe_kurzbz
+		ORDER BY
+			tbl_fachbereich.fachbereich_kurzbz,
+			tbl_lehrveranstaltung.studiengang_kz,
+			tbl_lehrveranstaltung.semester,
+			tbl_lehrveranstaltung.bezeichnung,
+			tbl_lehrveranstaltung.lehrveranstaltung_id,
 			tbl_lehreinheit.lehreinheit_id";
 
 // Creating a workbook
@@ -230,17 +238,17 @@ if($result = $db->db_query($qry))
 				$worksheet->write($zeile, $spalte,sprintf('%.2f',$stunden_lv), $format_bold);
 				$spalte=11;
 				$worksheet->writeNumber($zeile, $spalte, $kosten_lv, $format_number);
-				
+
 				$gesamtkosten_lva +=$kosten_lv;
 				$stunden_lv=0;
 				$kosten_lv=0;
 			}
-			
+
 			if($last_fb!=$row->fachbereich_kurzbz && $last_fb!='')
 			{
 				drawBetreuungen();
 			}
-			
+
 			if($last_fb=='' || $last_fb!=$row->fachbereich_kurzbz)
 			{
 				$zeile++;
@@ -248,7 +256,7 @@ if($result = $db->db_query($qry))
 				$zeile++;
 				$last_fb = $row->fachbereich_kurzbz;
 			}
-			
+
 			$last_lva=$row->lehrveranstaltung_id;
 			$zeile++;
 			$spalte=0;
@@ -268,7 +276,7 @@ if($result = $db->db_query($qry))
 		}
 
 		$gruppen='';
-		$qry_grp = "SELECT * FROM lehre.tbl_lehreinheitgruppe WHERE lehreinheit_id='$row->lehreinheit_id'";
+		$qry_grp = "SELECT * FROM lehre.tbl_lehreinheitgruppe WHERE lehreinheit_id=".$db->db_add_param($row->lehreinheit_id);
 		if($result_grp = $db->db_query($qry_grp))
 		{
 			while($row_grp = $db->db_fetch_object($result_grp))
@@ -290,7 +298,7 @@ if($result = $db->db_query($qry))
 		$worksheet->write($zeile, ++$spalte, $gruppen);
 		$worksheet->write($zeile, ++$spalte, "$row->nachname $row->vorname");
 		$worksheet->writeNumber($zeile, ++$spalte, ($row->lektor_stundensatz*$row->lektor_faktor*$row->lektor_semesterstunden), $format_number1);
-		
+
 		$kosten_lv +=($row->lektor_stundensatz*$row->lektor_faktor*$row->lektor_semesterstunden);
 		$stunden_lv +=$row->lektor_semesterstunden;
 	}
@@ -299,14 +307,14 @@ if($result = $db->db_query($qry))
 	$worksheet->write($zeile, $spalte,sprintf('%.2f',$stunden_lv), $format_bold);
 	$spalte=11;
 	$worksheet->writeNumber($zeile, $spalte, $kosten_lv, $format_number);
-	
+
 	$gesamtkosten_lva +=$kosten_lv;
 	$stunden_lv=0;
 	$kosten_lv=0;
 
 
 	drawBetreuungen();
-		
+
 }
 
 $zeile++;
