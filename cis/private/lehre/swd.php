d2dbcabc01c06ef1da80fdda5c9d3db6a8d44f20
Andreas Österreicher <oesi@technikum-wien.at>
2018-01-23 11:25:55 +0100

    Wenn der Schlagwortdienst beim Upload von Arbeiten nicht erreichbar ist, wird jetzt ein Hinweistext angezeigt anstatt einer leeren Seite

--- cis/private/lehre/swd.php
+++ cis/private/lehre/swd.php
@@ -3,6 +3,9 @@ $url = "http://xgnd.bsz-bw.de/";
 $zielfeld = "kontrollschlagwoerter";
 $url = $url."?zielfeld=".$zielfeld;
 header('Content-Type: text/html; charset=utf-8');
-print file_get_contents($url);
+$content = file_get_contents($url);
+if($content)
+	print $content;
+else
+	echo "Der Schlagwortdienst ist derzeit nicht erreichbar. Bitte füllen Sie die Schlagwörter manuell aus um den Upload abzuschließen";
 ?>
-
