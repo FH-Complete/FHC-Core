b480ec30a269b1ff52cc4527aa15c704210964b2
Andreas Österreicher <oesi@technikum-wien.at>
2018-02-08 17:32:41 +0100

    Handled Errors if there are no Filters

--- application/controllers/system/infocenter/InfoCenter.php
+++ application/controllers/system/infocenter/InfoCenter.php
@@ -386,6 +386,9 @@ class InfoCenter extends VileSci_Controller
 	 */
 	private function _setNavigationMenuArray()
 	{
+		$listFiltersSent = array();
+		$listFiltersNotSent = array();
+
 		$filtersSent = $this->FiltersModel->getFilterList('infocenter', 'PersonActions', '%InfoCenterSentApplication%');
 		if (hasData($filtersSent))
 		{
