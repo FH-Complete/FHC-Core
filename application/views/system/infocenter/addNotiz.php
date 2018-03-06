56d2686e722d25c9ebf50a7017799ae1386762d0
Manfred Kindl <kindlm@technikum-wien.at>
2018-03-06 16:54:49 +0100

    Infocenter Notiz Button für Autotext
    "Anmerkung zur Bewerbung"-Text wird automatisch eingefügt
    - "Select..."-Option bei Preview nur wenn mehr als 1 Empfänger

--- application/views/system/infocenter/addNotiz.php
+++ application/views/system/infocenter/addNotiz.php
@@ -3,14 +3,20 @@
 		<div class="text-center">
 			<label>Notiz hinzuf&uuml;gen</label>
 		</div>
-		<div class="form-group">
-			<label>Titel: </label><input type="text" class="form-control"
-										 name="notiztitel"/>
+		<div>
+			<div class="form-group">
+				<label>Titel: </label>
+				<div class="input-group">
+					<input id="inputNotizTitel" type="text" class="form-control" name="notiztitel"/>
+					<div class="input-group-addon" onclick="document.getElementById('inputNotizTitel').value='Anmerkung zur Bewerbung'">
+						<span class="glyphicon glyphicon-text-background"></span>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="form-group">
-			<label>Text: </label><textarea name="notiz" class="form-control"
-										   rows="10"
-										   cols="32"></textarea>
+			<label>Text: </label>
+			<textarea name="notiz" class="form-control" rows="10" cols="32"></textarea>
 		</div>
 		<div class="text-right">
 			<button type="submit" class="btn btn-default">Speichern</button>
