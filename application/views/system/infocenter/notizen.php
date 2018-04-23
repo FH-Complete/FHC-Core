b7160b00fb8ebfc5abc14d8dd76a633ca5c22d38
Cris <hainberg@technikum-wien.at>
2018-04-23 14:58:29 +0200

    Adapted curser pointer for notices
    cursor changes now to pointer-symbol when hovering a notice

--- application/views/system/infocenter/notizen.php
+++ application/views/system/infocenter/notizen.php
@@ -11,7 +11,7 @@
 		<tr data-toggle="tooltip"
 			title="<?php echo isset($notiz->text) ? strip_tags($notiz->text) : '' ?>">		
 			<td><?php echo date_format(date_create($notiz->insertamum), 'd.m.Y H:i:s') ?></td>
-			<td><?php echo html_escape($notiz->titel) ?></td>
+			<td style="cursor: pointer"><?php echo html_escape($notiz->titel) ?></td>
 			<td><?php echo $notiz->verfasser_uid ?></td>
 			<td style="display: none"><?php echo $notiz->notiz_id ?></td>
 		</tr>
