e00ccee235bebb8f42d672a1a7f182f8434e6fe8
Andreas Österreicher <oesi@technikum-wien.at>
2018-10-23 16:54:01 +0200

    Beim Zusammenlegen von Personen wird das bPK berücksichtigt und das zusammenlegen ggf verhindert wenn zwei unterschiedliche bPK eingetragen sind

--- vilesci/stammdaten/personen_wartung.php
+++ vilesci/stammdaten/personen_wartung.php
@@ -159,6 +159,14 @@ if (isset($personToDelete) && isset($personToKeep) && $personToDelete >= 0 && $p
 				$error = true;
 			}
 
+			// Wenn beide Personen ein BPK haben, abbrechen
+			if ($personToDelete_obj->bpk != '' && $personToKeep_obj->bpk != '' && $personToDelete_obj->bpk != $personToKeep_obj->bpk)
+			{
+				$msg_error[] = 'Beide Personen haben unterschiedliche BPK und können nicht zusammengelegt werden.<br>
+						Bitte wenden Sie sich an einen Administrator.';
+				$error = true;
+			}
+
 			// Wenn zwei gleiche rt_person Einträge vorhanden sind, wird ein Fehler ausgegeben und abgebrochen
 			$reihungstest_personToKeep = new reihungstest();
 			$reihungstest_personToKeep->getReihungstestPerson($personToKeep);
@@ -276,6 +284,16 @@ if (isset($personToDelete) && isset($personToKeep) && $personToDelete >= 0 && $p
 					$matr_nr = $personToKeep_obj->matr_nr;
 				if ($personToKeep_obj->matr_nr == '' && $personToDelete_obj->matr_nr != '')
 					$matr_nr = $personToDelete_obj->matr_nr;
+				else
+					$matr_nr = $personToKeep_obj->matr_nr;
+
+				$bpk = '';
+				if ($personToDelete_obj->bpk == '' && $personToKeep_obj->bpk != '')
+					$bpk = $personToKeep_obj->bpk;
+				if ($personToKeep_obj->bpk == '' && $personToDelete_obj->bpk != '')
+					$bpk = $personToDelete_obj->bpk;
+				else
+					$bpk = $personToKeep_obj->bpk;
 
 				// Letztbenutzten Zugangscode abfragen und übernehmen
 				$zugangscode = '';
@@ -338,6 +356,9 @@ if (isset($personToDelete) && isset($personToKeep) && $personToDelete >= 0 && $p
 
 				// Matr_nr erst setzen, wenn nur mehr eine Person vorhanden ist
 				$sql_query_upd1 .= "UPDATE public.tbl_person SET matr_nr=" . $db->db_add_param($matr_nr, FHC_STRING) . " WHERE person_id=" . $db->db_add_param($personToKeep, FHC_INTEGER) . ";";
+
+				// BPK erst setzen, wenn nur mehr eine Person vorhanden ist
+				$sql_query_upd1 .= "UPDATE public.tbl_person SET bpk=" . $db->db_add_param($bpk, FHC_STRING) . " WHERE person_id=" . $db->db_add_param($personToKeep, FHC_INTEGER) . ";";
 
 				if ($db->db_query($sql_query_upd1))
 				{
