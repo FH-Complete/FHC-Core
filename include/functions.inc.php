3cb7b41114386d529a9457a9b8e2237278f5ad38
Andreas Österreicher <oesi@technikum-wien.at>
2018-12-10 14:41:02 +0100

    Removed useless include

--- include/functions.inc.php
+++ include/functions.inc.php
@@ -23,7 +23,7 @@ require_once(dirname(__FILE__).'/basis_db.class.php');
 require_once(dirname(__FILE__).'/authentication.class.php');
 require_once(dirname(__FILE__).'/betriebsmittelperson.class.php');
 require_once(dirname(__FILE__).'/personlog.class.php');
-require_once(dirname(__FILE__).'/benutzerberechtigung.class.php');
+//require_once(dirname(__FILE__).'/benutzerberechtigung.class.php');
 
 // Auth: Benutzer des Webportals
 /**
