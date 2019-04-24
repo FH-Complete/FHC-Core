1da4cdb9aa4ef62351a0f967e0b73117e3fcac8a
Paolo <bison@technikum-wien.at>
2019-04-24 19:46:19 +0200

    - Removed languages files for CI file system based phrase system - Renamed application/language/de-AT/ to application/language/english/ - Kept language file rest_controller_lang.php to be used with the REST_Controller

--- application/language/system/fhc_model_lang.php
+++
@@ -1,9 +0,0 @@
-<?php  
-if ( ! defined('BASEPATH'))
-	exit('No direct script access allowed');
-
-// DB-Errormessages
-$lang['fhc_'.FHC_MODEL_ERROR]	= 'Fehler in Model';
-$lang['fhc_'.FHC_NODBTABLE]	= '"dbTable" ist nicht gesetzt!';
-$lang['fhc_'.FHC_NORIGHT]	= 'Rechte sind nicht ausreichend!';
-
