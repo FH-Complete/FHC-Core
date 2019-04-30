bbe62cb9978b03c24d3e74c3c93484689e7fc416
Paolo <bison@technikum-wien.at>
2019-04-30 14:16:54 +0200

    Added new controller system/extensions/CLI_Manager to manage extensions from command line

---
+++ application/controllers/system/extensions/CLI_Manager.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ *
+ */
+class CLI_Manager extends CLI_Controller
+{
+	/**
+	 *
+	 */
+	public function __construct()
+    {
+        parent::__construct();
+	}
+
+	/**
+	 * Installiert eine Extension.
+	 * Es wird davon ausgegangen, dass die Extension ueber einen Fileupload hochgeladen wird.
+	 * alternativ kann hier auch der Name und Filename uebergeben werden um die installation ueber
+	 * die Commandline ohne Upload durchzufuehren.
+	 * @param $extensioName string Name der Extension
+	 * @param $filename Url Encoded Pfad zum tgz File der Extension
+	 */
+	public function uploadExtension($extensionName = null, $filename = null)
+	{
+		$this->extensionslib->installExtension($extensionName, urldecode($filename));
+	}
+}
