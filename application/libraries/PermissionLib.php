615ed03fbe80d3eb5df68be1fc68195d8482a5a7
Cris <hainberg@technikum-wien.at>
2019-09-19 13:28:15 +0200

    Added method getOE_isEntitledFor()
    The method checks permission of the logged in user and retrieves all
    organisational units this user is entitled for.

--- application/libraries/PermissionLib.php
+++ application/libraries/PermissionLib.php
@@ -274,6 +274,25 @@ class PermissionLib
         }
     }
 
+    /**
+     * Returns the organisational units the person is entitled for.
+     * @param null $berechtigung_kurzbz
+     * @return array|bool array of oe_kurzbz the person is entitled for. False on error.
+     */
+    public function getOE_isEntitledFor($berechtigung_kurzbz = null)
+    {
+        $oe_kurzbz_arr = array();
+
+        if (self::$bb->getOEkurzbz($berechtigung_kurzbz))
+        {
+            return $oe_kurzbz_arr =  self::$bb->getOEkurzbz($berechtigung_kurzbz);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 	//------------------------------------------------------------------------------------------------------------------
 	// Private methods
 
