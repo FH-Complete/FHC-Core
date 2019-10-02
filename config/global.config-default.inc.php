544fd4d976c8686b98273885d97b7403d317acc2
Cris <hainberg@technikum-wien.at>
2019-10-02 17:13:16 +0200

    Added config var FAS_LV_LEKTORINNENZUTEILUNG_VERTRAGSDETAILS_ANZEIGEN
    If set to true the Vertragdetails will be displayed in FAS
    LektorInnenzuteilung.

--- config/global.config-default.inc.php
+++ config/global.config-default.inc.php
@@ -123,6 +123,9 @@ define('FAS_UDF', true);
 // Legt fest ob Aufnahmegruppen bei Reihungstests verwaltet werden true|false
 define('FAS_REIHUNGSTEST_AUFNAHMEGRUPPEN',false);
 
+// Legt fest, ob Vertragsdetails zum Lehrauftrag im Reiter LektorInnenzuteilung angezeigt werden
+define('FAS_LV_LEKTORINNENZUTEILUNG_VERTRAGSDETAILS_ANZEIGEN', false);
+
 // Anzeigeoptionen für LV-Plan Menü
 define('CIS_LVPLAN_EXPORT_ANZEIGEN',true);
 define('CIS_LVPLAN_PERSONENAUSWAHL_ANZEIGEN',true);
