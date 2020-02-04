a5ad57ceeefa9b5825646848783229643d3ca400
alex <karpen_ko@hotmail.com>
2020-02-04 17:53:32 +0100

    testtool/admin/edit_gebiet.php: Offsetpunkte können nun über admin GUI eingetragen werden

--- cis/testtool/admin/edit_gebiet.php
+++ cis/testtool/admin/edit_gebiet.php
@@ -186,6 +186,7 @@ if (isset($_POST['speichern']))
 		$gebiet->zufallvorschlag = isset($_POST['zufallvorschlag']);
 		$gebiet->levelgleichverteilung = isset($_POST['levelgleichverteilung']);
 		$gebiet->maxpunkte = $_POST['maxpunkte'];
+		$gebiet->offsetpunkte = $_POST['offsetpunkte'];
 		$gebiet->maxfragen = $_POST['maxfragen'];
 		$gebiet->level_start = $_POST['level_start'];
 		$gebiet->level_sprung_auf = $_POST['level_sprung_auf'];
@@ -256,6 +257,8 @@ if ($gebiet_id != '')
 	else
 		$hinweis = '';
 	echo '<td>Maximale Punkteanzahl</td><td><input type="text" size="5" maxlength="5" name="maxpunkte" value="'.$gebiet->maxpunkte.'">'.$hinweis.'</td>';
+	echo '</tr><tr>';
+	echo '<td>Offsetpunkte (minimale Punkteanzahl)</td><td><input type="text" size="5" maxlength="5" name="offsetpunkte" value="'.$gebiet->offsetpunkte.'"></td>';
 	echo '</tr><tr>';
 	echo '<td>Maximale Fragenanzahl</td><td><input type="text" size="5" maxlength="5" name="maxfragen" value="'.$gebiet->maxfragen.'"></td>';
 	echo '</tr><tr>';
