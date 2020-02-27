a9bc7a6c77d165791cce794d4f7518dc4e7b5cec
alex <karpen_ko@hotmail.com>
2020-02-27 17:20:25 +0100

    auswertung_fhtw: - Notice "gewichtet" added for points sum and percent sum - added percent sign for percent sum - removed commented code

--- vilesci/stammdaten/auswertung_fhtw.php
+++ vilesci/stammdaten/auswertung_fhtw.php
@@ -1456,18 +1456,6 @@ if (isset($_REQUEST['reihungstest']))
 		}
 
 		$ergebnis[$row->prestudent_id]->gebiet[$row->gebiet_id]->name = $row->gebiet;
-		/*if ($row->punkte == 0 && $row->punkte != '')
-		{
-			$prozent = '0';
-		}
-		elseif ($row->punkte >= $row->maxpunkte) //wenn maxpunkte ueberschritten wurde -> 100%
-		{
-			$prozent = 100;
-		}
-		else
-		{
-			$prozent = ($row->punkte / $row->maxpunkte) * 100;
-		}*/
 
 		if ($row->punkte >= $row->maxpunkte)
 		{
@@ -1496,7 +1484,7 @@ if (isset($_REQUEST['reihungstest']))
 			}
 			else
 			{
-				$ergebnis[$row->prestudent_id]->gebiet[$row->gebiet_id]->prozent = $maxpunkte_positiv > 0 ? $punkte_positiv / $maxpunkte_positiv * /*$row->gewicht **/ 100 : null;
+				$ergebnis[$row->prestudent_id]->gebiet[$row->gebiet_id]->prozent = $maxpunkte_positiv > 0 ? $punkte_positiv / $maxpunkte_positiv * 100 : null;
 			}
 		}
 
@@ -1692,32 +1680,32 @@ if (isset($_REQUEST['format']) && $_REQUEST['format'] == 'xls')
 	$worksheet->mergeCells(0, 12, 1, 12);
 	$maxlength[12] = 20;
 
-	$spalte = 11;
+	$spalte = 12;
 	$zeile = 0;
 
 	foreach ($gebiet AS $gbt)
 	{
-		++$spalte;
 		$worksheet->write($zeile, ++$spalte, strip_tags($gbt->name) . (	isset($gbt->gewicht) ? " (Gew: $gbt->gewicht)" : ""), $format_bold_border);
-		$worksheet->mergeCells($zeile, $spalte, 0, $spalte + 1);
-		$maxlength[$spalte] = 10;
+		$worksheet->mergeCells($zeile, $spalte, 0, $spalte + 2);
+		$spalte += 2;
 	}
-	$worksheet->write($zeile, ++$spalte + 1, 'Gesamt', $format_bold_border);
-	$worksheet->mergeCells($zeile, ++$spalte, 0, $spalte + 1);
-	$maxlength[$spalte] = 12;
+	$worksheet->write($zeile, ++$spalte, 'Gesamt (gewichtet)', $format_bold_border);
+	$worksheet->mergeCells($zeile, $spalte, 0, $spalte + 2);
 
 	$spalte = 12;
 	$zeile = 0;
 
 	foreach ($gebiet AS $gbt)
 	{
+		$maxlength[$spalte +1] = $maxlength[$spalte + 2] = $maxlength[$spalte + 3] = 14;
 		$worksheet->write($zeile + 1, ++$spalte, 'Punkte', $format_bold_border);
+		$worksheet->write($zeile + 1, ++$spalte, 'Punkte + Offset', $format_bold_border);
 		$worksheet->write($zeile + 1, ++$spalte, 'Prozent', $format_bold_border);
-		$maxlength[$spalte] = 10;
 	}
+	$maxlength[$spalte +1] = $maxlength[$spalte + 2] = $maxlength[$spalte + 3] = 14;
 	$worksheet->write($zeile + 1, ++$spalte, 'Punkte', $format_bold_border);
+	$worksheet->write($zeile + 1, ++$spalte, 'Punkte + Offset', $format_bold_border);
 	$worksheet->write($zeile + 1, ++$spalte, 'Prozent', $format_bold_border);
-	$maxlength[$spalte] = 10;
 
 	$maxspalten = $spalte;
 
@@ -1762,6 +1750,14 @@ if (isset($_REQUEST['format']) && $_REQUEST['format'] == 'xls')
 					{
 						$worksheet->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->punkte, $format_punkte_rot);
 					}
+					if ($erg->gebiet[$gbt->gebiet_id]->punktemitoffset != '' && $erg->gebiet[$gbt->gebiet_id]->punktemitoffset != '0')
+					{
+						$worksheet->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->punktemitoffset, $format_punkte);
+					}
+					else
+					{
+						$worksheet->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->punktemitoffset, $format_punkte_rot);
+					}
 					if ($erg->gebiet[$gbt->gebiet_id]->prozent != '0%')
 					{
 						$worksheet->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->prozent / 100, $format_prozent);
@@ -1775,10 +1771,12 @@ if (isset($_REQUEST['format']) && $_REQUEST['format'] == 'xls')
 				{
 					$worksheet->write($zeile, ++$spalte, '');
 					$worksheet->write($zeile, ++$spalte, '');
+					$worksheet->write($zeile, ++$spalte, '');
 				}
 			}
 			$worksheet->writeNumber($zeile, ++$spalte, $erg->gesamtpunkte, $format_punkte);
-			$worksheet->writeNumber($zeile, ++$spalte, $erg->gesamt, $format_punkte);
+			$worksheet->writeNumber($zeile, ++$spalte, $erg->gesamtoffsetpunkte, $format_punkte);
+			$worksheet->writeNumber($zeile, ++$spalte, $erg->gesamt / 100, $format_prozent);
 		}
 	}
 
@@ -1787,165 +1785,6 @@ if (isset($_REQUEST['format']) && $_REQUEST['format'] == 'xls')
 	{
 		$worksheet->setColumn($i, $i, $breite);
 	}
-
-	// Worksheet ohne Physik nur für Bachelor-Studiengänge
-	/*if (isset($studiengangObj) && $studiengangObj->typ == 'b')
-	{
-		$worksheetOhnePhsyik =& $workbook->addWorksheet("Auswertung OHNE Physik " . ($titel_studiengang ? $stg_arr[$_REQUEST['studiengang']] : '') . ($titel_semester ? ' ' . $semester . '.Semester' : ''));
-		$worksheetOhnePhsyik->setInputEncoding('utf-8');
-		$worksheetOhnePhsyik->setZoom(85);
-
-		$spalte = 0;
-		$zeile = 0;
-
-		$worksheetOhnePhsyik->write(0, $spalte, 'PrestudentIn_ID', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 0, 1, 0);
-		$maxlength[0] = 15;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'Nachname', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 1, 1, 1);
-		$maxlength[1] = 15;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'Vorname', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 2, 1, 2);
-		$maxlength[2] = 15;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'GebDatum', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 3, 1, 3);
-		$maxlength[3] = 10;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'G', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 4, 1, 4);
-		$maxlength[4] = 2;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'Registriert', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 5, 1, 5);
-		$maxlength[5] = 18;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'STG', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 6, 1, 6);
-		$maxlength[6] = 4;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'Studiengang', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 7, 1, 7);
-		$maxlength[7] = 25;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'S', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 8, 1, 8);
-		$maxlength[8] = 2;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'OrgForm', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 9, 1, 9);
-		$maxlength[9] = 8;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'Prio', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 10, 1, 10);
-		$maxlength[10] = 5;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'ZGV', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 11, 1, 11);
-		$maxlength[11] = 20;
-		$worksheetOhnePhsyik->write(0, ++$spalte, 'ZGV MA', $format_bold);
-		$worksheetOhnePhsyik->mergeCells(0, 12, 1, 12);
-		$maxlength[12] = 20;
-
-		$spalte = 11;
-		$zeile = 0;
-
-		foreach ($gebiet AS $gbt)
-		{
-			if ($gbt->gebiet_id == 10)
-			{
-				continue;
-			}
-			++$spalte;
-			$worksheetOhnePhsyik->write($zeile, ++$spalte, strip_tags($gbt->name), $format_bold_border);
-			$worksheetOhnePhsyik->mergeCells($zeile, $spalte, 0, $spalte + 1);
-			$maxlength[$spalte] = 10;
-		}
-		$worksheetOhnePhsyik->write($zeile, ++$spalte + 1, 'Gesamt', $format_bold_border);
-		$worksheetOhnePhsyik->mergeCells($zeile, ++$spalte, 0, $spalte + 1);
-		$maxlength[$spalte] = 12;
-
-		$spalte = 12;
-		$zeile = 0;
-
-		foreach ($gebiet AS $gbt)
-		{
-			if ($gbt->gebiet_id == 10)
-			{
-				continue;
-			}
-			$worksheetOhnePhsyik->write($zeile + 1, ++$spalte, 'Punkte', $format_bold_border);
-			$worksheetOhnePhsyik->write($zeile + 1, ++$spalte, 'Prozent', $format_bold_border);
-			$maxlength[$spalte] = 10;
-		}
-		$worksheetOhnePhsyik->write($zeile + 1, ++$spalte, 'Punkte', $format_bold_border);
-		$worksheetOhnePhsyik->write($zeile + 1, ++$spalte, 'Prozent', $format_bold_border);
-		$maxlength[$spalte] = 10;
-
-		$maxspalten = $spalte;
-
-		$zeile = 1;
-		$spalte = 0;
-
-		if (isset($ergb))
-		{
-			foreach ($ergb AS $erg)
-			{
-				$zeile++;
-				$spalte = 0;
-				$worksheetOhnePhsyik->write($zeile, $spalte, $erg->prestudent_id);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->nachname);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->vorname);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->gebdatum, $format_date);
-				if ($erg->geschlecht == 'm')
-				{
-					$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->geschlecht, $format_male);
-				}
-				else
-				{
-					$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->geschlecht, $format_female);
-				}
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->registriert, $format_registriert);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->stg_kurzbz);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->stg_bez);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->ausbildungssemester);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->orgform);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $erg->prioritaet);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $zgv_arr[$erg->zgv]);
-				$worksheetOhnePhsyik->write($zeile, ++$spalte, $zgvma_arr[$erg->zgvma]);
-				foreach ($gebiet AS $gbt)
-				{
-					if ($gbt->gebiet_id == 10)
-					{
-						continue;
-					}
-					if (isset($erg->gebiet[$gbt->gebiet_id]))
-					{
-						if ($erg->gebiet[$gbt->gebiet_id]->punkte != '' && $erg->gebiet[$gbt->gebiet_id]->punkte != '0')
-						{
-							$worksheetOhnePhsyik->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->punkte, $format_punkte);
-						}
-						else
-						{
-							$worksheetOhnePhsyik->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->punkte, $format_punkte_rot);
-						}
-						if ($erg->gebiet[$gbt->gebiet_id]->prozent != '0%')
-						{
-							$worksheetOhnePhsyik->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->prozent / 100, $format_prozent);
-						}
-						else
-						{
-							$worksheetOhnePhsyik->writeNumber($zeile, ++$spalte, $erg->gebiet[$gbt->gebiet_id]->prozent / 100, $format_prozent_rot);
-						}
-					}
-					else
-					{
-						$worksheetOhnePhsyik->write($zeile, ++$spalte, '');
-						$worksheetOhnePhsyik->write($zeile, ++$spalte, '');
-					}
-				}
-				$worksheetOhnePhsyik->writeNumber($zeile, ++$spalte, $erg->gesamtpunkte_ohne_physik, $format_punkte);
-				$worksheetOhnePhsyik->writeNumber($zeile, ++$spalte, $erg->gesamt_ohne_physik, $format_punkte);
-			}
-		}
-
-		//Die Breite der Spalten setzen
-		foreach ($maxlength as $i => $breite)
-		{
-			$worksheetOhnePhsyik->setColumn($i, $i, $breite);
-		}
-	}*/
 
 	if (isset($erg_kat) && count($erg_kat) > 0)
 	{
@@ -2935,7 +2774,7 @@ else
 				<th rowspan="2">Raum</th>
 				<th title="Teilgenommen" rowspan="2">TG</th>
 				<th style="display: none"></th>
-				<th colspan="3">Gesamt</th>';
+				<th colspan="3">Gesamt (gewichtet)</th>';
 
 		foreach ($gebiet AS $gbt)
 		{
@@ -2989,7 +2828,7 @@ else
 						<td class='".$inaktiv."'>$erg->raum</td>
 						<td class='".$inaktiv."'>".($erg->teilgenommen == true ? "<span class='glyphicon glyphicon-ok'></span>" : "")."</td>";
 				//<td>$erg->idnachweis</td>
-				$gesamtprozent = ($erg->gesamt != '' ? number_format($erg->gesamt, 2, ',', ' ') : '');
+				$gesamtprozent = ($erg->gesamt != '' ? number_format($erg->gesamt, 2, ',', ' ') . ' %': '');
 				echo '<td style="display: none">'. $gesamtprozent .'</td>';
 				echo '	<td style="text-align: right; padding-right: 3px" class="punkte '.$inaktiv.'" nowrap>';
 				// Punkte können nur gelöscht werden, solange "Zum Reihungstest angetreten" nicht gesetzt ist
