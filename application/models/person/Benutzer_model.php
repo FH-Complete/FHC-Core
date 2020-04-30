554fb2586a3bc8612f3011c7986e84f628310aac
Alexei <karpen_ko@hotmail.com>
2020-04-30 21:42:30 +0200

    person/Benutzer_model: - added methods for generating alias and checking if alias exists ressource/Mitarbeiter_model: - added method getPersonal

--- application/models/person/Benutzer_model.php
+++ application/models/person/Benutzer_model.php
@@ -33,4 +33,53 @@ class Benutzer_model extends DB_Model
 
 		return $this->execQuery($sql, array($person_id, $oe_kurzbz));
 	}
+
+	/**
+	 * Checks if alias exists
+	 * @param $alias
+	 */
+	public function aliasExists($alias)
+	{
+		$this->addSelect('1');
+		$result = $this->loadWhere(array('alias' => $alias));
+
+		if (!isError($result))
+		{
+			if (hasData($result))
+			{
+				$result = success(array(true));
+			}
+			else if (!hasData($result))
+			{
+				$result = success(array(false));
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Generates alias for a uid
+	 * @param $uid
+	 * @return array the alias
+	 */
+	public function generateAlias($uid)
+	{
+		$aliasres = '';
+		$this->addSelect('vorname, nachname');
+		$this->addJoin('public.tbl_person', 'person_id');
+		$nameresult = $this->loadWhere(array('uid' => $uid));
+
+		if (hasData($nameresult))
+		{
+			$aliasdata = getData($nameresult);
+			$alias = mb_strtolower($aliasdata[0]->vorname).'.'.mb_strtolower($aliasdata[0]->nachname);
+			$aliasexists = $this->aliasExists($alias);
+
+			if (hasData($aliasexists) && !getData($aliasexists)[0])
+				$aliasres = $alias;
+
+		}
+		return success($aliasres);
+	}
 }
