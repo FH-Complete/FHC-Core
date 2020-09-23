b62a2007af3862ea0456741bfeb0941fa11efc85
Andreas Österreicher <oesi@technikum-wien.at>
2020-09-23 16:33:14 +0200

    Schlagwortdienst auf HTTPS geaendert

--- cis/private/lehre/swd.php
+++ cis/private/lehre/swd.php
@@ -1,5 +1,5 @@
 <?php
-$url = "http://xgnd.bsz-bw.de/";
+$url = "https://xgnd.bsz-bw.de/";
 $zielfeld = "kontrollschlagwoerter";
 $url = $url."?zielfeld=".$zielfeld;
 header('Content-Type: text/html; charset=utf-8');
