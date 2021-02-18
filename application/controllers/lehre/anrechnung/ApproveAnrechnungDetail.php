9a3addd79d3680cf5182cd38824fba664cdb0f67
Cris <hainberg@technikum-wien.at>
2021-02-18 14:59:11 +0100

    Removed comments on 'if (! defined('BASEPATH')) exit('No direct script access allowed');'
    Signed-off-by: cris-technikum <hainberg@technikum-wien.at>

--- application/controllers/lehre/anrechnung/ApproveAnrechnungDetail.php
+++ application/controllers/lehre/anrechnung/ApproveAnrechnungDetail.php
@@ -1,6 +1,6 @@
 <?php
 
-//if (! defined('BASEPATH')) exit('No direct script access allowed');
+if (! defined('BASEPATH')) exit('No direct script access allowed');
 
 class approveAnrechnungDetail extends Auth_Controller
 {
--- application/controllers/lehre/anrechnung/ReviewAnrechnungDetail.php
+++ application/controllers/lehre/anrechnung/ReviewAnrechnungDetail.php
@@ -1,6 +1,6 @@
 <?php
 
-//if (! defined('BASEPATH')) exit('No direct script access allowed');
+if (! defined('BASEPATH')) exit('No direct script access allowed');
 
 class reviewAnrechnungDetail extends Auth_Controller
 {
