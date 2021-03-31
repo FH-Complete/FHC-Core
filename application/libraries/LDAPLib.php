376f413d2ab0344ea5b596d6561b8ce249eeeb7e
FHBGLD <33415881+FHBGLD@users.noreply.github.com>
2021-03-31 14:58:31 +0200

    Update LDAPLib.php

--- application/libraries/LDAPLib.php
+++ application/libraries/LDAPLib.php
@@ -167,7 +167,8 @@ class LDAPLib
 		}
 
 		// LDAP connection
-		$ldapConnection = @ldap_connect($ldapConfigs[self::SERVER], $ldapConfigs[self::PORT]);
+		//$ldapConnection = @ldap_connect($ldapConfigs[self::SERVER], $ldapConfigs[self::PORT]);
+		$ldapConnection = @ldap_connect($ldapConfigs[self::SERVER].':'.$ldapConfigs[self::PORT]);
 		if ($ldapConnection) // if success
 		{
 			// Sets the LDAP protocol version
