8c100ba99313f87700cdb4972a42c335d875ebc1
Harald Bamberger <harald.bamberger@technikum-wien.at>
2021-11-01 15:25:24 +0100

    moved db logic to lehrelisthelper class

--- cis/private/lehre/covidstatusliste.php
+++ cis/private/lehre/covidstatusliste.php
@@ -18,7 +18,6 @@
  * Authors: Manfred Kindl <manfred.kindl@technikum-wien.at>
  */
 require_once('../../../config/cis.config.inc.php');
-require_once('../../../include/dokument_export.class.php');
 require_once('../../../include/lehrveranstaltung.class.php');
 require_once('../../../include/lehreinheitgruppe.class.php');
 require_once('../../../include/lehreinheit.class.php');
@@ -28,6 +27,7 @@ require_once('../../../include/studiensemester.class.php');
 require_once('../../../include/functions.inc.php');
 require_once('../../../include/erhalter.class.php');
 require_once('../../../include/datum.class.php');
+require_once('../../../include/lehrelisthelper.class.php');
 require_once('../../../include/covid/covidhelper.class.php');
 
 $debug = false;
@@ -59,199 +59,13 @@ if(	!$berechtigung->isBerechtigt('admin')
 	&& !check_lektor_lehrveranstaltung($user,$lvid,$studiensemester))
 	die('Sie muessen LektorIn der LV sein oder das Recht "ADMIN", "ASSISTENZ" oder "LEHRE" haben, um diese Seite aufrufen zu koennen');
 
-$output='pdf';
-
-if(isset($_GET['output']) && ($output='odt' || $output='doc'))
-	$output=$_GET['output'];
-
 isset($_GET['stg_kz']) ? $studiengang = $_GET['stg_kz'] : $studiengang = NULL;
 isset($_GET['lehreinheit_id']) ? $lehreinheit = $_GET['lehreinheit_id'] : $lehreinheit = NULL;
 
-// Teilnehmende Gruppen laden
-$qry = "SELECT DISTINCT ON(kuerzel, semester, verband, gruppe, gruppe_kurzbz)
-			UPPER(stg_typ::varchar(1) || stg_kurzbz) as kuerzel,
-			semester,
-			verband,
-			gruppe,
-			gruppe_kurzbz
-		FROM campus.vw_lehreinheit
-		WHERE lehrveranstaltung_id=".$db->db_add_param($lvid, FHC_INTEGER)."
-		AND studiensemester_kurzbz=".$db->db_add_param($studiensemester);
-if($lehreinheit!='')
-	$qry.=" AND lehreinheit_id=".$db->db_add_param($lehreinheit, FHC_INTEGER);
-
-$gruppen_string = '';
-if($result = $db->db_query($qry))
-{
-	while($row = $db->db_fetch_object($result))
-	{
-		if($gruppen_string!='')
-			$gruppen_string.=', ';
-		if($row->gruppe_kurzbz=='')
-			$gruppen_string.=trim($row->kuerzel.'-'.$row->semester.$row->verband.$row->gruppe);
-		else
-			$gruppen_string.=$row->gruppe_kurzbz;
-	}
-}
-
-
-// Verplante Räume laden
-$qry = "SELECT distinct(ort_kurzbz)
-        FROM lehre.tbl_stundenplan
-		WHERE lehreinheit_id in
-			(
-				SELECT lehreinheit_id
-				FROM campus.vw_lehreinheit
-				WHERE lehrveranstaltung_id = ".$db->db_add_param($lvid, FHC_INTEGER)."
- 				AND studiensemester_kurzbz = ".$db->db_add_param($studiensemester)."
-			)";
-if($lehreinheit!='')
-	$qry.= " AND tbl_stundenplan.lehreinheit_id = ".$db->db_add_param($lehreinheit, FHC_INTEGER);
-
-
-$raum_string = '';
-if($result = $db->db_query($qry))
-{
-	while($row = $db->db_fetch_object($result))
-	{
-		if($raum_string!='')
-			$raum_string.=', ';
-		if($row->ort_kurzbz!='')
-			$raum_string.=$row->ort_kurzbz;
-	}
-}
-
-
-$stg = new studiengang();
-$stg->load($lv->studiengang_kz);
-
-$studiengang_bezeichnung=$stg->bezeichnung;
-
-$stg->getAllTypes();
-
-$data = array(
-	'gruppen'=>$gruppen_string,
-	'bezeichnung'=>$lv->bezeichnung,
-	'lehrveranstaltung_id'=>$lv->lehrveranstaltung_id,
-	'studiengang'=>$studiengang_bezeichnung,
-	'studiengang_kz'=>$lv->studiengang_kz,
-	'typ'=>$stg->studiengang_typ_arr[$stg->typ],
-	'ects'=>$lv->ects,
-	'sprache'=>$lv->sprache,
-	'studiensemester'=>$studiensemester,
-	'semester'=>$lv->semester,
-	'orgform'=>$lv->orgform_kurzbz,
-	'raum'=>$raum_string,
-);
-
-//Lehrende der LV laden und in ein Array schreiben
-$lehrende = new lehreinheitmitarbeiter();
-$lehrende->getMitarbeiterLV($lvid, $studiensemester, $lehreinheit);
-$arr_lehrende = array();
-if (isset($lehrende->result))
-{
-	foreach($lehrende->result AS $row)
-	{
-		$data[]=array('lehrende'=>array('uid'=>$row->uid,'name'=>$row->vorname.' '.$row->nachname));
-		$arr_lehrende[]=mb_strtoupper($row->uid);
-	}
-}
-
-
-//Studierende der LV laden und in ein Array schreiben
-
-$qry = 'SELECT
-			distinct on(nachname, vorname, person_id) vorname, nachname, matrikelnr, public.tbl_student.student_uid,
-			tbl_studentlehrverband.semester, tbl_studentlehrverband.verband, tbl_studentlehrverband.gruppe,
-			(SELECT status_kurzbz FROM public.tbl_prestudentstatus WHERE prestudent_id=tbl_student.prestudent_id ORDER BY datum DESC, insertamum DESC, ext_id DESC LIMIT 1) as status,
-			tbl_bisio.bisio_id, tbl_bisio.von, tbl_bisio.bis, tbl_student.studiengang_kz AS stg_kz_student,
-			tbl_note.lkt_ueberschreibbar, tbl_note.anmerkung, tbl_mitarbeiter.mitarbeiter_uid, tbl_person.matr_nr, tbl_studiengang.kurzbzlang, tbl_mobilitaet.mobilitaetstyp_kurzbz
-		FROM
-			campus.vw_student_lehrveranstaltung
-			JOIN public.tbl_benutzer USING(uid)
-			JOIN public.tbl_person USING(person_id) LEFT JOIN public.tbl_student ON(uid=student_uid)
-			LEFT JOIN public.tbl_mitarbeiter ON(uid=mitarbeiter_uid)
-			LEFT JOIN public.tbl_studentlehrverband USING(student_uid,studiensemester_kurzbz)
-			LEFT JOIN lehre.tbl_zeugnisnote on(vw_student_lehrveranstaltung.lehrveranstaltung_id=tbl_zeugnisnote.lehrveranstaltung_id AND tbl_zeugnisnote.student_uid=tbl_student.student_uid AND tbl_zeugnisnote.studiensemester_kurzbz=tbl_studentlehrverband.studiensemester_kurzbz)
-			LEFT JOIN lehre.tbl_note USING (note)
-			LEFT JOIN bis.tbl_bisio ON(uid=tbl_bisio.student_uid)
-			LEFT JOIN public.tbl_studiengang ON(tbl_student.studiengang_kz=tbl_studiengang.studiengang_kz)
-			LEFT JOIN bis.tbl_mobilitaet USING(prestudent_id)
-		WHERE
-			vw_student_lehrveranstaltung.lehrveranstaltung_id='.$db->db_add_param($lvid, FHC_INTEGER).'	AND
-			vw_student_lehrveranstaltung.studiensemester_kurzbz='.$db->db_add_param($studiensemester);';';
-
-
-if($lehreinheit!='')
-	$qry.=' AND vw_student_lehrveranstaltung.lehreinheit_id='.$db->db_add_param($lehreinheit, FHC_INTEGER);
-
-$qry.=' ORDER BY nachname, vorname, person_id, tbl_bisio.bis DESC';
-
-$stsem_obj = new studiensemester();
-$stsem_obj->load($studiensemester);
-$stsemdatumvon = $stsem_obj->start;
-$stsemdatumbis = $stsem_obj->ende;
-
-$erhalter = new erhalter();
-$erhalter->getAll();
-
-$a_o_kz = '9'.sprintf("%03s", $erhalter->result[0]->erhalter_kz); //Stg_Kz AO-Studierende auslesen (9005 fuer FHTW)
-$anzahl_studierende = 0;
-$datum = new datum();
-$zusatz = '';
-
-$studentuids = array();
-if($result = $db->db_query($qry))
-{
-	while($row = $db->db_fetch_object($result))
-	{
-		if($row->status!='Abbrecher' && $row->status!='Unterbrecher')
-		{
-			$anzahl_studierende++;
-
-			if($row->status=='Incoming') //Incoming
-				$zusatz='(i)';
-			else
-				$zusatz='';
-
-			if($row->bisio_id!='' && $row->status!='Incoming' && ($row->bis > $stsemdatumvon || $row->bis=='') && $row->von < $stsemdatumbis) //Outgoing
-				$zusatz.='(o)(ab '.$datum->formatDatum($row->von,'d.m.Y').')';
-
-			if($row->lkt_ueberschreibbar == 'f') // angerechnet / intern angerechnet / nicht zugelassen
-				$zusatz.= '('. $row->anmerkung. ')';
-
-			if($row->mitarbeiter_uid!='') //mitarbeiter
-				$zusatz.='(ma)';
-
-			if($row->stg_kz_student==$a_o_kz) //Außerordentliche Studierende
-				$zusatz.='(a.o.)';
-
-			if($row->mobilitaetstyp_kurzbz !='') //Double Degree Student
-				$zusatz.='(d.d.)';
-
-			//$zusatz.='test';
-
-			$studentuids[] = $row->student_uid;
-			$data[]=array('student'=>array(
-							'uid' => $row->student_uid,
-							'vorname'=>$row->vorname,
-							'nachname'=>$row->nachname,
-							'personenkennzeichen'=>trim($row->matrikelnr),
-							'matr_nr'=>$row->matr_nr,
-							'semester'=>$row->semester,
-							'verband'=>trim($row->verband),
-							'gruppe'=>trim($row->gruppe),
-							'zusatz'=>$zusatz,
-							'studiengang_kurzbz'=>$row->kurzbzlang,
-							'mobilitaetstyp_kurzbz'=>$row->mobilitaetstyp_kurzbz
-							));
-		}
-	}
-	//Anzahl Studierende in Array $data (an erster Stelle) einfuegen
-	$data = array_reverse($data, true);
-	$data['anzahl_studierende'] = $anzahl_studierende;
-	$data = array_reverse($data, true);
-}
+$lehrelisthelper = new LehreListHelper($db, $studiensemester, $lvid, $lv, $lehreinheit);
+$arr_lehrende = $lehrelisthelper->getArr_Lehrende();
+$data = $lehrelisthelper->getData();
+$studentuids = $lehrelisthelper->getStudentUids();
 
 $covidhelper = new CovidHelper();
 $covidhelper->fetchCovidStatus($studentuids);
@@ -273,15 +87,20 @@ header('Content-Type: text/html; charset=utf-8');
 	<div class="row">
 		<div class="col-lg-6">
 			
-			<h1>Covid-Status-Liste <?php echo $data['bezeichnung']; ?></h1>
-			<h2>Gruppen: <?php echo $data['gruppen']; ?> Studiensemester: <?php echo $data['studiensemester']; ?></h2>
-			<h3>Lehrende: <?php echo implode(', ',array_unique($arr_lehrende)); ?></h3>
-			<h4>generiert: <?php echo $now->format('d.m.Y H:i'); ?></h4>			
+			<h1>Covid-Status-Liste "<?php echo $data['bezeichnung']; ?>"</h1>
+			<ul>
+				<li><strong>Gruppen</strong>: <?php echo $data['gruppen']; ?></li>
+				<li><strong>Studiensemester</strong>: <?php echo $data['studiensemester']; ?></li>
+				<li><strong>Lehrende</strong>: <?php echo $lehrelisthelper->getLehrende_String(); ?></li>
+				<li><strong>generiert</strong>: <?php echo $now->format('d.m.Y H:i'); ?></li>
+				<li><strong>Anzahl der Studierenden</strong>: <?php echo $data['anzahl_studierende']; ?></li>
+			</ul>
 			
 			<table class="table table-striped table-hover table-condensed">
 				<thead>
 					<tr>
-						<th>Student</th>
+						<th>lfd.Nr.</th>
+						<th>Name</th>
 						<th>Kennzeichen</th>
 						<th>Gruppe</th>
 						<th>Covid-Status</th>
@@ -289,6 +108,8 @@ header('Content-Type: text/html; charset=utf-8');
 				</thead>
 				<tbody>
 <?php
+$len = strlen($data['anzahl_studierende']);
+$lfdnr = 1;
 foreach ($data as $value) {
 	if( !(is_array($value) && isset($value['student'])) ) {
 		continue;
@@ -296,12 +117,14 @@ foreach ($data as $value) {
 	$tmpstudent =& $value['student']; 
 ?>
 					<tr class="<?php echo $covidhelper->getBootstrapClass($tmpstudent['uid'])?>" title="<?php echo $covidhelper->getTitle($tmpstudent['uid'])?>">
+						<td><?php echo sprintf('%0' . $len . 'd', $lfdnr); ?></td>
 						<td><?php echo $tmpstudent['nachname'] . ' ' . $tmpstudent['vorname'] . ' ' . $tmpstudent['zusatz']; ?></td>
 						<td><?php echo $tmpstudent['personenkennzeichen']; ?></td>
 						<td><?php echo $tmpstudent['semester'] . $tmpstudent['verband'] . $tmpstudent['gruppe']; ?></td>
 						<td><?php echo $covidhelper->getIconHtml($tmpstudent['uid']); ?></td>
 					</tr>
 <?php
+	$lfdnr++;
 }
 ?>
 				</tbody>
