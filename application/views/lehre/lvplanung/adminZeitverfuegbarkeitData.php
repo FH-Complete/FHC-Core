a788459d541875ea37dcafa587ca57e28aa20465
Andreas Österreicher <oesi@technikum-wien.at>
2022-04-08 14:46:59 +0200

    Zeitverfügbarkeiten werden nur aus dem aktuellen und kommenden Semester angezeigt, vergangene Einträge werden ausgeblendet

--- application/views/lehre/lvplanung/adminZeitverfuegbarkeitData.php
+++ application/views/lehre/lvplanung/adminZeitverfuegbarkeitData.php
@@ -4,8 +4,8 @@ $STUDIENGANG_KZ_ARR = $studiengang_kz_arr; // stg the user is entitled to admini
 
 $qry = '
     SELECT * FROM (
-        SELECT DISTINCT ON (zeitsperre_id, zsp.mitarbeiter_uid) zeitsperre_id, zsp.mitarbeiter_uid, 
-            concat_ws(\' \', nachname, vorname) AS "lektor", 
+        SELECT DISTINCT ON (zeitsperre_id, zsp.mitarbeiter_uid) zeitsperre_id, zsp.mitarbeiter_uid,
+            concat_ws(\' \', nachname, vorname) AS "lektor",
             vondatum, vonstunde, bisdatum, bisstunde, zsp.bezeichnung
         FROM public.tbl_person
         JOIN public.tbl_benutzer b USING (person_id)
@@ -17,8 +17,7 @@ $qry = '
         JOIN campus.tbl_zeitsperre zsp ON zsp.mitarbeiter_uid = lema.mitarbeiter_uid
         WHERE lv.studiengang_kz IN ('. implode (',', $STUDIENGANG_KZ_ARR). ')
         AND b.aktiv
-        AND ma.personalnummer > 0
-        AND NOW() <= ss.ende
+		AND zsp.vondatum >= ss.start
         AND zeitsperretyp_kurzbz = \'ZVerfueg\'
         ORDER BY zeitsperre_id, zsp.mitarbeiter_uid
     ) as tmp
@@ -57,7 +56,7 @@ $filterWidgetArray = array(
     'datasetRepFieldsDefs' => '{
         zeitsperre_id: {visible:false},
         mitarbeiter_uid: {visible: true, headerFilter:"input"},
-        lektor: {visible: true, headerFilter:"input"},     
+        lektor: {visible: true, headerFilter:"input"},
         vondatum: {visible: true, headerFilter:"input"},
         vonstunde: {visible: true, headerFilter:"input"},
         bisdatum: {visible: true, headerFilter:"input"},
