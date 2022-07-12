a780338367ad0f1775db4dacfa85b786f40eb2c7
Paolo <bison@technikum-wien.at>
2022-07-12 12:16:48 +0200

    Includes fixes components/font-awesome to fortawesome/font-awesome4

--- cis/private/lehre/covidstatusliste.php
+++ cis/private/lehre/covidstatusliste.php
@@ -82,7 +82,7 @@ header('Content-Type: text/html; charset=utf-8');
 <head>
 	<title>FHC - Nachweisliste</title>
 	<link rel="stylesheet" type="text/css" href="../../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-	<link href="../../../vendor/components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>	
+	<link href="../../../vendor/fortawesome/font-awesome4/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 
