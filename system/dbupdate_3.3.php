282b46d54e22a918b87b3779221bd4f989826eb8
KarpAlex <karpen_ko@hotmail.com>
2022-08-23 15:26:13 +0200

    Added betreuerart Senatsvorsitz and Senatsprüfer

--- system/dbupdate_3.3.php
+++ system/dbupdate_3.3.php
@@ -6273,6 +6273,34 @@ if($result = @$db->db_query("SELECT * FROM information_schema.role_table_grants 
 	}
 }
 
+// lehre.tbl_betreuerart: add type Senatsvorsitz
+if($result = $db->db_query("SELECT 1 FROM lehre.tbl_betreuerart WHERE betreuerart_kurzbz='Senatsvorsitz'"))
+{
+	if($db->db_num_rows($result)==0)
+	{
+	$qry = "INSERT INTO lehre.tbl_betreuerart(betreuerart_kurzbz, beschreibung) VALUES('Senatsvorsitz', 'Senatsvorsitz kommissionelle Prüfung');";
+
+		if(!$db->db_query($qry))
+			echo '<strong>Betreuerart: '.$db->db_last_error().'</strong><br>';
+		else
+			echo '<br>Neue Betreuerart Senatsvorsitz in lehre.tbl_betreuerart hinzugefügt';
+	}
+}
+
+// lehre.tbl_betreuerart: add type Senatsprüfer
+if($result = $db->db_query("SELECT 1 FROM lehre.tbl_betreuerart WHERE betreuerart_kurzbz='Senatspruefer'"))
+{
+	if($db->db_num_rows($result)==0)
+	{
+		$qry = "INSERT INTO lehre.tbl_betreuerart(betreuerart_kurzbz, beschreibung) VALUES('Senatspruefer', 'Senatsprüfer kommissionelle Prüfung');";
+
+		if(!$db->db_query($qry))
+			echo '<strong>Betreuerart: '.$db->db_last_error().'</strong><br>';
+		else
+			echo '<br>Neue Betreuerart Senatspruefer in lehre.tbl_betreuerart hinzugefügt';
+	}
+}
+
 // *** Pruefung und hinzufuegen der neuen Attribute und Tabellen
 echo '<H2>Pruefe Tabellen und Attribute!</H2>';
 
