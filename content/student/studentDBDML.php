bfb24320d75edd21567c60de6dccb57440519b0e
ma0048 <ma0048@technikum-wien.at>
2022-10-14 11:11:25 +0200

    - editieren der ZGV Master nur noch mit eigenen Recht moeglich

--- content/student/studentDBDML.php
+++ content/student/studentDBDML.php
@@ -821,7 +821,16 @@ if(!$error)
 				$prestudent->zgvdatum = $_POST['zgvdatum'];
 				$prestudent->zgvnation = $_POST['zgvnation'];
 				$prestudent->zgv_erfuellt = $_POST['zgv_erfuellt'];
-				$prestudent->zgvmas_code = $_POST['zgvmas_code'];
+				// Die Master-ZGV darf nur mit einem eigenen Recht geändert werden
+				if($rechte->isBerechtigt('student/editMakkZgv',$_POST['studiengang_kz'],'suid'))
+				{
+					$prestudent->zgvmas_code = $_POST['zgvmas_code'];
+				}
+				elseif ($prestudent->zgvmas_code != $_POST['zgvmas_code'])
+				{
+					$errormsg = 'Keine Berechtigung zum Ändern der ZGV';
+					$error = true;
+				}
 				$prestudent->zgvmaort = $_POST['zgvmaort'];
 				$prestudent->zgvmadatum = $_POST['zgvmadatum'];
 				$prestudent->zgvmanation = $_POST['zgvmanation'];
