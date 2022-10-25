1d6c5b42576ece9e91fcc7a28f09411157e01341
Cris <hainberg@technikum-wien.at>
2022-10-25 14:17:32 +0200

    Added Url Widget

--- application/models/content/News_model.php
+++ application/models/content/News_model.php
@@ -11,4 +11,18 @@ class News_model extends DB_Model
 		$this->dbTable = 'campus.tbl_news';
 		$this->pk = 'news_id';
 	}
+
+    public function getAll($limit = null)
+    {
+        // TODO check ob über content table. Aktuell sind die news texte NULL, deshalb über content table holen.
+//        $this->addJoin('campus.tbl_content', 'content_id');
+//        $this->addJoin('campus.tbl_contentsprache', 'content_id');
+
+        return $this->loadWhere('
+            text IS NOT NULL
+            AND datum <= NOW() AND (datum_bis IS NULL OR datum_bis >= now()::date)
+            ORDER BY datum DESC
+            LIMIT '. $this->escape($limit)
+        );
+    }
 }
