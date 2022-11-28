7cdb7688c4cb858c0fca8044c0ea4cc53276db89
Cris <hainberg@technikum-wien.at>
2022-11-28 17:27:39 +0100

    Fixed: Vergangene Anrechnungen können jetzt eingesehen werden
    Manche vergangene Anrechnungen konnten nicht eingesehen werden, weil
    die Zuordnung der Studenten zu den Lehrveranstaltungen aufgelöst bzw. geändert werden.
    Gefixt, indem nun bei einer bestehenden Anrechnung die bei der Anrechnung hinterlegten Lehrveranstaltung
    herangezogen wird.

--- application/controllers/lehre/anrechnung/ApproveAnrechnungDetail.php
+++ application/controllers/lehre/anrechnung/ApproveAnrechnungDetail.php
@@ -90,7 +90,8 @@ class approveAnrechnungDetail extends Auth_Controller
 		$antragData = $this->anrechnunglib->getAntragData(
 			$anrechnungData->prestudent_id,
 			$anrechnungData->studiensemester_kurzbz,
-			$anrechnungData->lehrveranstaltung_id
+			$anrechnungData->lehrveranstaltung_id,
+            $anrechnungData->anrechnung_id
 		);
 
 		// Get Empfehlung data
--- application/controllers/lehre/anrechnung/ReviewAnrechnungDetail.php
+++ application/controllers/lehre/anrechnung/ReviewAnrechnungDetail.php
@@ -84,7 +84,8 @@ class reviewAnrechnungDetail extends Auth_Controller
 		$antragData = $this->anrechnunglib->getAntragData(
 			$anrechnungData->prestudent_id,
 			$anrechnungData->studiensemester_kurzbz,
-			$anrechnungData->lehrveranstaltung_id
+			$anrechnungData->lehrveranstaltung_id,
+            $anrechnungData->anrechnung_id
 		);
 
 		// Get Empfehlung data
