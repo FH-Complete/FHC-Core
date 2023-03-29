9e4489093c4e10e9b57da51e2b55a7e3e003f99a
Cris <hainberg@technikum-wien.at>
2023-03-29 10:29:06 +0200

    Adapted & updated Tabulator default options (PersistenceID now changeable / Updates for options in Tabulator 4.9)
    . Adapted
    .. PersistenceID now changeable
    .. check: only set default option if not set in tabulator
    
    . Updates in version 4.9:
    .. persistentLayout => persistence
    .. getVisibility() => isVisible()

--- application/views/lehre/anrechnung/approveAnrechnungUebersichtData.php
+++ application/views/lehre/anrechnung/approveAnrechnungUebersichtData.php
@@ -167,9 +167,7 @@ $filterWidgetArray = array(
 	'datasetRepOptions' => '{
 		height: func_height(this),
 		layout: "fitColumns",           // fit columns to width of table
-		persistentLayout:true,
-		persistentSort:true,
-		persistentFilter:true,
+		persistenceID: "approveAnrechnungUebersicht_V1",
 		autoResize: false, 				// prevent auto resizing of table (false to allow adapting table size when cols are (de-)activated
 	    headerFilterPlaceholder: " ",
         index: "anrechnung_id",             // assign specific column as unique id (important for row indexing)
