2a59fad2fcf88f0f689ac68e3620603eec079b3d
cgfhtw <christopher.gerbrich@technikum-wien.at>
2023-03-31 09:21:52 +0200

    CMS loading speedup

--- public/js/apps/Cis/Cms.js
+++ public/js/apps/Cis/Cms.js
@@ -1,23 +1,20 @@
 import BsConfirm from "../../components/Bootstrap/Confirm.js";
 
 Vue.createApp({
-    components: {
-        BsConfirm
-    },
-    mounted() {
-    	document.querySelectorAll('#cms [data-confirm]').forEach(el => {
-            el.addEventListener('click', evt => {
-                evt.preventDefault();
-                BsConfirm.popup(el.dataset.confirm).then(() => {
-                    Axios.get(el.href).then(res => {
-                        // TODO(chris): check for success then show message and/or reload
-                        location = location;
-                    }).catch(err => console.error('ERROR:', err));
-                }).catch(()=>{});
-            });
-        });
-        document.querySelectorAll('#cms [data-href]').forEach(el => {
-            el.href = el.dataset.href.replace(/^ROOT\//, FHC_JS_DATA_STORAGE_OBJECT.app_root);
-        });
-    }
-}).mount('#cms');
+	mounted() {
+		document.querySelectorAll('#cms [data-confirm]').forEach(el => {
+			el.addEventListener('click', evt => {
+				evt.preventDefault();
+				BsConfirm.popup(el.dataset.confirm).then(() => {
+					Axios.get(el.href).then(res => {
+						// TODO(chris): check for success then show message and/or reload
+						location = location;
+					}).catch(err => console.error('ERROR:', err));
+				}).catch(()=>{});
+			});
+		});
+		document.querySelectorAll('#cms [data-href]').forEach(el => {
+			el.href = el.dataset.href.replace(/^ROOT\//, FHC_JS_DATA_STORAGE_OBJECT.app_root);
+		});
+	}
+}).mount('#cms [data-confirm], #cms [data-href]');
