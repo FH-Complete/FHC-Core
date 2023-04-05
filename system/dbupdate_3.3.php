5e799aa6e123f29ee13abe31eee77fff8b1bbb22
Paolo <bison@technikum-wien.at>
2023-04-05 14:31:00 +0200

    Moved DB changes from system/dbupdate_3.3.php to system/dbupdate_3.4/13011_installation_on_multiple_servers.php

--- system/dbupdate_3.3.php
+++ system/dbupdate_3.3.php
@@ -1012,39 +1012,13 @@ if ($result = @$db->db_query("SELECT conname FROM pg_constraint WHERE conname = 
 // UNIQUE INDEX uidx_extensions_name_version
 if ($result = $db->db_query("SELECT 0 FROM pg_class WHERE relname = 'uidx_extensions_name_version'"))
 {
-	if ($db->db_num_rows($result) == 1)
+	if ($db->db_num_rows($result) == 0)
 	{
-		$qry = 'DROP INDEX system.uidx_extensions_name_version';
+		$qry = 'CREATE UNIQUE INDEX uidx_extensions_name_version ON system.tbl_extensions USING btree (name, version);';
 		if (!$db->db_query($qry))
 			echo '<strong>uidx_extensions_name_version '.$db->db_last_error().'</strong><br>';
 		else
-			echo '<br>Dropped unique uidx_extensions_name_version';
-	}
-}
-
-// stud_selfservice boolean fuer public.tbl_vorlage
-if(!@$db->db_query("SELECT server_kurzbz FROM system.tbl_extensions LIMIT 1"))
-{
-	$qry = "ALTER TABLE system.tbl_extensions ADD COLUMN server_kurzbz varchar(64);
-			ALTER TABLE system.tbl_extensions ADD CONSTRAINT fk_extensios_server_kurzbz FOREIGN KEY (server_kurzbz) REFERENCES system.tbl_server(server_kurzbz) ON UPDATE CASCADE ON DELETE RESTRICT;";
-
-	if(!$db->db_query($qry))
-		echo '<strong>App: '.$db->db_last_error().'</strong><br>';
-	else
-		echo '<br>Neue Spalte server_kurzbz in system.tbl_extensions hinzugefügt';
-}
-
-
-// UNIQUE INDEX uidx_extensions_name_version_server
-if ($result = $db->db_query("SELECT 0 FROM pg_class WHERE relname = 'uidx_extensions_name_version_server'"))
-{
-	if ($db->db_num_rows($result) == 0)
-	{
-		$qry = 'CREATE UNIQUE INDEX uidx_extensions_name_version_server ON system.tbl_extensions USING btree (name, version, server_kurzbz);';
-		if (!$db->db_query($qry))
-			echo '<strong>uidx_extensions_name_version_server '.$db->db_last_error().'</strong><br>';
-		else
-			echo '<br>Created unique uidx_extensions_name_version_server';
+			echo '<br>Created unique uidx_extensions_name_version';
 	}
 }
 
@@ -7097,7 +7071,7 @@ $tabellen=array(
 	"system.tbl_cronjob"  => array("cronjob_id","server_kurzbz","titel","beschreibung","file","last_execute","aktiv","running","jahr","monat","tag","wochentag","stunde","minute","standalone","reihenfolge","updateamum", "updatevon","insertamum","insertvon","variablen"),
 	"system.tbl_benutzerrolle"  => array("benutzerberechtigung_id","rolle_kurzbz","berechtigung_kurzbz","uid","funktion_kurzbz","oe_kurzbz","art","studiensemester_kurzbz","start","ende","negativ","updateamum", "updatevon","insertamum","insertvon","kostenstelle_id","anmerkung"),
 	"system.tbl_berechtigung"  => array("berechtigung_kurzbz","beschreibung"),
-	"system.tbl_extensions" => array("extension_id","name","version","description","license","url","core_version","dependencies","enabled", "server_kurzbz"),
+	"system.tbl_extensions" => array("extension_id","name","version","description","license","url","core_version","dependencies","enabled"),
 	"system.tbl_fehler" => array("fehlercode","fehler_kurzbz","fehlercode_extern","fehlertext","fehlertyp_kurzbz","app"),
 	"system.tbl_fehlertyp" => array("fehlertyp_kurzbz","bezeichnung_mehrsprachig"),
 	"system.tbl_fehler_zustaendigkeiten" => array("fehlerzustaendigkeiten_id","fehlercode","person_id","oe_kurzbz","funktion_kurzbz", "insertamum", "insertvon"),
