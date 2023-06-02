f5ee097b72eb37c7ce10f976c7b3a6c0ae8709b9
ma0048 <ma0048@technikum-wien.at>
2023-06-02 12:01:31 +0200

    - doppelte benutzer bug fix

--- application/models/person/Person_model.php
+++ application/models/person/Person_model.php
@@ -326,10 +326,12 @@ class Person_model extends DB_Model
 
 				SELECT p2.person_id
 				FROM tbl_person p1
+				JOIN tbl_prestudent ps ON p1.person_id = ps.person_id
 				INNER JOIN (
-					SELECT vorname, nachname, gebdatum, person_id
-					FROM tbl_person
-					) p2
+					SELECT vorname, nachname, gebdatum, person.person_id
+					FROM tbl_person person
+					JOIN tbl_prestudent sps ON person.person_id = sps.person_id
+				) p2
 					ON (lower(p1.vorname) = lower(p2.vorname) AND lower(p1.nachname) = lower(p2.nachname) AND p1.gebdatum = p2.gebdatum)
 				WHERE p1.person_id != p2.person_id AND (p1.person_id = ?)";
 
