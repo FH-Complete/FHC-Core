bcdf29f57fa4a06f671f9cf3a188f8b0e2ba57ff
Werner Masik <werner@gefi.at>
2023-06-06 16:24:57 +0200

    added method to display rudimentary chart data

--- application/models/vertragsbestandteil/Gehaltsbestandteil_model.php
+++ application/models/vertragsbestandteil/Gehaltsbestandteil_model.php
@@ -75,40 +75,14 @@ class Gehaltsbestandteil_model extends DB_Model implements IEncryption
     {		
 
 		$qry = "
-        SELECT
-            gehaltsbestandteil_id,
-            gbt.von,
-            gbt.bis,
-            gbt.anmerkung,
-            gbt.dienstverhaeltnis_id,
-            gehaltstyp_kurzbz,
-            valorisierungssperre,
-            gbt.valorisierung,
-            grundbetrag as grund_betrag_decrypted,
-            betrag_valorisiert as betrag_val_decrypted,
-            gt.bezeichnung as gehaltstyp_bezeichnung,
-			vb.vertragsbestandteiltyp_kurzbz,
-			bf.funktion_kurzbz,
-			bf.oe_kurzbz,
-			fkt.beschreibung as fkt_beschreibung,
-			fb.bezeichnung as fb_bezeichnung,
-			org.bezeichnung as org_bezeichnung,
-			freitext.freitexttyp_kurzbz,
-			freitext.titel as freitext_titel
-        FROM hr.tbl_gehaltsbestandteil gbt LEFT JOIN hr.tbl_gehaltstyp gt using(gehaltstyp_kurzbz)
-			LEFT JOIN hr.tbl_vertragsbestandteil vb using(vertragsbestandteil_id)
-			LEFT JOIN hr.tbl_vertragsbestandteil_funktion vbf using(vertragsbestandteil_id)
-			LEFT JOIN public.tbl_benutzerfunktion bf using(benutzerfunktion_id)
-			LEFT JOIN public.tbl_funktion fkt using(funktion_kurzbz)
-			LEFT JOIN public.tbl_fachbereich fb using(fachbereich_kurzbz)
-			LEFT JOIN public.tbl_organisationseinheit org on (bf.oe_kurzbz=org.oe_kurzbz)
-			LEFT JOIN hr.tbl_vertragsbestandteil_freitext freitext on(vb.vertragsbestandteil_id=freitext.vertragsbestandteil_id)
-        WHERE gbt.dienstverhaeltnis_id=? 
-        ORDER BY gt.sort
+        WITH gbt as
+			(select von,bis,grundbetrag as grund_betrag_decrypted  from hr.tbl_gehaltsbestandteil where dienstverhaeltnis_id=?)
+			select von,bis, (select sum(gbt.grund_betrag_decrypted) as sum_betrag  from gbt where gbt.von<=gbtmeta.von and (gbt.bis is null or gbt.bis>=gbtmeta.von)
+			) as summe from gbt as gbtmeta order by von,bis
         ";
 
         return $this->execQuery($qry,
-			array($dienstverhaeltnis_id, $datestring, $datestring),
+			array($dienstverhaeltnis_id),
 			$this->getEncryptedColumns());
     }
 	
