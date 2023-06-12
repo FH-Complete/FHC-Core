ab5dc7b1ac34f3f29e557828d7747ab5bbcd945c
KarpAlex <karpen_ko@hotmail.com>
2023-06-12 22:32:09 +0200

    plausichecks: removed plausicheck lib, instead, methods are directly in the library for the fehler

--- application/libraries/issues/plausichecks/AktSemesterNull.php
+++ application/libraries/issues/plausichecks/AktSemesterNull.php
@@ -21,7 +21,7 @@ class AktSemesterNull extends PlausiChecker
 		$studiengang_kz = isset($params['studiengang_kz']) ? $params['studiengang_kz'] : null;
 
 		// get all students failing the plausicheck
-		$prestudentRes = $this->_ci->plausichecklib->getAktSemesterNull($studiensemester_kurzbz, $studiengang_kz, null, $exkludierte_studiengang_kz);
+		$prestudentRes = $this->getAktSemesterNull($studiensemester_kurzbz, $studiengang_kz, null, $exkludierte_studiengang_kz);
 
 		if (isError($prestudentRes)) return $prestudentRes;
 
@@ -49,5 +49,52 @@ class AktSemesterNull extends PlausiChecker
 
 		// return the results
 		return success($results);
+	}
+
+	/**
+	 * Current Ausbildungssemester shouldn't be 0.
+	 * @param studiensemester_kurzbz string check is to be executed for certain Studiensemester
+	 * @param studiengang_kz int if check is to be executed for certain Studiengang
+	 * @param prestudent_id int if check is to be executed only for one prestudent
+	 * @param exkludierte_studiengang_kz array if certain Studiengänge have to be excluded from check
+	 * @return success with prestudents or error
+	 */
+	public function getAktSemesterNull($studiensemester_kurzbz, $studiengang_kz = null, $prestudent_id = null, $exkludierte_studiengang_kz = null)
+	{
+		$params = array($studiensemester_kurzbz);
+
+		$qry = "
+			SELECT
+				DISTINCT pre.person_id, pre.prestudent_id, stg.oe_kurzbz AS prestudent_stg_oe_kurzbz, prestat.studiensemester_kurzbz
+			FROM
+				public.tbl_prestudent pre
+				JOIN public.tbl_prestudentstatus prestat USING(prestudent_id)
+				JOIN public.tbl_studiengang stg USING(studiengang_kz)
+			WHERE
+				prestat.status_kurzbz != 'Incoming'
+				AND prestat.studiensemester_kurzbz = ?
+				AND ausbildungssemester = 0
+				AND stg.melderelevant
+				AND pre.bismelden";
+
+		if (isset($studiengang_kz))
+		{
+			$qry .= " AND stg.studiengang_kz = ?";
+			$params[] = $studiengang_kz;
+		}
+
+		if (isset($prestudent_id))
+		{
+			$qry .= " AND pre.prestudent_id = ?";
+			$params[] = $prestudent_id;
+		}
+
+		if (isset($exkludierte_studiengang_kz) && !isEmptyArray($exkludierte_studiengang_kz))
+		{
+			$qry .= " AND stg.studiengang_kz NOT IN ?";
+			$params[] = $exkludierte_studiengang_kz;
+		}
+
+		return $this->_db->execReadOnlyQuery($qry, $params);
 	}
 }
--- application/libraries/issues/resolvers/CORE_INOUT_0007.php
+++ application/libraries/issues/resolvers/CORE_INOUT_0007.php
@@ -17,10 +17,10 @@ class CORE_INOUT_0007 implements IIssueResolvedChecker
 
 		$this->_ci =& get_instance(); // get code igniter instance
 
-		$this->_ci->load->library('issues/PlausicheckLib');
+		$this->_ci->load->library('issues/plausichecks/IncomingHeimatNationOesterreich');
 
 		// check if issue persists
-		$checkRes = $this->_ci->plausichecklib->getIncomingHeimatNationOesterreich($params['studiensemester_kurzbz'], null, $params['issue_person_id']);
+		$checkRes = $this->_ci->incomingheimatnationoesterreich->getIncomingHeimatNationOesterreich($params['studiensemester_kurzbz'], null, $params['issue_person_id']);
 
 		if (isError($checkRes)) return $checkRes;
 
