a2d1465d612bc5d8ca200e9ce7a4821d45d75272
ma0048 <ma0048@technikum-wien.at>
2023-06-16 14:09:28 +0200

    - student_uid geloescht

--- system/dbupdate_3.4/29357_eine_uid_tbl_lehre_abschlusspruefung.php
+++ system/dbupdate_3.4/29357_eine_uid_tbl_lehre_abschlusspruefung.php
@@ -20,3 +20,14 @@ if(!$result = @$db->db_query("SELECT prestudent_id FROM lehre.tbl_abschlusspruef
 	else
 		echo '<br>lehre.tbl_abschlusspruefung: Index prestudent_id hinzugefuegt';
 }
+
+if ($result = @$db->db_query("SELECT student_uid FROM lehre.tbl_abschlusspruefung LIMIT 1;"))
+{
+	$qry = "ALTER TABLE lehre.tbl_abschlusspruefung DROP COLUMN student_uid;";
+	
+	if (!$db->db_query($qry))
+		echo '<strong>lehre.tbl_abschlusspruefung: ' . $db->db_last_error() . '</strong><br>';
+	else
+		echo 'lehre.tbl_abschlusspruefung: Spalte student_uid entfernt.<br>';
+}
+
