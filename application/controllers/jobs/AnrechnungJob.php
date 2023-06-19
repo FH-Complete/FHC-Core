4e22b1ac1c296cd61e9c33e14b811ad200a3d209
Cris <hainberg@technikum-wien.at>
2023-06-19 15:03:38 +0200

    Added: New Job sendMailRecommendationRequests
    Send Sancho mail to LV-Leitung (fallback Lectors) that were requested for recommendation yesterday.

--- application/controllers/jobs/AnrechnungJob.php
+++ application/controllers/jobs/AnrechnungJob.php
@@ -227,6 +227,74 @@ class AnrechnungJob extends JOB_Controller
 		$this->logInfo('SUCCEDED: Sending emails to STGL about yesterdays new Anrechnungen succeded.');
 	}
 
+    // Send Sancho mail to LV-Leitung (fallback Lectors) that were requested for recommendation yesterday.
+    public function sendMailRecommendationRequests(){
+
+        $this->logInfo('Start AnrechnungJob sendMailRecommendationRequests to inform lecturers about yesterdays requests for recommendation.');
+
+        // Get Anrechnungen, für die gestern eine Empfehlung angefragt worden ist
+        $this->AnrechnungModel->addSelect('astat.anrechnung_id, astat.datum, astat.insertamum');
+        $this->AnrechnungModel->addDistinct('astat.anrechnung_id');
+        $this->AnrechnungModel->addJoin('lehre.tbl_anrechnung_anrechnungstatus astat', 'anrechnung_id');
+
+        $result = $this->AnrechnungModel->loadWhere('
+            studiensemester_kurzbz = (SELECT studiensemester_kurzbz FROM tbl_studiensemester WHERE now()::date BETWEEN start AND ende)
+            AND genehmigt_von IS NULL                             
+            AND empfehlung_anrechnung IS NULL
+            AND status_kurzbz = '. $this->db->escape(self::ANRECHNUNGSTATUS_PROGRESSED_BY_LEKTOR) .' -- in Bearbeitung durch Lektor
+            AND NOW()::date = (astat.datum + interval \'1 day\')                    -- nur gestrige Empfehlungsanfrage
+            ORDER BY astat.anrechnung_id, astat.datum DESC, astat.insertamum DESC   -- nur letzten status dabei prüfen
+        ');
+
+        // Exit, wenn es gestern keine Empfehlungsanfragen gab
+        if (!hasData($result))
+        {
+            $this->logInfo('End AnrechnungJob sendMailRecommendationRequests, because no recommendations were requested yesterday.');
+            exit;
+        }
+
+        $anrechnung_id_arr = array_column(getData($result), 'anrechnung_id');
+
+        $arr_lvLector_arr = array();
+        foreach ($anrechnung_id_arr as $anrechnung_id)
+        {
+            $arr_lvLector_arr[]= $this->anrechnunglib->getLectors($anrechnung_id); // Returns LV Leitung. If not present, then all lectors of LV.
+        }
+
+        // Unique lector array to send only one mail per lector
+        $arr_lvLector_arr = array_unique($arr_lvLector_arr, SORT_REGULAR);
+
+        // Link to 'Anrechnungen prüfen' dashboard
+        $url =
+            CIS_ROOT. 'cis/index.php?menu='.
+            CIS_ROOT. 'cis/menu.php?content_id=&content='.
+            CIS_ROOT. index_page(). self::REVIEW_ANRECHNUNG_URI;
+
+        foreach ($arr_lvLector_arr as $lvLector_arr)
+        {
+            foreach ($lvLector_arr as $lector)
+            {
+                // Prepare mail content
+                $fields = array(
+                    'vorname'       => $lector->vorname,
+                    'stgl_name'     => 'Die Studiengangsleitung',
+                    'link'          => anchor($url, 'Anrechnungsanträge Übersicht')
+                );
+
+                // Send mail
+                sendSanchoMail(
+                    'AnrechnungEmpfehlungAnfordern',
+                    $fields,
+                    $lector->uid. '@'. DOMAIN,
+                    'Deine Empfehlung wird benötigt zur Anerkennung nachgewiesener Kenntnisse'
+                );
+            }
+        }
+
+        $this->logInfo('SUCCEDED AnrechnungJob sendMailRecommendationRequests');
+
+    }
+
 	/**
 	 * Send Sancho mail to students, whose Anrechnungen were approved 24 hours ago.
 	 */
