0f80b9d31b337766da425b90cc0c3ae20c489972
Cris <hainberg@technikum-wien.at>
2023-10-17 11:30:35 +0200

    Added Primevue-Toast and -Confirmdialog libraries to FHC-Footer

--- application/views/templates/FHC-Footer.php
+++ application/views/templates/FHC-Footer.php
@@ -123,6 +123,10 @@
 		generateJSsInclude('vendor/npm-asset/primevue/calendar/calendar.min.js');
 		generateJSsInclude('vendor/npm-asset/primevue/skeleton/skeleton.min.js');
 		generateJSsInclude('vendor/npm-asset/primevue/autocomplete/autocomplete.min.js');
+		generateJSsInclude('vendor/npm-asset/primevue/toast/toast.min.js');
+		generateJSsInclude('vendor/npm-asset/primevue/toastservice/toastservice.min.js');
+		generateJSsInclude('vendor/npm-asset/primevue/confirmdialog/confirmdialog.min.js');
+		generateJSsInclude('vendor/npm-asset/primevue/confirmationservice/confirmationservice.min.js');
 	}
 
 	// --------------------------------------------------------------------------------------------------------
