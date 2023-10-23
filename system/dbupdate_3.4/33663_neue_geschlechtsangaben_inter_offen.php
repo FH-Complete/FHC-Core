c37170937cb7fdabaf080d515a81528a39b6f168
ma0068 <ma0068@technikum-wien.at>
2023-10-23 13:38:23 +0200

    neue Geschlechtsangabe keine Angabe

---
+++ system/dbupdate_3.4/33663_neue_geschlechtsangaben_inter_offen.php
@@ -0,0 +1,44 @@
+<?php
+if (! defined('DB_NAME')) exit('No direct script access allowed');
+
+// Add geschlecht i {inter/inter}
+if($result = $db->db_query("SELECT 1 FROM public.tbl_geschlecht WHERE geschlecht = 'i'"))
+{
+	if($db->db_num_rows($result)==0)
+	{
+		$qry = "INSERT INTO public.tbl_geschlecht(geschlecht, bezeichnung_mehrsprachig, sort) VALUES ('i', '{\"inter\",\"inter\"}', 5)";
+
+		if(!$db->db_query($qry))
+			echo '<strong>Geschlecht: '.$db->db_last_error().'</strong><br>';
+		else
+			echo '<br>Neues Geschlecht i {inter/inter} in public.tbl_geschlecht hinzugefügt';
+	}
+}
+
+// Add geschlecht {offen/open}
+if($result = $db->db_query("SELECT 1 FROM public.tbl_geschlecht WHERE geschlecht = 'o'"))
+{
+	if($db->db_num_rows($result)==0)
+	{
+		$qry = "INSERT INTO public.tbl_geschlecht(geschlecht, bezeichnung_mehrsprachig, sort) VALUES ('o', '{\"offen\",\"open\"}', 6)";
+
+		if(!$db->db_query($qry))
+			echo '<strong>Geschlecht: '.$db->db_last_error().'</strong><br>';
+		else
+			echo '<br>Neues Geschlecht o {offen/open} in public.tbl_geschlecht hinzugefügt';
+	}
+}
+
+// Add geschlecht {offen/open}
+if($result = $db->db_query("SELECT 1 FROM public.tbl_geschlecht WHERE geschlecht = 'k'"))
+{
+	if($db->db_num_rows($result)==0)
+	{
+		$qry = "INSERT INTO public.tbl_geschlecht(geschlecht, bezeichnung_mehrsprachig, sort) VALUES ('k', '{\"keine Angabe\",\"not specified\"}', 7)";
+
+		if(!$db->db_query($qry))
+			echo '<strong>Geschlecht: '.$db->db_last_error().'</strong><br>';
+		else
+			echo '<br>Neues Geschlecht k {keine Angabe/not specified} in public.tbl_geschlecht hinzugefügt';
+	}
+}
