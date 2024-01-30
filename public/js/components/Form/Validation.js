ef3ee76fa1274e0c513d424fc27680daafa3299f
cgfhtw <christopher.gerbrich@technikum-wien.at>
2024-01-30 11:39:17 +0100

    Form Validation

---
+++ public/js/components/Form/Validation.js
@@ -0,0 +1,40 @@
+export default {
+	inject: [
+		'$registerToForm'
+	],
+	data() {
+		return {
+			feedback: {
+				success: [],
+				danger: []
+			}
+		};
+	},
+	methods: {
+		clearValidation() {
+			this.feedback = {
+				success: [],
+				danger: []
+			};
+		},
+		setFeedback(valid, feedback) {
+			if (!feedback)
+				feedback = [];
+			if (!Array.isArray(feedback))
+				feedback = [feedback];
+			this.feedback[valid ? 'success' : 'danger'] = feedback;
+		}
+	},
+	mounted() {
+		if (this.$registerToForm)
+			this.$registerToForm(this);
+	},
+	template: `
+	<template v-for="(arr, key) in feedback" :key="key">
+		<div v-for="msg in arr" :key="msg" class="alert alert-dismissible fade show" :class="'alert-' + key" role="alert">
+			{{msg}}
+			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+		</div>
+	</template>
+	`
+};
