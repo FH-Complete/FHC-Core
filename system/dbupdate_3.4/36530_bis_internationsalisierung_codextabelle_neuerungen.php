d9ec506c86005ecf3612265c06ce508831591811
Alexei Karpenko <karpen_ko@hotmail.com>
2024-02-23 16:54:51 +0100

    Codextabelle Mobilitaetsprogramm Neuerungen: changed name of some programs, set correct sichtbar and sichtbar_outgoing booleans

--- system/dbupdate_3.4/36530_bis_internationsalisierung_codextabelle_neuerungen.php
+++ system/dbupdate_3.4/36530_bis_internationsalisierung_codextabelle_neuerungen.php
@@ -11,11 +11,11 @@ if($result = @$db->db_query("SELECT 1 FROM bis.tbl_mobilitaetsprogramm WHERE mob
 				INSERT INTO bis.tbl_mobilitaetsprogramm(mobilitaetsprogramm_code, kurzbz, beschreibung, sichtbar, sichtbar_outgoing) VALUES
 				(43, 'AMinKunst', 'Auslandsstipendium des Bundesministeriums für Kunst', TRUE, TRUE),
 				(44, 'BundeslandProg', 'Bundesland-Programm', TRUE, TRUE),
-				(45, 'ErasmusSms', 'ERASMUS+ (SMS) - Studienaufenthalte', TRUE, TRUE),
-				(46, 'ErasmusSmt', 'ERASMUS+ (SMT) - Studierendenpraktika', TRUE, TRUE),
-				(47, 'ErasmusMundus', 'Erasmus Mundus Joint Master Degrees / Erasmus Mundus Joint Master', TRUE, TRUE),
+				(45, 'ERASMUSSMS', 'ERASMUS+ (SMS) - Studienaufenthalte', TRUE, TRUE),
+				(46, 'ERASMUSSMT', 'ERASMUS+ (SMT) - Studierendenpraktika', TRUE, TRUE),
+				(47, 'ERASMUSMundus', 'Erasmus Mundus Joint Master Degrees / Erasmus Mundus Joint Master', FALSE, FALSE),
 				(48, 'EUGrad', 'Mobilitätsprogramm für Graduierte im EU-Bereich', TRUE, TRUE),
-				(49, 'ÖStiftung', 'Stipendienstiftung der Republik Österreich', TRUE, TRUE),
+				(49, 'ÖStiftung', 'Stipendienstiftung der Republik Österreich', TRUE, FALSE),
 				(50, 'ÖAkadWiss', 'Stipendienabkommen der Österreichischen Akademie der Wissenschaften', TRUE, TRUE),
 				(51, 'FondWissForsch', 'Stipendium des Fonds zur Förderung der wissenschaftlichen Forschung', TRUE, TRUE),
 				(52, 'SEMP', 'Swiss-European Mobility Programme (SEMP)', TRUE, TRUE)
