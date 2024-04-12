29f7e34e10dd99f822a1e5f8b9d9c55aef5df198
SimonGschnell <simongschnell@gmail.com>
2024-04-12 14:38:01 +0200

    adds pagination logic to C4 news

--- application/controllers/CisHmvc/Cms.php
+++ application/controllers/CisHmvc/Cms.php
@@ -1,6 +1,7 @@
 <?php
 
-if (! defined('BASEPATH')) exit('No direct script access allowed');
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
 /**
  *
@@ -89,7 +90,9 @@ class Cms extends Auth_Controller
 	 */
 	public function news($infoscreen = false, $studiengang_kz = null, $semester = null, $mischen = true, $titel = '', $edit = false, $sichtbar = true)
 	{
-		$news = $this->cmslib->getNews($infoscreen, $studiengang_kz, $semester, $mischen, $titel, $edit, $sichtbar);
+		$page = intval($this->input->get('page', true));
+		$pagination_size = 10;
+		$news = $this->cmslib->getNews($infoscreen, $studiengang_kz, $semester, $mischen, $titel, $edit, $sichtbar, $page, $pagination_size);
 
 		if (isError($news))
 			return $this->load->view('CisHmvc/Error', ['error' => getError($news)]);
