7bd70a643657db0b0f88336271f6257b6c12dc5e
SimonGschnell <simongschnell@gmail.com>
2024-04-17 14:54:22 +0200

    makes it possible to send the page_size from the frontend to the backend to adopt how many rows to query

--- public/js/components/Pagination/Pagination.js
+++ public/js/components/Pagination/Pagination.js
@@ -16,17 +16,14 @@ export default {
       //console.log("hier", data.page);
     },
   },
-  mounted() {
-    console.log("pagination mounted");
-  },
+  mounted() {},
   template: /*html*/ `
     
-    <pre>{{JSON.stringify(maxPageCount,null,2)}}</pre>
+    
     <paginator @page="(data)=>$emit('page',{...data, page:data.page+1})" :rows="10" :totalRecords="maxPageCount" :rowsPerPageOptions="[10, 20, 30]" ></paginator>
     <slot>
     Placeholder
     </slot>
-    <paginator :rows="10" :totalRecords="120" :rowsPerPageOptions="[10, 20, 30]"></paginator>
-
+   
   `,
 };
