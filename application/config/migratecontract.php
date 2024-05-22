8cba0aec5a1e6afcde5e385a860487a81b2411cf
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-05-22 15:32:28 +0200

    vertragsarten dvbund, dvanderegk und dvanderebet hinzugefuegt, MigrateContract default oe und ba1code mapping in config verschoben und config check implementiert

---
+++ application/config/migratecontract.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright (C) 2023 fhcomplete.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['migratecontract_oe_default'] = 'TODO_OE_DEFAULT';
+
+$config['migratecontract_matching_ba1_vertragsart'] = array(
+    '101'=>'dvbund',
+    '102'=>'dvanderengk',
+    '103'=>'echterdv',
+    '104'=>'studentischehilfskr',
+    '105'=>'externerlehrender',
+    '106'=>'dvanderenbet',
+    '107'=>'werkvertrag',
+    '108'=>'studentischehilfskr',
+    '109'=>'ueberlassungsvertrag',
+    '110'=>'echterfreier',
+    '111'=>'echterdv' //All-In
+);
