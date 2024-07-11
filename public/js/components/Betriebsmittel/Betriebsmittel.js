9d455c35d0b26bfd954003384ad0204a1f4b77eb
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-07-11 18:32:47 +0200

    Betriebsmittel.js: use arrow function for dynamic ajaxParams, Filter.js: remove required from filterType

--- public/js/components/Betriebsmittel/Betriebsmittel.js
+++ public/js/components/Betriebsmittel/Betriebsmittel.js
@@ -36,10 +36,12 @@ export default {
 			tabulatorOptions: {
 				ajaxURL: 'dummy',
 				ajaxRequestFunc: this.endpoint.getAllBetriebsmittel,
-				ajaxParams: {
-					type: this.typeId,
-					id: this.id
-				},
+				ajaxParams: () => {
+                                    return {
+                                        type: this.typeId,
+                                        id: this.id
+                                    }
+                                },
 				ajaxResponse: (url, params, response) => response.data,
 				columns: [
 					{title: "Nummer", field: "nummer", width: 150},
@@ -149,7 +151,7 @@ export default {
 	},
 	watch: {
 		id() {
-			this.$refs.table.tabulator.setData('dummy', {type: this.typeId, id: this.id});
+			this.$refs.table.tabulator.setData('dummy');
 		}
 	},
 	methods: {
