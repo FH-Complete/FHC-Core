5d169e188a082215538a8d624307c2d5359f97da
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-08-13 14:12:55 +0200

    move permissions from abstract to concrete Notiz Controller

--- application/controllers/api/frontend/v1/notiz/NotizPerson.php
+++ application/controllers/api/frontend/v1/notiz/NotizPerson.php
@@ -9,6 +9,14 @@ class NotizPerson extends Notiz_Controller
 	public function __construct()
 	{
 		parent::__construct([
+			'getUid' => ['admin:r', 'assistenz:r'],
+			'getNotizen' => ['admin:r', 'assistenz:r'],
+			'loadNotiz' => ['admin:r', 'assistenz:r'],
+			'addNewNotiz' => ['admin:rw', 'assistenz:rw'],
+			'updateNotiz' => ['admin:rw', 'assistenz:rw'],
+			'deleteNotiz' => ['admin:rw', 'assistenz:rw'],
+			'loadDokumente' => ['admin:r', 'assistenz:r'],
+			'getMitarbeiter' => ['admin:r', 'assistenz:r'],
 			'isBerechtigt' => ['admin:r', 'assistenz:r'],
 		]);
 	}
