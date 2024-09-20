a2e4704223f854e4d5e51188cb7ce3a7f17b2601
Johann Hoffmann <ma0646@technikum-wien.at>
2024-09-20 13:12:50 +0200

    developing new components in lektor ui for ux improvements; anw function now explicitely in extension schema

--- public/js/plugin/FhcApi.js
+++ public/js/plugin/FhcApi.js
@@ -47,9 +47,8 @@ export default {
 			return result;
 		}
 		const baseURL = FHC_JS_DATA_STORAGE_OBJECT.app_root + FHC_JS_DATA_STORAGE_OBJECT.ci_router + "/";
-		console.log(baseURL)
 		const fhcApiAxios = axios.create({
-			timeout: 5000,
+			timeout: 500000,
 			baseURL: FHC_JS_DATA_STORAGE_OBJECT.app_root + FHC_JS_DATA_STORAGE_OBJECT.ci_router + "/"
 		});
 
