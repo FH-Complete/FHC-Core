0e385e8628a8c352392e10a06fe6a83135eb657f
SimonGschnell <simongschnell@gmail.com>
2024-09-30 10:30:11 +0200

    makes the menu of the CISVUE-Header also sticky now

--- public/js/components/Cis/Menu.js
+++ public/js/components/Cis/Menu.js
@@ -44,26 +44,28 @@ export default {
         <img :src="logoUrl" alt="Logo">
     </a>
     <nav id="nav-main" class="offcanvas offcanvas-start bg-dark" tabindex="-1" aria-labelledby="nav-main-btn" data-bs-backdrop="false">
-        <div id="nav-main-toggle" class="position-static d-none d-lg-block bg-dark">
-            <button type="button" class="btn bg-dark text-light rounded-0 p-1 d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#nav-main-menu" aria-expanded="true" aria-controls="nav-main-menu">
-                <i class="fa fa-arrow-circle-left"></i>
-            </button>
-        </div>
-        <div class="offcanvas-body p-0">
-            <button id="nav-user-btn" class="btn btn-link rounded-0" type="button" data-bs-toggle="collapse" data-bs-target="#nav-user-menu" aria-expanded="false" aria-controls="nav-user-menu">
-                <img :src="avatarUrl" class="avatar rounded-circle"/>
-            </button>
-            <ul id="nav-user-menu" class="collapse list-unstyled" aria-labelledby="nav-user-btn">
-                <li><a class="btn btn-level-2 rounded-0 d-block" href="#" id="menu-profil">Profil</a></li>
-                <li><a class="btn btn-level-2 rounded-0 d-block" href="#">Ampeln</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="btn btn-level-2 rounded-0 d-block" :href="logoutUrl">Logout</a></li>
-            </ul>
-            <div id="nav-main-menu" class="collapse collapse-horizontal show">
-                <div>
-                    <cis-menu-entry @activeEntry="setActiveEntry" :activeContent="activeEntry" v-for="entry in entries" :key="entry.content_id" :entry="entry" />
-                </div>
-            </div>
-        </div>
+		<div id="nav-main-sticky">
+			<div id="nav-main-toggle" class="position-static d-none d-lg-block bg-dark">
+				<button type="button" class="btn bg-dark text-light rounded-0 p-1 d-flex align-items-center" data-bs-toggle="collapse" data-bs-target="#nav-main-menu" aria-expanded="true" aria-controls="nav-main-menu">
+					<i class="fa fa-arrow-circle-left"></i>
+				</button>
+			</div>
+			<div class="offcanvas-body p-0">
+				<button id="nav-user-btn" class="btn btn-link rounded-0" type="button" data-bs-toggle="collapse" data-bs-target="#nav-user-menu" aria-expanded="false" aria-controls="nav-user-menu">
+					<img :src="avatarUrl" class="avatar rounded-circle"/>
+				</button>
+				<ul id="nav-user-menu" class="collapse list-unstyled" aria-labelledby="nav-user-btn">
+					<li><a class="btn btn-level-2 rounded-0 d-block" href="#" id="menu-profil">Profil</a></li>
+					<li><a class="btn btn-level-2 rounded-0 d-block" href="#">Ampeln</a></li>
+					<li><hr class="dropdown-divider"></li>
+					<li><a class="btn btn-level-2 rounded-0 d-block" :href="logoutUrl">Logout</a></li>
+				</ul>
+				<div id="nav-main-menu" class="collapse collapse-horizontal show">
+					<div>
+						<cis-menu-entry @activeEntry="setActiveEntry" :activeContent="activeEntry" v-for="entry in entries" :key="entry.content_id" :entry="entry" />
+					</div>
+				</div>
+			</div>
+		</div>
     </nav>`
 };
