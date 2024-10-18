0775fae67de7179007cdf5fc4f4f484a8ab177c1
Harald Bamberger <harald.bamberger@technikum-wien.at>
2024-10-18 16:49:52 +0200

    remove unnecessary comment

--- application/config/Events.php
+++ application/config/Events.php
@@ -3,8 +3,6 @@ if (! defined('BASEPATH')) exit('No direct script access allowed');
 
 use CI3_Events as Events;
 
-// build the menu for fhc 
-
 /**
  * NOTE(chris): example:
 	Events::on('stv_conf_student', function (&$res) {
