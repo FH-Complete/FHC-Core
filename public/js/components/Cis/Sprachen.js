8f07bd6b4dab33c0f1669032eb7d8b1739b27126
SimonGschnell <simongschnell@gmail.com>
2024-12-12 14:46:32 +0100

    update(Sprachen.js): shows the sprachen in the component always using their bezeichnung

--- public/js/components/Cis/Sprachen.js
+++ public/js/components/Cis/Sprachen.js
@@ -2,6 +2,7 @@ export default {
 	data(){
 		return {
 			allActiveLanguages: null,
+			sprachenTranslation:null,
 		}
 	}, 
 	methods:{
@@ -11,20 +12,25 @@ export default {
 				this.$p.setLanguage(lang, this.$fhcApi);
 			}
 		},
+		getSprachenBezeichnung: function(lang){
+			if (!Array.isArray(this.sprachenTranslation) || this.sprachenTranslation.length == 0) return;
+			return this.sprachenTranslation.find(s=>s.sprache == lang)?.bezeichnung;
+		},
 	},
 	mounted(){
 		this.$fhcApi.factory.phrasen.getActiveDbLanguages()
 			.then(res => res.data)
 			.then(
 				(langs) => {
-					this.allActiveLanguages = langs;
+					this.allActiveLanguages = langs.map(l=>l.sprache);
+					this.sprachenTranslation = langs;
 				}
 		);
 	},
 	template:/*html*/`
 	<div class="container">
 		<div class="row justify-content-center align-items-center flex-nowrap overflow-hidden">
-			<button v-for="lang in allActiveLanguages" @click.prevent="changeLanguage(lang)" class="col text-white fhc-entry btn text-center w-100" :selected="$p.user_language.value==lang?'':null">{{lang}}</button>
+			<button v-for="lang in allActiveLanguages" @click.prevent="changeLanguage(lang)" class="col text-white fhc-entry btn text-center w-100" :selected="$p.user_language.value==lang?'':null">{{getSprachenBezeichnung(lang)}}</button>
 		</div>
 	</div>
 	`,
