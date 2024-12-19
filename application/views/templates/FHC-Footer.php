14eb27ae248acdbc409d3b514804409548d76682
cgfhtw <christopher.gerbrich@technikum-wien.at>
2024-12-19 16:19:46 +0100

    add PrimeVue TieredMenu

--- application/views/templates/FHC-Footer.php
+++ application/views/templates/FHC-Footer.php
@@ -132,6 +132,7 @@
 		generateJSsInclude('vendor/npm-asset/primevue/toastservice/toastservice.min.js');
 		generateJSsInclude('vendor/npm-asset/primevue/confirmdialog/confirmdialog.min.js');
 		generateJSsInclude('vendor/npm-asset/primevue/confirmationservice/confirmationservice.min.js');
+		generateJSsInclude('vendor/npm-asset/primevue/tieredmenu/tieredmenu.min.js');
 	}
 
 	// --------------------------------------------------------------------------------------------------------
