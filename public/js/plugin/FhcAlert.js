09b2615d82f0dbbea05b6de678b7e48cf1395470
SimonGschnell <simongschnell@gmail.com>
2025-01-15 14:42:13 +0100

    update(FhcAlert.js): if the request was aborted, no error message is shown to the user

--- public/js/plugin/FhcAlert.js
+++ public/js/plugin/FhcAlert.js
@@ -263,6 +263,10 @@ export default {
 				return false;
 			},
 			handleSystemError(error) {
+				// don't show an error message to the user if the error was an aborted request
+				if(error.hasOwnProperty('name') && error.name.toLowerCase() === "AbortError".toLowerCase())
+					return;
+				
 				// Error is string
 				if (typeof error === 'string')
 					return $fhcAlert.alertSystemError(error);
@@ -278,6 +282,7 @@ export default {
 				// Error is object
 				if (typeof error === 'object' && error !== null) {
 					let errMsg = '';
+
 
 					if (error.hasOwnProperty('response') && error.response?.data?.retval)
 						errMsg += 'Error Message: ' + (error.response.data.retval.message || error.response.data.retval) + '\r\n';
