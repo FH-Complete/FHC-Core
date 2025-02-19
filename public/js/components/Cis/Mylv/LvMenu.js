5cb09c8e7b060179c659f897c8724df6079c3165
SimonGschnell <simongschnell@gmail.com>
2025-02-19 14:50:19 +0100

    fix(LvMenu): fixes the highlighting styles for the lvMenu, instead of using fhc-entry it uses its own class

--- public/js/components/Cis/Mylv/LvMenu.js
+++ public/js/components/Cis/Mylv/LvMenu.js
@@ -57,13 +57,13 @@ export default {
 	},
 	template:/*html*/`
 	<div v-if="!menu">No Menu available</div>
-	<div v-else>
+	<div id="cis-menu" v-else>
 		<div class="container" :class="containerStyles">
 			<div class="row g-2 justify-content-center" :class="rowStyles">
 							<div style="min-height:150px; min-width:150px;" class="col-12 col-lg-6 col-xl-4" v-for="(menuItem, index) in menu" :key="index">
 					<a :id="menuItem.name" :class="{'dropdown-toggle':menuItem.c4_moodle_links?.length }"  role="button" :href="c4_link(menuItem)"
 					:disabled="c4_disabled(menuItem)" :data-bs-toggle="menuItem.c4_moodle_links?.length?'dropdown':null"
-					class="fhc-entry p-2 w-100 text-wrap border border-1 rounded-3 d-flex flex-column align-items-center justify-content-center text-center text-decoration-none link-dark h-100">
+					class="menu-entry p-2 w-100 text-wrap border border-1 rounded-3 d-flex flex-column align-items-center justify-content-center text-center text-decoration-none link-dark h-100">
 						<img :src="menuItem.c4_icon" :alt="menuItem.name" />
 						<p @click="selectMenu(menuItem)" class="w-100 mt-2">{{menuItem.name}}</p>
 						<p v-for="([text,link],index) in menuItem.c4_linkList" @click.stop="selectMenu(menuItem,index)" class="mt-1 w-100" :index="index">{{text}}</p>
