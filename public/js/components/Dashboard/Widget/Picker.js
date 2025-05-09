a99721bb2204bf2dcfccd34c48406ab2e5f42dc8
SimonGschnell <simongschnell@gmail.com>
2025-05-09 09:34:12 +0200

    feature(Dashboard Mobile): the modal to add new widgets distributes the space to display widgetes more evenly

--- public/js/components/Dashboard/Widget/Picker.js
+++ public/js/components/Dashboard/Widget/Picker.js
@@ -36,11 +36,11 @@ export default {
 		<bs-modal ref="modal" class="fade" :dialog-class="{'modal-fullscreen-sm-down': 1, 'modal-xl': widgets && widgets.length > 0}" @hiddenBsModal="close">
 			<template v-slot:title>Create new widget</template>
 			<template v-slot:default>
-				<div v-if="widgets" class="row">
+				<div v-if="widgets" class="row g-2">
 					<div v-if="!widgets.length">
 						No Widgets available
 					</div>
-					<div v-for="widget in widgets" :key="widget.widget_id" class="col-sm-6 col-md-4 col-lg-3 col-xl-2">
+					<div v-for="widget in widgets" :key="widget.widget_id" class="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-2">
 						<widget-icon @select="pick" :widget="widget" ></widget-icon>
 					</div>
 				</div>
