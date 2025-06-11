49c510537a6b5d52fdfbf0691ee44a26d3b70cc3
SimonGschnell <simongschnell@gmail.com>
2025-06-11 11:06:35 +0200

    fix(Profil Mailverteiler): adds safeguards and checks on the return value of the Mailverteiler of a person

--- public/js/components/Cis/Profil/ProfilComponents/Mailverteiler.js
+++ public/js/components/Cis/Profil/ProfilComponents/Mailverteiler.js
@@ -10,6 +10,7 @@ export default {
 
     },
     template: /*html*/`
+	<template v-if="Array.isArray(data) && data.length >0">
     <div class="card">
         <div class="card-header">
             {{title}}
@@ -18,7 +19,7 @@ export default {
             <h6 class="card-title">{{$p.t('profil','mailverteilerMitglied')}}</h6>
             <div class="card-text row text-break mb-2" v-for="verteiler in data">
                 <div class="col-12 ">
-                    <div class="row">  
+                    <div class="row">
                         <div class="col-1 ">
                             <i class="fa-solid fa-envelope" style="color: #00649C;"></i>
                         </div>
@@ -26,9 +27,10 @@ export default {
                             <a :href="verteiler.mailto"><b>{{verteiler.gruppe_kurzbz}}</b></a>
                         </div>
                     </div>
-                </div> 
+                </div>
                 <div class="col-11 offset-1 ">{{verteiler.beschreibung}}</div>
             </div>
         </div>
-    </div>`,
+    </div>
+	</template>`,
 };
