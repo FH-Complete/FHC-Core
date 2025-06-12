44b538885f6a642c36a98eef9dbd9c4b2fd42c58
Harald Bamberger <harald.bamberger@technikum-wien.at>
2025-06-12 11:40:24 +0200

    use additional result variable

--- application/controllers/api/frontend/v1/LvPlan.php
+++ application/controllers/api/frontend/v1/LvPlan.php
@@ -81,7 +81,8 @@ class LvPlan extends FHCAPI_Controller
 		$end_date = $this->input->get('end_date', TRUE);
 		$lv_id = $this->input->get('lv_id', TRUE);
 
-		$lvplan_events = $this->getDataOrTerminateWithError($this->lvplanlib->getLvPlan($start_date,$end_date,$lv_id));
+		$res_lvplan_events = $this->lvplanlib->getLvPlan($start_date,$end_date,$lv_id);
+		$lvplan_events = $this->getDataOrTerminateWithError($res_lvplan_events);
 		if( is_null($lvplan_events) || isEmptyArray($lvplan_events) )
 		{
 			$lvplan_events = array();
