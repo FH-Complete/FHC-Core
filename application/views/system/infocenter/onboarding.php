02df06288e7590e04c4fed0799651687d19cb05d
Andreas Österreicher <oesi@technikum-wien.at>
2025-06-16 11:37:04 +0200

    Bugfix Rückstellgrund

--- application/views/system/infocenter/onboarding.php
+++ application/views/system/infocenter/onboarding.php
@@ -19,7 +19,7 @@
 				'ui' => array('bitteEintragWaehlen')
 			),
 			'customCSSs' => array('public/css/sbadmin2/tablesort_bootstrap.css', 'public/css/infocenter/infocenterPersonDataset.css'),
-			'customJSs' => array('public/js/bootstrapper.js', 'public/js/infocenter/infocenterPersonDataset.js')
+			'customJSs' => array('public/js/bootstrapper.js', 'public/js/infocenter/rueckstellung.js', 'public/js/infocenter/infocenterPersonDataset.js')
 		)
 	);
 ?>
