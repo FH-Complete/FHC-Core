e99cbc9f3d49f5a2b18f2aff5e257300c7d86c9b
SimonGschnell <simongschnell@gmail.com>
2025-06-24 13:28:32 +0200

    fix(lvMenu optionen):fixes click event bugs with the lv menu and changes the styling for some lvmenu options

--- public/js/components/Cis/Mylv/LvMenu.js
+++ public/js/components/Cis/Mylv/LvMenu.js
@@ -32,25 +32,6 @@ export default {
 			if (menuItem.c4_moodle_links?.length > 0) return null;
 			return menuItem.c4_target ?? null;
 		},
-		selectMenu: function (menuItem, index = null) {
-
-			// early return if link is #
-			if (index != null && menuItem.c4_linkList[index][1] == '#') return;
-
-			switch (menuItem.id) {
-				case "core_menu_mailanstudierende": window.location.href = menuItem.c4_link; break;
-				default:
-					this.selectedMenu = { ...menuItem };
-					this.$emit("update:isMenuSelected", true);
-			}
-
-			if (this.selectedMenu && index != null && menuItem.c4_linkList[index][1] != '#') {
-				this.selectedMenu.c4_link = menuItem.c4_linkList[index][1];
-				this.selectedMenu.name += ' - ' + menuItem.c4_linkList[index][0];
-			}
-
-			
-		},
 		c4_link(menuItem) {
 			if (!menuItem) return null;
 			if (Array.isArray(menuItem.c4_moodle_links) && menuItem.c4_moodle_links.length) 
@@ -73,8 +54,8 @@ export default {
 					:disabled="c4_disabled(menuItem)" :data-bs-toggle="menuItem.c4_moodle_links?.length?'dropdown':null"
 					class="menu-entry p-2 w-100 text-wrap border border-1 rounded-3 d-flex flex-column align-items-center justify-content-center text-center text-decoration-none link h-100">
 						<img :src="menuItem.c4_icon" :alt="menuItem.name" />
-						<p @click="selectMenu(menuItem)" class="w-100 mt-2">{{menuItem.name}}</p>
-						<p v-for="([text,link],index) in menuItem.c4_linkList" @click.stop="selectMenu(menuItem,index)" class="mt-1 w-100" :index="index">{{text}}</p>
+						<p class="w-100 mt-2 mb-0">{{menuItem.name}}</p>
+						<a v-for="([text,link],index) in menuItem.c4_linkList" target="_blank" :href="link" class="my-1 w-100 submenu text-decoration-none" :index="index">{{text}}</p>
 					</a>
 					<ul v-if="menuItem.c4_moodle_links?.length" class="dropdown-menu p-0" :aria-labelledby="menuItem.name">
 						<li v-for="item in menuItem.c4_moodle_links"><a class="dropdown-item border-bottom" :href="item.url">{{item.lehrform}}</a></li>
