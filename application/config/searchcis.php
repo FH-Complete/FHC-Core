cc91fe7588e1325fc7c6287aebd4655c03e11c8c
Harald Bamberger <harald.bamberger@technikum-wien.at>
2025-08-04 16:17:38 +0200

    searchcis only students and employees with active useraccount

--- application/config/searchcis.php
+++ application/config/searchcis.php
@@ -7,6 +7,15 @@ $CI =& get_instance();
 
 
 $config['employee'] = $CI->config->item('employee', 'search');
+$config['employee']['resultjoin'] = "
+		JOIN public.tbl_mitarbeiter m USING (mitarbeiter_uid)
+		JOIN public.tbl_benutzer b ON (b.uid = m.mitarbeiter_uid AND b.aktiv = true)
+		JOIN public.tbl_person p USING(person_id)
+		LEFT JOIN (
+			SELECT kontakt, standort_id
+			FROM public.tbl_kontakt
+			WHERE kontakttyp = 'telefon'
+		) k ON (k.standort_id = m.standort_id)";
 
 $config['student'] = $CI->config->item('student', 'search');
 unset($config['student']['searchfields']['email']);
@@ -23,6 +32,10 @@ $config['student']['resultfields'] = [
 		AS photo_url",
 	"b.aktiv"
 ];
+$config['student']['resultjoin'] = "
+		JOIN public.tbl_student s USING (student_uid)
+		JOIN public.tbl_benutzer b ON(b.uid = s.student_uid AND b.aktiv = true)
+		JOIN public.tbl_person p USING(person_id)";
 
 $config['organisationunit'] = $CI->config->item('organisationunit', 'search');
 $config['organisationunit']['prepare'] = 'active_organisationseinheit AS (SELECT * FROM public.tbl_organisationseinheit WHERE aktiv = true)';
