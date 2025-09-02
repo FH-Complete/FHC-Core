396251b0613d8fea526f8382d5702fd1b1fb44a2
ma0048 <ma0048@technikum-wien.at>
2025-09-02 13:35:15 +0200

    lvverwaltung einschraenkung bei den tags erledigte tags werden in der header suche nicht beruecksichtigt

---
+++ application/config/lvverwaltung.php
@@ -0,0 +1,11 @@
+<?php
+
+$config['lvverwaltung_tags'] = [
+	'hinweis_stg_kf' => ['readonly' => false],
+	'hinweis' => ['readonly' => false],
+	'hinweis_assistenz' => ['readonly' => false],
+	'finished_stg' => ['readonly' => false],
+
+	'finished_kf' => ['readonly' => true],
+	'inwork_kf' => ['readonly' => true],
+];
