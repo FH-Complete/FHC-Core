34111c8e4ec5ab706f68568c516ff2644c14e197
Alexei Karpenko <karpen_ko@hotmail.com>
2025-11-04 13:06:36 +0100

    kennzeichen sequence privilege: removed author block

--- system/dbupdate_3.4/69060_permission_fuer_kennzeichen_sequence_fuer_web_user.php
+++ system/dbupdate_3.4/69060_permission_fuer_kennzeichen_sequence_fuer_web_user.php
@@ -15,10 +15,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
- * Authors: Harald Bamberger <harald.bamberger@technikum-wien.at>,
- *
- * Beschreibung:
- * Dashboard DB Aenderungen
  */
 if (! defined('DB_NAME')) exit('No direct script access allowed');
 
