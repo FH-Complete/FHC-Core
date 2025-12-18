34dca18a462a5add2a15082cf230676820b36235
Paolo <bison@technikum-wien.at>
2025-12-18 13:14:48 +0100

    - Added PHPMailer to composer - Changed application/libraries/MailLib.php to use PHPMailer - Changed application/config/mail.php to be used by MailLib + PHPMailer

--- application/config/mail.php
+++ application/config/mail.php
@@ -4,34 +4,31 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 // Define configuration parameters
 $config['email_number_to_sent'] = 1000; // Number of emails to sent each time sendAll is called
-$config['email_number_per_time_range'] = 1; // Number of emails to sent before pause
+$config['email_number_per_time_range'] = 882; // Number of emails to sent before pause
 $config['email_time_range'] = 1; // Length of the pause in seconds
 $config['email_from_system'] = 'no-reply@technikum-wien.at';
 $config['alias_from_system'] = 'No Reply';
 
-// Smtp: if the CI email library has to connect to a smtp server
-// Mail: if the system is setup to send emails with the standard php mail function
-// Sendmail: if the system is setup to send email via Sendmail (or similar)
-$config['protocol'] = ''; // mail, sendmail, or smtp
-
-// If protocol is set to sendmail
-$config['mailpath'] = ''; // SThe server path to Sendmail (or similar)
-
 // If protocol is set to smtp
 $config['smtp_host'] = 'localhost'; // SMTP Server Address
 $config['smtp_port'] = 25;
-$config['smtp_timeout'] = 5; // in seconds
+$config['smtp_timeout'] = 1; // in seconds
 $config['smtp_keepalive'] = false; // Enable persistent SMTP connections
+$config['smtp_auth'] = false;
 $config['smtp_user'] = '';
 $config['smtp_pass'] = '';
-$config['wordwrap'] = true; // {unwrap}http://example.com/a_long_link_that_should_not_be_wrapped.html{/unwrap}
-$config['wrapchars'] = 76; // Character count to wrap at.
-$config['mailtype'] = 'html'; // html or text
-$config['priority'] = 3; // Email Priority. 1 = highest. 5 = lowest. 3 = normal
-$config['validate'] = false; // If true then the email address will be validated
+$config['smtp_encryption'] = ''; // '', 'tls' or 'ssl'
+$config['wordwrap'] = 76;
+$config['is_html'] = true; // html or text
+$config['priority'] = 3; // 1 = High, 3 = Normal, 5 = low. When null, the header is not set at all.
 
-// If enabled will be logged info about emails in Codeigniter error logs
-$config['enable_debug'] = false;
+// If enabled will be logged info about emails
+// 0: Disable debugging (you can also leave this out completely, 0 is the default).
+// 1: Output messages sent by the client.
+// 2: as 1, plus responses received from the server (this is the most useful setting).
+// 3: as 2, plus more information about the initial connection - this level can help diagnose STARTTLS failures.
+// 4: as 3, plus even lower-level information, very verbose, don't use for debugging SMTP, only low-level problems.
+$config['enable_debug'] = 0;
 
 // default sender
 $config['sancho_mail_default_sender'] = defined('SANCHO_MAIL_DEFAULT_SENDER') ? SANCHO_MAIL_DEFAULT_SENDER : '';
@@ -47,3 +44,4 @@ $config['sancho_mail_header_img'] = defined('SANCHO_MAIL_HEADER_IMG') ? SANCHO_M
 
 // footer image for custom mails
 $config['sancho_mail_footer_img'] = defined('SANCHO_MAIL_FOOTER_IMG') ? SANCHO_MAIL_FOOTER_IMG : '';
+
