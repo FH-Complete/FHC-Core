c113c8086253f64556823171cd9935190d989cf2
Harald Bamberger <harald.bamberger@technikum-wien.at>
2025-12-22 21:11:05 +0100

    fix calculation of next student matrikelnr aka personenkennzeichen and student uid

--- application/libraries/PrestudentLib.php
+++ application/libraries/PrestudentLib.php
@@ -687,9 +687,6 @@ class PrestudentLib
 		$now = date('c');
 		$today = date('Y-m-d');
 
-		$jahr = mb_substr($studiensemester_kurzbz, 4, 2);
-
-
 		// Genererate Personenkennzeichen
 		$personenkennzeichen = $this->_ci->StudentModel->generateMatrikelnummer2(
 			$student_data->studiengang_kz,
@@ -699,8 +696,9 @@ class PrestudentLib
 		if (isError($personenkennzeichen))
 			return $personenkennzeichen;
 		$personenkennzeichen = getData($personenkennzeichen);
-		
-		
+
+		$jahr = mb_substr($personenkennzeichen, 0, 2);
+
 		// Generate UID
 		$uid = $this->_ci->StudentModel->generateUID(
 			$student_data->kurzbz,
