e7f57c9ddd3ffd36ad01eb93727dca6cf9c04cab
Johann Hoffmann <ma0646@technikum-wien.at>
2026-03-30 13:52:50 +0200

    remove last pa-le join from statistik/lvplanung.php

--- content/statistik/lvplanung.php
+++ content/statistik/lvplanung.php
@@ -298,15 +298,14 @@ if($studiengang_kz!='')
 	$qry = "SELECT
 				*
 			FROM
-				lehre.tbl_projektarbeit, lehre.tbl_lehreinheit, lehre.tbl_lehrveranstaltung,
+				lehre.tbl_projektarbeit, lehre.tbl_lehrveranstaltung,
 				lehre.tbl_projektbetreuer, public.tbl_person
 			WHERE
-				tbl_projektarbeit.lehreinheit_id=tbl_lehreinheit.lehreinheit_id AND
-				tbl_lehreinheit.lehrveranstaltung_id=tbl_lehrveranstaltung.lehrveranstaltung_id AND
+				tbl_projektarbeit.lehrveranstaltung_id=tbl_lehrveranstaltung.lehrveranstaltung_id AND
 				tbl_projektarbeit.projektarbeit_id=tbl_projektbetreuer.projektarbeit_id AND
 				tbl_person.person_id=tbl_projektbetreuer.person_id AND
 				tbl_lehrveranstaltung.studiengang_kz=".$db->db_add_param($studiengang_kz, FHC_INTEGER)." AND
-				tbl_lehreinheit.studiensemester_kurzbz=".$db->db_add_param($semester_aktuell)."
+				tbl_projektarbeit.studiensemester_kurzbz=".$db->db_add_param($semester_aktuell)."
 				";
 
 	if($semester!='')
