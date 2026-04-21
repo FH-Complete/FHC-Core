956b2017576cc7cde388f67e7f51b174f93aa65e
adisposkofh <ma1434@technikum-wien.at>
2026-04-21 11:25:48 +0200

    minor fix

--- public/js/apps/Dashboard/Fhc.js
+++ public/js/apps/Dashboard/Fhc.js
@@ -233,11 +233,18 @@ const app = Vue.createApp({
 		renderers: null,
 	}),
 	components: {},
-	inject: ["isMobile"],
+	computed: {
+		isMobile() {
+			const smallScreen = window.matchMedia("(max-width: 767px)").matches;
+			const touchCapable = ("ontouchstart" in window) || navigator.maxTouchPoints > 0;
+			return smallScreen;// && touchCapable;
+		},
+	},
 	provide() {
 		return { // provide injectable & watchable language property
 			language: Vue.computed(() => this.$p.user_language),
 			renderers: Vue.computed(() => this.renderers),
+			isMobile: this.isMobile,
 		}	
 	},
 	methods: {
