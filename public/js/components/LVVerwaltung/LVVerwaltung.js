10a97a80de8a30e6df5ab20fea144cef745f293c
Harald Bamberger <harald.bamberger@technikum-wien.at>
2026-08-05 13:35:51 +0200

    add missing nav-language component import

--- public/js/components/LVVerwaltung/LVVerwaltung.js
+++ public/js/components/LVVerwaltung/LVVerwaltung.js
@@ -6,6 +6,7 @@ import BaseTreemenu from '../Base/Treemenu.js';
 import StvStudiensemester from "../Stv/Studentenverwaltung/Studiensemester.js";
 import LvTable from "./Setup/Table.js";
 import LvTabs from "./Setup/Tabs.js";
+import NavLanguage from "../navigation/Language.js";
 
 import ApiDetails from "../../api/lehrveranstaltung/details.js";
 import ApiLektor from "../../api/lehrveranstaltung/lektor.js";
@@ -25,7 +26,8 @@ export default {
 		StvStudiensemester,
 		LvTable,
 		LvTabs,
-		AppConfig
+		AppConfig,
+		NavLanguage
 	},
 	props: {
 		avatarUrl: String,
