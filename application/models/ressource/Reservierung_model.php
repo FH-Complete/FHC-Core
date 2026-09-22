c34a74c0401a97b496a538f1c11380f4e2b2ce21
adisposkofh <ma1434@technikum-wien.at>
2026-09-22 14:16:31 +0200

    reservierung model table alias fix

--- application/models/ressource/Reservierung_model.php
+++ application/models/ressource/Reservierung_model.php
@@ -24,7 +24,7 @@ class Reservierung_model extends DB_Model
 		$lvplan_reservierungen_query = "SELECT r.* , stund.beginn, stund.ende,
 			CASE
 				WHEN r.gruppe_kurzbz IS NOT NULL THEN r.gruppe_kurzbz 
-				ELSE CONCAT(UPPER(studg.typ),UPPER(studg.kurzbz),'-',COALESCE(CAST(r.semester AS varchar),'/'),COALESCE(CAST(r.verband AS varchar),'/'),COALESCE(CAST(res.gruppe AS varchar),'/')) 
+				ELSE CONCAT(UPPER(studg.typ),UPPER(studg.kurzbz),'-',COALESCE(CAST(r.semester AS varchar),'/'),COALESCE(CAST(r.verband AS varchar),'/'),COALESCE(CAST(r.gruppe AS varchar),'/')) 
 			END as gruppen_kuerzel
 			FROM campus.vw_reservierung r
 			JOIN public.tbl_studiengang studg ON studg.studiengang_kz=r.studiengang_kz
